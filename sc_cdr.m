3e42fbd4e698b6fc3b111dd9fb6469fb63e174b0
Cai <jcai@cvm.tamu.edu>
2019-07-21 22:25:49 -0500

    .

--- sc_cdr.m
+++ sc_cdr.m
@@ -1,11 +1,12 @@
-function [r]=sc_cdr(X,bgcutoff)
+function [r]=sc_cdr(X,cutoff)
 %SC_CDR - calcualtes Cellular Detection Rate (CDR) of cells
 % CDR is the proportion of genes detected in each cell.
 % https://genomebiology.biomedcentral.com/articles/10.1186/s13059-015-0844-5
 
-if nargin<2, bgcutoff=0; end
-n=size(X,1);   % number of genes
-r=sum(X>bgcutoff)./n;
+if nargin<2, cutoff=0; end
+% n=size(X,1);   % number of genes
+% r=sum(X>cutoff)./n;
+r=mean(X>cutoff);
 
 % Usage:
 % [~,s]=pca(X');
