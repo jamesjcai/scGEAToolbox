90a62e808a8a71c817a2101c59b3ca7190630715
Cai <jcai@cvm.tamu.edu>
2019-08-05 22:01:14 -0500

    .

--- sc_transform.m
+++ sc_transform.m
@@ -2,7 +2,7 @@ function [X]=sc_transform(X,varargin)
 
 p = inputParser;
 defaultType = 'csndm';
-validTypes = {'csndm'};
+validTypes = {'csndm','bigscale'};
 checkType = @(x) any(validatestring(x,validTypes));
 
 addRequired(p,'X',@isnumeric);
@@ -13,4 +13,10 @@ parse(p,X,varargin{:})
 switch p.Results.type
     case 'csndm'
         [X]=run_csndm(X);
+    case 'bigscale'
+        pw1=fileparts(which(mfilename));
+        pth=fullfile(pw1,'thirdparty/bigSCale');
+        addpath(pth);
+        % model=1. Log(x), then each row (gene) normalized between [-5:5]
+        [X]=transform_bigscale(X);
 end
