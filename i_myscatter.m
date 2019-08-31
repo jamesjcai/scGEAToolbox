a04e3589c84abca4ed45e64743af561bd93a9cb6
Cai <jcai@cvm.tamu.edu>
2019-08-31 17:16:43 -0500

    Update i_myscatter.m

--- i_myscatter.m
+++ i_myscatter.m
@@ -1,25 +1,18 @@
-function h=i_myscatter(s,c)
+function h=i_myscatter(s,c,methodid)
+if nargin<3, methodid=1; end
+if nargin<2, c='b'; end
+
 x=s(:,1);
 y=s(:,2);
 
-if nargin<2
-    c='b';
-end
-
-if size(s,2)==3
-    z=s(:,3);
-end
-
-if nargout>0
-    if size(s,2)==2
-                h=scatter(x,y,10,c,'filled');
-    elseif size(s,2)==3
-                h=scatter3(x,y,z,10,c,'filled');
-    end
-else
-    if size(s,2)==2
-                scatter(x,y,10,c,'filled');
-    elseif size(s,2)==3
-                scatter3(x,y,z,10,c,'filled');
-    end   
+switch methodid
+    case 1
+        if size(s,2)==3, z=s(:,3); end
+        if size(s,2)==2
+           h=scatter(x,y,10,c,'filled');
+        elseif size(s,2)==3
+           h=scatter3(x,y,z,10,c,'filled');
+        end
+    case 2
+        h=gscatter(x,y,c);
 end
