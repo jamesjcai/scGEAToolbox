fee02a07030add68e7bfd255f1c15906b380d128
Cai <jcai@cvm.tamu.edu>
2019-08-31 17:18:58 -0500

    .

--- sc_pcnet.m
+++ sc_pcnet.m
@@ -1,11 +1,14 @@
 function [A]=sc_pcnet(X,ncom)
-% [A]=sc_pcnet(X,ncom)
+% [A]=sc_pcnet(X,ncom)      % X = expression matrix of genes x cells
 % ncom - number of components used (default=3)
+% ref: https://rdrr.io/cran/dna/man/PCnet.html
+% https://github.com/cran/dna/blob/master/src/rpcnet.c
+
 if nargin<2
    ncom=3;
 end
 
-% [X]=sc_norm(X);
+%[X]=sc_norm(X);
 X=X';
 X=zscore(X);
 
@@ -17,9 +20,9 @@ for k=1:n
     Xi(:,k)=[];
     [~,~,coeff]=svds(Xi,ncom);    
     score=Xi*coeff;
-    %[coeff,score]=pca(Xi);
-    %coeff=coeff(:,1:ncom);
-    score=(score./(vecnorm(score).^2));
+    % [coeff,score]=pca(Xi);
+    % coeff=coeff(:,1:ncom);
+    score=(score./(vecnorm(score).^2));    
     Beta=sum(y.*score);
     A(k,A(k,:)==1)=coeff*Beta';
 end
