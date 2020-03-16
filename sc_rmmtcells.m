8b3a47d44f2028d0615b1323e996441a5296a356
James Cai <jamescai@genomezoo.net>
2020-03-16 11:00:13 -0500

    .

--- sc_rmmtcells.m
+++ sc_rmmtcells.m
@@ -1,9 +1,22 @@
-function [X]=sc_rmmtcells(X,genelist,mtratio,txtpat)
+function [X]=sc_rmmtcells(X,genelist,mtratio,txtpat,vebrose)
 
 if nargin<3, mtratio=0.1; end
 if nargin<4, txtpat="mt-"; end
+if nargin<5, vebrose=true; end
+
 idx=startsWith(genelist,txtpat,'IgnoreCase',true);
+if sum(idx)>0 && vebrose
+   fprintf('%d mt-genes found.\n',sum(idx));
+else
+   fprintf('No mt-genes found.\n',sum(idx));  
+end
 lbsz=sum(X); 
 lbsz_mt=sum(X(idx,:));
 f_mtreads=lbsz_mt./lbsz;
-X=X(:,f_mtreads<mtratio);
+if sum(f_mtreads>=mtratio)>0
+    X=X(:,f_mtreads<mtratio);
+    if vebrose
+        fprintf('%d cells with >=%.2f mt-read ratio removed.\n',...
+            sum(f_mtreads<mtratio),mtratio);
+    end
+end
--- sc_rmmtgenes.m
+++ sc_rmmtgenes.m
@@ -1,11 +1,17 @@
-function [X,genelist,idx]=sc_rmmtgenes(X,genelist,txtpat)
+function [X,genelist,idx]=sc_rmmtgenes(X,genelist,txtpat,vebrose)
 if nargin<3
    txtpat="mt-";
 end
+if nargin<4, vebrose=true; end
 idx=startsWith(genelist,txtpat,'IgnoreCase',true);
 % genelist(idx);
 genelist=genelist(~idx);
 X=X(~idx,:);
+if sum(idx)>0 && vebrose
+   fprintf('%d mt-genes removed.\n',sum(idx));
+else
+   fprintf('No mt-genes found.\n',sum(idx));
+end
 if nargout>3
     idx=find(idx);
 end
