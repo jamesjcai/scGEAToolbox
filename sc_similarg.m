f827807466e1ff0e58e378fb41a94da9296fca35
James Cai <jamescai@genomezoo.net>
2020-03-27 14:19:42 -0500

    Create sc_similarg.m

---
+++ sc_similarg.m
@@ -0,0 +1,25 @@
+function [idx]=sc_similarg(X,genelist,targetgene,k,isgene)
+if nargin<5, isgene=true; end
+if nargin<4, k=5; end
+if nargin<3
+    error('Need 3 inputs (X,genelist,targetgene)'); 
+end
+
+if isnumeric(targetgene)
+    targetidx=targetgene;
+else
+    [y,targetidx]=ismember(targetgene, genelist);
+    if ~y, idx=0; return; end    
+end
+if ~(targetidx>0), idx=0; return; end
+if isgene
+    u=nanmean(X,2);
+    cv2=nanvar(X,0,2)./u.^2;
+    A=[u cv2];
+    idx=knnsearch(A,A(targetidx,:),'K',k+1,...
+        'Distance','Euclidean');
+    idx=idx(2:end);
+end
+end
+
+
