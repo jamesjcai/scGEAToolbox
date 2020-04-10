a057a393aec9f90cfa1ada39d7ed5528d40b7cdb
James Cai <jamescai@genomezoo.net>
2020-04-09 23:22:40 -0500

    Update sc_qcfilter.m

--- sc_qcfilter.m
+++ sc_qcfilter.m
@@ -1,8 +1,8 @@
-function [X,genelist,keptidxv]=sc_qcfilter(X,genelist,lbsz,mtratio,zeroratio)
+function [X,genelist,keptidxv]=sc_qcfilter(X,genelist,libsize,mtratio,dropout)
 
-if nargin<3, lbsz=1000; end
+if nargin<3, libsize=1000; end
 if nargin<4, mtratio=0.1; end
-if nargin<5, zeroratio=0.05; end
+if nargin<5, dropout=0.01; end
 
 [X,keptidx]=sc_rmmtcells(X,genelist,mtratio);
 keptidxv{1}=keptidx;
@@ -12,11 +12,11 @@ newsz=1;
 c=1;
 while ~isequal(oldsz,newsz)
     oldsz=size(X);
-    [X,genelist]=sc_filterg(X,genelist,zeroratio);
+    [X,genelist]=sc_filterg(X,genelist,dropout);
     [X,keptidx]=sc_filterc(X);
     keptidxv{end+1}=keptidx;
     [X,genelist]=sc_selectg(X,genelist);
-    [X,keptidx]=sc_selectc(X,lbsz);
+    [X,keptidx]=sc_selectc(X,libsize);
     keptidxv{end+1}=keptidx;
     newsz=size(X);
     c=c+1;
