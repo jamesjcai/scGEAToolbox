d937206b5d30fd949b3f2e2b29302c8778d1d89e
James Cai <jamescai@genomezoo.net>
2020-07-28 12:47:42 -0500

    add run_mast

--- run_mast.m
+++ run_mast.m
@@ -1,21 +1,25 @@
-function [A]=run_mast(X,Y)
-
-% if nargin<2, plotit=false; end
+function [A]=run_mast(X,Y,genelist)
+if nargin<3
+    genelist=(1:size(X,1))';
+end
 if isempty(FindRpath)
    error('Rscript.exe is not found.');
 end
-
 oldpth=pwd;
 pw1=fileparts(which(mfilename));
 pth=fullfile(pw1,'thirdparty/R_MAST');
 cd(pth);
 fprintf('CURRENTWDIR = "%s"\n',pth);
-
+if exist('output.csv','file')
+    delete('output.csv');
+end
 writematrix(X,'input1.txt');
 writematrix(Y,'input2.txt');
 % RunRcode('script.R');
-if exist('output.txt','file')
-    A=readmatrix('output.txt',1,1);
+if exist('output.csv','file')
+    A=readtable('output.csv');
+    A.Var1=genelist(A.Var1);
+    A.Properties.VariableNames{'Var1'} = 'gene';
 else
     A=[];
 end
--- thirdparty/R_MAST/script.R
+++ thirdparty/R_MAST/script.R
@@ -3,28 +3,20 @@
 #if (!requireNamespace("BiocManager", quietly = TRUE))
 #    install.packages("BiocManager")
 #BiocManager::install("MAST")
+#install.packages("Seurat")
 
 library(MAST)
-X <- as.matrix(read.table("input1.txt", sep="\t"))
-Y <- as.matrix(read.table("input2.txt", sep="\t"))
+library(Seurat)
+X <- as.matrix(read.table("input1.txt", sep=","))
+Y <- as.matrix(read.table("input2.txt", sep=","))
 
+rownames(X) <- rownames(Y) <- paste0('', seq_len(nrow(X)))
 
-# write.table(A, paste(dir,"/A.txt",sep=""), row.names=F, col.names=F, sep="\t")
+X <- CreateSeuratObject(X, project = 'X')
+Y <- CreateSeuratObject(Y, project = 'Y')
 
-cond<-factor(colData(sca)$condition)
-cond<-relevel(cond,"Unstim")
-colData(sca)$condition<-cond
-zlmCond <- zlm(~condition + cngeneson, sca)
-# The following are equivalent
-## lrt <- lrTest(zlm, "condition")
-## lrt <- lrTest(zlm, CoefficientHypothesis('conditionStim'))
-
-# This would test if 2*cngeneson=conditionStim
-#  This is sheer nonsense biologically and statistically, but gives an example of the flexibility.
-## lrt <- lrTest(zlm, Hypothesis('2*cngeneson-conditionStim'))
-We could run a likelihood ratio test here, testing for differences when we drop the condition factor. Note that any arbitrary contrast matrix can be tested here, and specified either using a matrix or syntactically. See Hypothesis for details.
-
-#only test the condition coefficient.
-summaryCond <- summary(zlmCond, doLRT='conditionStim') 
-#print the top 4 genes by contrast using the logFC
-print(summaryCond, n=4)
+ALL <- merge(X,Y)
+ALL <- NormalizeData(ALL)
+ALL <- ScaleData(ALL)
+DE <- FindMarkers(ALL, ident.1 = 'X', ident.2 = 'Y', logfc.threshold = 0, test.use = 'MAST')
+write.csv(DE, 'output.csv')
