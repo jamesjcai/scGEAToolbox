b5918c45edbf0b7fe7fdc343ca2530a0546f9e77
James Cai <jamescai@genomezoo.net>
2020-08-05 13:42:48 -0500

    .

--- thirdparty/celltype_mat/i_preparemarkerdb.m
+++ thirdparty/celltype_mat/i_preparemarkerdb.m
@@ -7,13 +7,16 @@ T=T(i,:);
 
 a=string(unique(T.cellType));
 gt=string(T.cellType);
-to=string(T.officialGeneSymbol);
+genesymbollist=string(T.officialGeneSymbol);
+organlist=string(T.organ);
+
 fid=fopen(sprintf('markerlist_%s_panglaodb.txt',lower(sp)),'w');
 for k=1:length(a)
     k
     idx=find(gt==a(k));
     fprintf(fid,'%s\t',a(k));
-    fprintf(fid,'%s,', to(idx));
+    % fprintf(fid,'%s\t',organlist(k));
+    fprintf(fid,'%s,', genesymbollist(idx));
     fprintf(fid,'\n');
 end
 fclose(fid);
