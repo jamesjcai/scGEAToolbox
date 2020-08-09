ef9bc49531fd4487c10401dde359ab00348e7ca8
James Cai <jamescai@genomezoo.net>
2020-08-09 12:00:44 -0500

    Update sc_scattermarker.m

--- sc_scattermarker.m
+++ sc_scattermarker.m
@@ -1,11 +1,22 @@
 function sc_scattermarker(X,genelist,g,s)
 
-x=s(:,1);
-y=s(:,2);
-z=log2(1+X(genelist==g,:));
-if isempty(z)
-    warning('No expression.');
-else
-    sc_stemscatter(x,y,z);
-    title(g)
+if iscell(g)
+    for k=1:length(g)
+        sc_scattermarker(X,genelist,g{k},s);
+    end
+elseif isstring(g) && ~isStringScalar(g)
+    for k=1:length(g)
+        sc_scattermarker(X,genelist,g(k),s);
+    end
+elseif isStringScalar(g) || ischar(g)
+    x=s(:,1);
+    y=s(:,2);
+    if ismember(g,genelist)
+        z=log2(1+X(genelist==g,:));
+        figure;
+        sc_stemscatter(x,y,z);
+        title(g)
+    else
+        warning('No expression.');
+    end
 end
