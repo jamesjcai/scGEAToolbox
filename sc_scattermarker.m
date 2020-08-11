9fc9d1e54dd1efe65cbe64061ff8a780fb5b4a07
James Cai <jamescai@genomezoo.net>
2020-08-11 11:52:57 -0500

    Update sc_scattermarker.m

--- sc_scattermarker.m
+++ sc_scattermarker.m
@@ -1,22 +1,37 @@
-function sc_scattermarker(X,genelist,g,s)
+function sc_scattermarker(X,genelist,g,s,methodid)
+%SC_SCATTERMARKER(X,genelist,g,s,methodid)
+%
+% USAGE:
+% s=sc_tsne(X,3);
+% g=["AGER","SFTPC","SCGB3A2","TPPP3"];
+% sc_scattermarker(X,genelist,g,s);
 
+
+if nargin<5, methodid=1; end
 if iscell(g)
     for k=1:length(g)
-        sc_scattermarker(X,genelist,g{k},s);
+        sc_scattermarker(X,genelist,g{k},s,methodid);
     end
 elseif isstring(g) && ~isStringScalar(g)
     for k=1:length(g)
-        sc_scattermarker(X,genelist,g(k),s);
+        sc_scattermarker(X,genelist,g(k),s,methodid);
     end
 elseif isStringScalar(g) || ischar(g)
-    x=s(:,1);
-    y=s(:,2);
     if ismember(g,genelist)
-        z=log2(1+X(genelist==g,:));
-        figure;
-        sc_stemscatter(x,y,z);
+        x=s(:,1);
+        y=s(:,2);            
+        figure;        
+        switch methodid
+            case 1
+                z=log2(1+X(genelist==g,:));
+                sc_stemscatter(x,y,z);
+            case 2
+                z=s(:,3);
+                c=log2(1+X(genelist==g,:));
+                scatter3(x,y,z,10,c,'filled');
+        end
         title(g)
     else
-        warning('No expression.');
+        warning('%s no expression',g);
     end
 end
