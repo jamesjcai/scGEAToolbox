4ee4ab41b82a6be3df4306d57dcc50cb05cc823f
James Cai <jamescai@genomezoo.net>
2020-08-17 16:42:56 -0500

    Update sc_readmtxfile.m

--- sc_readmtxfile.m
+++ sc_readmtxfile.m
@@ -1,10 +1,22 @@
 function [X,genelist,celllist]=sc_readmtxfile(matrixmtxfile,featurestxtfile,barcodestxtfile,coln)
 if nargin<4, coln=1; end
+if nargin<2, featurestxtfile=[]; end
+
 %[X,genelist,celllist]=sc_readmtxfile('matrix.mtx','features.tsv','barcodes.tsv')
 if exist(matrixmtxfile,'file') ~= 2
     error(message('FileNotFound'));        
 end
 X=mmread(matrixmtxfile);
+try
+    X=full(X);
+catch
+
+end
+if isempty(featurestxtfile)
+    genelist=[];
+    celllist=[];
+    return;
+end
 T=readtable(featurestxtfile,'ReadVariableNames',false,'filetype','text');
 if coln==1
     genelist=string(T.Var1);
@@ -19,10 +31,7 @@ if nargout>2 && ~isempty(barcodestxtfile)
     T=readtable(barcodestxtfile,'ReadVariableNames',false,'filetype','text');
     celllist=string(T.Var1);
 end
-try
-X=full(X);
-catch
-end
+
 assert(isequal(size(X,1),length(genelist)))
 
 
