4c9f5c9fd7f2c0e2821d405e747f375c4cd3c24d
James Cai <jamescai@genomezoo.net>
2020-08-21 23:15:49 -0500

    .

--- sc_markerexplorer.m
+++ sc_markerexplorer.m
@@ -7,25 +7,52 @@ function sc_markerexplorer(X,genelist,s,varargin)
    addRequired(p,'genelist',@isstring);
    addRequired(p,'s',@isnumeric);
    addOptional(p,'method',"ttest",@(x) (isstring(x)|ischar(x))&ismember(lower(string(x)),["ttest","mast"]));
+   addOptional(p,'numfig',1,@isnumeric);
    parse(p,X,genelist,s,varargin{:});
    method=p.Results.method;
+   numfig=p.Results.numfig;
    
   
 global mkexplorer_clustid
 mkexplorer_clustid=0;
 hFig = figure;
 hAx = axes('Parent',hFig);
-scatter3(hAx, s(:,1),s(:,2),s(:,3),10);
+
+if size(s,2)==3
+    scatter3(hAx, s(:,1),s(:,2),s(:,3),10);
+elseif size(s,2)==2
+    scatter(hAx, s(:,1),s(:,2),10);
+end
+
+defaultToolbar = findall(hFig,'Type','uitoolbar');
+pt = uipushtool(defaultToolbar);
+ptImage = rand(16,16,3);
+pt.CData = ptImage;
+pt.Tooltip = 'Change UI Figure Color';
+pt.ClickedCallback = @colorToolClicked;
+
+function colorToolClicked(src,event)    
+    [indx,tf] = listdlg('PromptString',{'Select a gene:',...
+    '',''},'SelectionMode','single','ListString',genelist(1:100));
+    if tf==1        
+        [ax,bx]=view();
+        figure;
+        sc_markerscatter(X,genelist,genelist(indx),s,3);
+        view(ax,bx);  
+    end
+end
+
+%h = uicontrol('Position',[5 5 150 30],'String','Calculate xdiff',...
+%              'Callback', @JCal);
 
 hBr = brush(hFig);
 % hBr.Enable='on';
-hBr.ActionPostCallback = {@onBrushAction,X,genelist,s,method};
+hBr.ActionPostCallback = {@onBrushAction,X,genelist,s,method,numfig};
 end
-
 
 % ref: https://www.mathworks.com/matlabcentral/answers/385226-how-to-use-the-data-brush-tool-to-automatically-save-selected-points-in-multiple-line-plots
 
-function onBrushAction(~,eventdata,X,genelist,s,method)
+function onBrushAction(~,eventdata,X,genelist,s,method,numfig)
 global mkexplorer_clustid
 % Extract plotted graphics objects
 % Invert order because "Children" property is in reversed plotting order
@@ -45,12 +72,16 @@ hLines = flipud(eventdata.Axes.Children);
                     [markerlist]=sc_pickmarkers(X,genelist,1+ptsSelected,2);
             end
             [ax,bx]=view();
+            for kkk=1:numfig
             figure;
             for kk=1:min([9,length(markerlist)])
                 subplot(3,3,kk)
-                sc_scattermarker(X,genelist,markerlist(kk),s,3);
-                view(ax,bx);            
+                sc_markerscatter(X,genelist,...
+                    markerlist(kk+9*(kkk-1)),s,3);
+                view(ax,bx);   
             end
+            end
+            mkexplorer_clustid=mkexplorer_clustid+1;
             assignin('base',sprintf('mkexplorerL%d',...
                 mkexplorer_clustid),markerlist);
         end
---
+++ sc_markerscatter.m
@@ -0,0 +1,58 @@
+function sc_markerscatter(X,genelist,g,s,methodid,sz)
+%SC_MARKERSCATTER(X,genelist,g,s,methodid)
+%
+% USAGE:
+% s=sc_tsne(X,3);
+% g=["AGER","SFTPC","SCGB3A2","TPPP3"];
+% sc_scattermarker(X,genelist,g,s);
+
+if isvector(s)||isscalar(s), error('S should be a matrix.'); end
+if nargin<6, sz=5; end
+if nargin<5, methodid=1; end
+if iscell(g)
+    for k=1:length(g)
+        figure;
+        sc_scattermarker(X,genelist,g{k},s,methodid,sz);
+    end
+elseif isstring(g) && ~isStringScalar(g)
+    for k=1:length(g)
+        figure;
+        sc_scattermarker(X,genelist,g(k),s,methodid,sz);
+    end
+elseif isStringScalar(g) || ischar(g)
+    if ismember(g,genelist)
+        x=s(:,1);
+        y=s(:,2);
+        if min(size(s))==2
+            z=[];
+        else
+            z=s(:,3);
+        end               
+        switch methodid
+            case 1
+                z=log2(1+X(genelist==g,:));
+                sc_stemscatter(x,y,z);
+            case 2                
+                c=log2(1+X(genelist==g,:));
+                if isempty(z)
+                    scatter(x,y,sz,c,'filled');
+                else
+                    scatter3(x,y,z,sz,c,'filled');
+                end
+                colormap('default');
+            case 3                
+                c=log2(1+X(genelist==g,:));
+                if isempty(z)
+                    scatter(x,y,sz,c,'filled');
+                else
+                    scatter3(x,y,z,sz,c,'filled');
+                end                
+                a=colormap('autumn');
+                a(1,:)=[.8 .8 .8];
+                colormap(a);
+        end
+        title(g)
+    else
+        warning('%s no expression',g);
+    end
+end
