6f92d0051a1f301b35bf3163bf38638a2d9473fd
James Cai <jamescai@genomezoo.net>
2020-09-04 14:17:14 -0500

    .

--- sc_celltypeexplorer.m
+++ sc_celltypeexplorer.m
@@ -78,7 +78,7 @@ hLines = flipud(eventdata.Axes.Children);
                 switch lower(method)
                 case 'alona'
                     %[Tct]=sc_celltypecaller(Xi,gi,[],'species',species,'organ',organ);                    
-                    [Tct]=sc_celltypebrushed(X,genelist,s,ptsSelected,species,organ);
+                    [Tct]=local_celltypebrushed(X,genelist,s,ptsSelected,species,organ);
                     ctxt=Tct.C1_Cell_Type{1};
                 case 'singler'
                     % [~,i]=ismember(brushedData,s,'rows');
@@ -122,5 +122,25 @@ hLines = flipud(eventdata.Axes.Children);
     end
 end
 
+function [Tct]=local_celltypebrushed(X,genelist,s,brushedData,species,organ)
+
+% USAGE:
+
+% s=sc_tsne(X,3);
+% figure; sc_cellscatter(s)
+% % get brushedData
+% [Tct]=sc_celltypesbrushed(X,genelist,s,brushedData)
+if nargin<6, organ='all'; end
+if nargin<5, species='human'; end
+
+if islogical(brushedData)
+    i=brushedData;
+else
+    [~,i]=ismember(brushedData,s,'rows');
+end
+Xi=X(:,i);
+[Xi,gi]=sc_selectg(Xi,genelist);
+[Tct]=sc_celltypecaller(Xi,gi,[],'species',species,'organ',organ);
+end
 
 
