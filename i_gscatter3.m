dad72053412ff35e70e564e5347a742b019aaedd
James Cai <jamescai@genomezoo.net>
2020-09-09 11:01:45 -0500

    .

--- i_gscatter3.m
+++ i_gscatter3.m
@@ -1,21 +1,22 @@
 function i_gscatter3(s,c,methodid)
 
 if nargin<3, methodid=1; end
-if nargin<2, c='b'; end
+if nargin<2, c=ones(size(s,1),1); end
 
 x=s(:,1);
 y=s(:,2);
 
 if iscell(c), c=grp2idx(c); end
-isbinary=logical(numel(unique(c))==2);
-        if size(s,2)>=3, z=s(:,3); end
+kc=numel(unique(c));
+
+if size(s,2)>=3, z=s(:,3); end
 
 switch methodid
     case 1
         if size(s,2)==2
-           scatter(x,y,10,c,'filled');
+           scatter(x,y,10,c);
         elseif size(s,2)>=3
-           scatter3(x,y,z,10,c,'filled');
+           scatter3(x,y,z,10,c);
         end
     case 2
         if size(s,2)==2
@@ -25,72 +26,14 @@ switch methodid
         end
 end
 
-if isbinary
-    colormap(lines(2));
+if kc<=5
+    colormap(lines(kc));
+else
+    a=colormap('autumn');
+    a(1,:)=[.8 .8 .8];
+    colormap(a);
 end
-
 add_3dcamera;
-
-% hFig=gcf;
-% tb = uitoolbar(hFig);
-% pt = uipushtool(tb,'Separator','off');
-% [img,map] = imread(fullfile(fileparts(which(mfilename)),...
-%             'private','camera.gif'));
-% ptImage = ind2rgb(img,map);
-% pt.CData = ptImage;
-% pt.Tooltip = 'Select a gene to show expression';
-% pt.ClickedCallback = @camera3dmp4;
-
 end
-% 
-% function camera3dmp4(~,~)
-%     OptionZ.FrameRate=15;OptionZ.Duration=5.5;OptionZ.Periodic=true;
-%     fname=tempname;
-%     CaptureFigVid([-20,10;-110,10;-190,80;-290,10;-380,10],fname,OptionZ)
-%     winopen(tempdir);
-%     winopen(sprintf('%s.mp4',fname));
-% end
 
 
-function gscatter3b(x,y,z,group,clr,sym,siz,doleg,xnam,ynam,znam)
-%GSCATTER3B  3D Scatter plot with grouping variable
-%   gscatter3(x,y,z,group,clr,sym,siz,doleg,xnam,ynam,znam)   
-%   Designed to work in the exactly same fashion as statistics toolbox's
-%   gscatter. Differently from GSCATTER3, this function requires the
-%   statistics toolbox installed.
-%   
-%
-%   See also GSCATTER, GSCATTER3
-%
-%   Copyright 2017 Gustavo Ferraz Trinade.
-% Set number of groups 
-cgroups = unique(group);
-cmap = lines(size(cgroups,1));
-% Input variables
-if (nargin < 5),  clr = lines(max(size(cgroups))); end
-if (nargin < 6) || isempty(sym), sym = '.'; end
-if (nargin < 7),  siz = 10;           end
-if (nargin < 8),  doleg = 'on';        end
-if (nargin < 9),  xnam = inputname(1); end
-if (nargin < 10), ynam = inputname(2); end
-if (nargin < 11), znam = inputname(3); end
-% Get current axes
-a = gca;
-hold(a,'on')
-% call GSCATTER and capture output argument (handles to lines)
-h = gscatter(x, y, group,clr,sym,siz,doleg,xnam,ynam);
-for i = 1:max(size(cgroups))
-        if iscell(cgroups) || ischar(cgroups)
-            gi = find(strcmp(group,cgroups(i)));
-        else
-            gi = find(group == cgroups(i));
-        end
-    
-        set(h(i), 'ZData', z( gi ));
-end
-zlabel(a,znam);    
-view(3)
-box on
-grid on
-    
-end
--- sc_explorer.m
+++ sc_explorer.m
@@ -1,19 +1,26 @@
 function sc_explorer(X,genelist,s,varargin)
-%load cleandata.mat
-%s=s_tsne;
-
    p = inputParser;
    addRequired(p,'X',@isnumeric);
    addRequired(p,'genelist',@isstring);
    addRequired(p,'s',@isnumeric);
-   addOptional(p,'method',"marker",@(x) (isstring(x)|ischar(x))&ismember(lower(string(x)),["marker","celltype","pseudotime"]));
-   addOptional(p,'numfig',1,@isnumeric);
+   addOptional(p,'type',"unknown",@(x) (isstring(x)|ischar(x))&ismember(lower(string(x)),["unknown","celltype","marker","pseudotime"]));   
    parse(p,X,genelist,s,varargin{:});
-   method=p.Results.method;
-   numfig=p.Results.numfig;   
-  
-global mkexplorer_clustid
-mkexplorer_clustid=0;
+   type=p.Results.type;
+
+   if strcmp(type,"unknown"), type=i_getoption; end
+   
+switch type
+    case "celltype"
+        MenuSelected1;
+        return; 
+    case "marker"
+        MenuSelected2;
+        return; 
+    case "pseudotime"
+        MenuSelected3;
+        return; 
+end
+
 hFig = figure();
 hAx = axes('Parent',hFig);
 if size(s,2)>=5    
@@ -30,8 +37,6 @@ tb = uitoolbar(hFig);
 pt = uipushtool(tb,'Separator','off');
 [img,map] = imread(fullfile(fileparts(which(mfilename)),...
             'private','explorer1.gif'));
-% [img,map] = imread(fullfile(matlabroot,...
-%             'toolbox','matlab','icons','HDF_object01.gif'));
 ptImage = ind2rgb(img,map);
 pt.CData = ptImage;
 pt.Tooltip = 'Cell Type Explorer...';
@@ -40,8 +45,6 @@ pt.ClickedCallback = @MenuSelected1;
 pt2 = uipushtool(tb,'Separator','on');
 [img,map] = imread(fullfile(fileparts(which(mfilename)),...
             'private','explorer2.gif'));
-%[img,map] = imread(fullfile(matlabroot,...
-%            'toolbox','matlab','icons','HDF_object01.gif'));
 ptImage = ind2rgb(img,map);
 pt2.CData = ptImage;
 pt2.Tooltip = 'Marker Gene Explorer...';
@@ -50,17 +53,10 @@ pt2.ClickedCallback = @MenuSelected2;
 pt3 = uipushtool(tb,'Separator','on');
 [img,map] = imread(fullfile(fileparts(which(mfilename)),...
             'private','explorer3.gif'));
-%[img,map] = imread(fullfile(matlabroot,...
-%            'toolbox','matlab','icons','HDF_object02.gif'));
 ptImage = ind2rgb(img,map);
 pt3.CData = ptImage;
 pt3.Tooltip = 'Pseudotime Explorer...';
 pt3.ClickedCallback = @MenuSelected3;
-
-
-       
-%hBr = brush(hFig);
-%hBr.ActionPostCallback = {@onBrushAction,X,genelist,s,method,numfig};
 
 m = uimenu('Text','&Explorers');
 mitem1 = uimenu(m,'Text','&Cell Type Explorer...');
@@ -72,7 +68,8 @@ mitem3.Accelerator = 'P';
 mitem1.MenuSelectedFcn = @MenuSelected1;
 mitem2.MenuSelectedFcn = @MenuSelected2;
 mitem3.MenuSelectedFcn = @MenuSelected3;
- 
+add_3dcamera;
+
 %         cm = uicontextmenu(hFig);
 %         m1 = uimenu(cm,'Text','Cell Type Explorer...');
 %         m2 = uimenu(cm,'Text','Marker Gene Explorer...');
@@ -83,7 +80,7 @@ mitem3.MenuSelectedFcn = @MenuSelected3;
 %         m2.MenuSelectedFcn = @MenuSelected2;
 %         m3.MenuSelectedFcn = @MenuSelected3;
 
-add_3dcamera;
+
 
     function MenuSelected1(src,event)
         answer = questdlg('Which species?', ...
@@ -118,35 +115,22 @@ add_3dcamera;
     function MenuSelected3(src,event)
         sc_pseudotimeexplorer(X,genelist,s);
     end
+
 end
 
 
-
-function i_view5d(s5d)
-    if size(s5d,2)<5
-        s5d=[s5d,zeros(size(s5d,1),5-size(s5d,2))];
-    end
-    i_view5dIDX=s5d(:,3);
-    assignin('base','i_view5dIDX',i_view5dIDX);
-    subplot(1,2,1);
-    h1=scatter3(s5d(:,1),s5d(:,2),s5d(:,3),10);
-    xlabel('dim 1')
-    ylabel('dim 2')
-    zlabel('dim 3')
-    grid on
-    box on
-    h1.ZDataSource='i_view5dIDX';
-
-    subplot(1,2,2);
-    h2=scatter3(s5d(:,3),s5d(:,4),s5d(:,5),10);
-    xlabel('dim 3')
-    ylabel('dim 4')
-    zlabel('dim 5')
-    grid on
-    box on
-    h2.XDataSource='i_view5dIDX';
-    hLD = linkdata('on');
-    evalin('base','h=findobj(gcf,''type'',''axes'');');
-    evalin('base','hlink = linkprop(h,{''CameraPosition'',''CameraUpVector''});');
-    evalin('base','rotate3d on');
+function [type]=i_getoption
+answer = questdlg('Select the type of explorer', ...
+	'Select Explorer Type', ...
+	'Cell type','Marker gene','Pseudotime','Cell type');
+switch answer
+    case 'Cell type'
+        type="celltype";
+    case 'Marker gene'
+        type="marker";
+    case 'Pseudotime'
+        type="pseudotime";
+    otherwise
+        type="unknown";
+end
 end
