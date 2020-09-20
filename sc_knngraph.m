a80871cdfa4f767938f19dfa9036533c340f4c2d
James Cai <jamescai@genomezoo.net>
2020-09-20 16:16:48 -0500

    Update sc_knngraph.m

--- sc_knngraph.m
+++ sc_knngraph.m
@@ -1,4 +1,4 @@
-function [Graph]=sc_knngraph(s,k, method)
+function [G]=sc_knngraph(s,k, method)
 if nargin<3, method=1; end
 if nargin<2, k=4; end
 
@@ -18,10 +18,26 @@ switch method
         Graph = reshape(Graph, kneighbors+1, N );
 end
 
+if nargout>0
+    N=size(s,1);
+    G=sparse(N,N);
+    for i = 1 : size(Graph,2)
+    for j = 1 : size(Graph,1)   % k+1
+         G(i,Graph(j,i))=1;
+         G(Graph(j,i),i)=1;
+    end
+    end
+    % G=0.5*(G+G');
+    G=G-diag(diag(G));
+end
+
 hold on
 for i = 1 : size(Graph,2)
- for j = 1 : size(Graph,1) 
-     line(s([i,Graph(j,i)],1), s([i,Graph(j,i)],2), s([i,Graph(j,i)],3))
- end
+for j = 1 : size(Graph,1)
+     line(s([i,Graph(j,i)],1),...
+          s([i,Graph(j,i)],2),...
+          s([i,Graph(j,i)],3),'Color','red');   
 end
+end
+
 end
