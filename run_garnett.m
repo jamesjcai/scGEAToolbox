a1d2aed062d109d59f7b1679ba662cc56dc97d52
James Cai <jamescai@genomezoo.net>
2020-11-17 16:34:30 -0600

    .

---
+++ run_garnett.m
@@ -0,0 +1,46 @@
+function [t,s]=run_garnett(X)
+% Run Monocle pseudotime analysis
+%[t_mono,s_mono]=run_monocle(X,true);
+
+if isempty(FindRpath)
+    error('Rscript.exe is not found.');
+end
+oldpth=pwd;
+pw1=fileparts(which(mfilename));
+pth=fullfile(pw1,'thirdparty/R_Garnett');
+cd(pth);
+fprintf('CURRENTWDIR = "%s"\n',pth);
+
+[~,cmdout]=RunRcode('require.R');
+if strfind(cmdout,'there is no package')>0
+    cd(oldpth);
+    error(cmdout);
+end
+websave('mmBrain_20191017.RDS','https://cole-trapnell-lab.github.io/garnett/classifiers/mmBrain_20191017.RDS');
+if exist('output.csv','file')
+    delete('output.csv');
+end
+if issparse(X), X=full(X); end
+csvwrite('input.csv',X);
+% Rpath = 'C:\Program Files\R\R-3.6.0\bin';
+% RscriptFileName = 'Z:\Cailab\mouse_neurons\adult_P10_cortex_SRR6061129\monocleMatlab.R';
+% RunRcode('monocleMatlab_3d.R');
+RunRcode('script.R');
+if exist('output.csv','file')
+    dat = csvread('output.csv',1,1);
+    t=dat(:,1);
+    s=dat(:,2:end);
+else
+    t=[];
+    s=[];
+end
+if exist('input.csv','file')
+    delete('input.csv');
+end
+if exist('output.csv','file')
+    delete('output.csv');
+end
+cd(oldpth);
+
+
+
---
+++ thirdparty/R_Garnett/script.R
@@ -0,0 +1,34 @@
+classifyCells <- function(X, outputFile){
+  cMatrix <- read.csv(X, header = FALSE)
+  rownames(cMatrix) <- paste0("G", seq_len(nrow(cMatrix)))
+  colnames(cMatrix) <- paste0("C", seq_len(ncol(cMatrix)))
+  cMatrix <- cMatrix[rowSums(cMatrix) > 0,]
+  # require(monocle)
+  require(garnett)
+  classifier <- readRDS("mmBrain_20191017.RDS")
+
+  fd <- data.frame('gene_short_name' = rownames(cMatrix))
+  rownames(fd) <- rownames(cMatrix)
+  fd <- new("AnnotatedDataFrame", data = fd)
+
+  cds <- newCellDataSet(as.matrix(cMatrix), featureData = fd, expressionFamily = negbinomial.size())
+  
+  #cds <- estimateSizeFactors(cds)
+  #cds <- reduceDimension(cds, reduction_method = "DDRTree", verbose = TRUE, max_components = 3)
+  #cds <- orderCells(cds)
+  #pseudoTiveV <- pData(cds)
+  #pseudoTiveV <- cbind(pseudoTiveV,t(cds@reducedDimS[,rownames(pseudoTiveV)]))
+  #pseudoTiveV <- pseudoTiveV[,c(2,4,5,6)]
+  #colnames(pseudoTiveV) <- c("pseudoTime", "DDRTree1", "DDRTree2", "DDRTree3")
+
+  library(org.Mm.eg.db)
+  cds <- classify_cells(cds, classifier,
+                           db = org.Mm.eg.db,
+                           cluster_extend = TRUE,
+                           cds_gene_id_type = "SYMBOL")
+  # head(pData(pbmc_cds))
+  datax = as.data.frame(pData(cds))
+  write.csv(datax, file = outputFile)
+}
+
+classifyCells(X = "input.csv", outputFile = "output.csv")
