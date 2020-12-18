e949c0efcfeeb747e8b4461366a6520df841fb9c
James Cai <jamescai@genomezoo.net>
2020-12-18 11:42:07 -0600

    .

--- +pkg/callback_PickColormap.m
+++ +pkg/callback_PickColormap.m
@@ -3,9 +3,9 @@ function callback_PickColormap(~,~)
     cx(1,:)=[.8 .8 .8];
     %a=lines(kc);
     %b=a(randperm(size(a,1)),:);
-    co={cx,'autumn','lines','default','summer',...
-        'jet','copper','winter','hsv'};
-    
+    mycmap=pkg.i_mycolormap;
+    co={cx,'lines','default','summer',...
+        'jet','copper','winter','hsv',mycmap};
 %     co={cx,a,b,'default',summer(kc),...
 %         jet(kc),copper(kc),winter(kc)};
     colormap(co{randi(length(co))});
