dba58f5c2ba64eb95585227026a87451a140c38a
James Cai <jamescai@genomezoo.net>
2020-12-20 22:07:28 -0600

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -31,10 +31,7 @@ author = 'James Cai'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    ...
-    "sphinx_rtd_theme",
-]
+extensions = ['sphinx_rtd_theme']
 
 
 # Add any paths that contain templates here, relative to this directory.
@@ -52,11 +49,11 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # a list of builtin themes.
 #
 # html_theme = 'alabaster'
-html_theme = "sphinx_rtd_theme"
+html_theme = 'sphinx_rtd_theme'
 
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_theme_path = ["_themes", ]
+html_theme_path = ['_themes']
