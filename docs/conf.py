b5c4a22bd72bdfeac22a4246704306b569a55269
James Cai <jamescai@genomezoo.net>
2020-12-20 22:05:13 -0600

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,7 @@ author = 'James Cai'
 # ones.
 extensions = [
     ...
-    "sphinx_rtd_theme"
+    "sphinx_rtd_theme",
 ]
 
 
@@ -58,5 +58,5 @@ html_theme = "sphinx_rtd_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
+html_static_path = ['_static']
 html_theme_path = ["_themes", ]
