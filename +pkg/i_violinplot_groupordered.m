0e2f7502867dc0467e01956b7ee63ece4d33b4e2
James Cai <jamescai@genomezoo.net>
2021-01-18 14:55:09 -0600

    Update i_violinplot_groupordered.m

--- +pkg/i_violinplot_groupordered.m
+++ +pkg/i_violinplot_groupordered.m
@@ -1,17 +1,32 @@
-function i_violinplot_groupordered(d,c)
+function p=i_violinplot_groupordered(d,c,sorted_c,colorit)
 import pkg.Violin
 import pkg.violinplot
 
-if ~isstring(c)
-    c=string(c);
-end
+if nargin<4, colorit=false; end
+if nargin<3, sorted_c=[]; end
+if ~isstring(c), c=string(c); end
 
 c=strrep(c,'_',' ');
 [~,cL]=grp2idx(c);
-[~,i]=sort(grpstats(d,c,@median),'descend');
-violinplot(d,c,'GroupOrder',cL(i),...
+if isempty(sorted_c)
+    [~,sortidx]=sort(grpstats(d,c,@median),'descend');
+    sorted_c=cL(sortidx);
+else
+    sorted_c=strrep(sorted_c,'_',' ');
+end
+
+if ~iscell(sorted_c)
+    sorted_c=cellstr(sorted_c);
+end
+if colorit
+p=violinplot(d,c,'GroupOrder',sorted_c,...
+    'ShowData',false,...
+    'EdgeColor',[0 0 0]);  
+else
+p=violinplot(d,c,'GroupOrder',sorted_c,...
     'ShowData',false,'ViolinColor',[1 1 1],...
     'EdgeColor',[0 0 0]);
+end
 xtickangle(-45);
 box on
 ylabel('Differentiation Potency');
