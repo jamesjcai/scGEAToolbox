168afbb45dcef6a8c72a997358432ae6a071e158
James Cai <jamescai@genomezoo.net>
2021-01-20 22:12:23 -0600

    Update callback_ComparePotency.m

--- +gui/callback_ComparePotency.m
+++ +gui/callback_ComparePotency.m
@@ -7,21 +7,26 @@ function callback_ComparePotency(src,~)
     end
     if ~isempty(sce.list_cell_attributes{2}) && ~isempty(sce.c_cell_type_tx)
         x=sce.list_cell_attributes{2};
-        y=sce.c_cell_type_tx;
+        y=sce.c_cell_type_tx;        
     figure;
     pkg.i_violinplot_groupordered(x,y);
     if ~isempty(sce.c_batch_id)
-        figure;
-        h1=subplot(1,2,1);
-        i=sce.c_batch_id==1;
-        pkg.i_violinplot_groupordered(x(i),y(i));
-        [aabb]=ylim();
-        h2=subplot(1,2,2);
-        i=sce.c_batch_id==2;
-        pkg.i_violinplot_groupordered(x(i),y(i));
-        [ccdd]=ylim();
-        ylim(h1,[min(aabb(1),ccdd(1)) max(aabb(2),ccdd(2))]);
-        ylim(h2,[min(aabb(1),ccdd(1)) max(aabb(2),ccdd(2))]);
+        try
+            f=figure('Visible','Off');
+            h1=subplot(1,2,1);
+            i=sce.c_batch_id==1;
+            pkg.i_violinplot_groupordered(x(i),y(i));
+            [aabb]=ylim();
+            h2=subplot(1,2,2);
+            i=sce.c_batch_id==2;
+            pkg.i_violinplot_groupordered(x(i),y(i));
+            [ccdd]=ylim();
+            ylim(h1,[min(aabb(1),ccdd(1)) max(aabb(2),ccdd(2))]);
+            ylim(h2,[min(aabb(1),ccdd(1)) max(aabb(2),ccdd(2))]);
+            set(f,'Visible','On');
+        catch
+            disp('check C_BATCH_ID={1,2}');
+        end
     end
     end
 end
