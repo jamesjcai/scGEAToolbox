d7ead48ebb670c4b817c765f0fe6f40e97f10ec1
James Cai <jamescai@genomezoo.net>
2021-03-03 09:19:09 -0600

    Update installx.m

--- installx.m
+++ installx.m
@@ -1,8 +1,8 @@
-% tic
-% disp('Installing scTenifoldNet...')
-% unzip('https://github.com/cailab-tamu/scTenifoldNet/archive/master.zip');
-% addpath('./scTenifoldNet-master/MATLAB');
-% toc
+tic
+disp('Installing scTenifoldNet...')
+unzip('https://github.com/cailab-tamu/scTenifoldNet/archive/master.zip');
+addpath('./scTenifoldNet-master/MATLAB');
+toc
 
 tic
 disp('Installing scGEAToolbox...')
