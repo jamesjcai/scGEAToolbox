aed15a5e7228de608836be212616c96a5883259a
James Cai <jamescai@genomezoo.net>
2021-03-26 12:11:46 -0500

    Update i_stem3scatter.m

--- +pkg/i_stem3scatter.m
+++ +pkg/i_stem3scatter.m
@@ -1,9 +1,12 @@
 function i_stem3scatter(x,y,c,t)
 if nargin<4, t=''; end
-figure;
+hFig=figure;
     stem3(x,y,c,'marker','none','color','m');
     hold on
     scatter3(x,y,zeros(size(y)),5,c,'filled');
     title(t);
+    % hFig=gcf;
+    tb = uitoolbar(hFig);
+    gui.add_3dcamera(tb,'HgB_markers');
 end
     
