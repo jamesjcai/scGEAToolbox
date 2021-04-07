5f62903c20b8e7769c8095c4ee8568d6734b8c95
James Cai <jamescai@genomezoo.net>
2021-04-06 23:16:31 -0500

    Update callback_SaveX.m

--- +gui/callback_SaveX.m
+++ +gui/callback_SaveX.m
@@ -1,17 +1,28 @@
 function callback_SaveX(src,~)
-answer = questdlg('Export & save data?');
-if ~strcmp(answer,'Yes'), return; end     
-    FigureHandle=src.Parent.Parent;
-    sce=guidata(FigureHandle);
-    labels = {'Save SCE to variable named:',...
-        'Save SCE.X to variable named:',...
-        'Save SCE.g to variable named:',...
-        'Save SCE.S to variable named:'}; 
-    vars = {'sce','X','genelist','s'};
-    values = {sce,sce.X,sce.g,sce.s};
-    export2wsdlg(labels,vars,values,...
-        'Save Data to Workspace',...
-        logical([1 0 0 0]),{@smhelp});
+answer = questdlg('Export & save data to:','','Workspace','MAT file','Cancel','Workspace');
+        FigureHandle=src.Parent.Parent;
+        sce=guidata(FigureHandle);
+switch answer
+    case 'Workspace'
+        labels = {'Save SCE to variable named:',...
+            'Save SCE.X to variable named:',...
+            'Save SCE.g to variable named:',...
+            'Save SCE.S to variable named:'}; 
+        vars = {'sce','X','genelist','s'};
+        values = {sce,sce.X,sce.g,sce.s};
+        export2wsdlg(labels,vars,values,...
+            'Save Data to Workspace',...
+            logical([1 0 0 0]),{@smhelp});
+    case 'MAT file'
+        [filen, pathn] = uiputfile( ...
+           {'*.mat','*.*'},'Save as');
+
+        filename=[pathn,filen];
+        if ~(filename), return; end
+        save(filename,'sce');        
+    otherwise
+        return;
+end
          
     function smhelp
         helpdlg({'Select one or both check boxes.',...
