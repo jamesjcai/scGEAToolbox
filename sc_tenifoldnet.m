2f03f75e595c4e69285545534ff8c3237459231b
James Cai <jamescai@genomezoo.net>
2021-05-03 10:42:51 -0500

    Update sc_tenifoldnet.m

--- sc_tenifoldnet.m
+++ sc_tenifoldnet.m
@@ -1,11 +1,11 @@
-function [T,glist,Tf,OUT]=sc_tenifoldnet(X0,X1,genelist)
+function [T,Tf,OUT]=sc_tenifoldnet(X0,X1,genelist)
 
 [T,A0,A1]=sctenifoldnet_m(X0,X1,genelist);
 glist=T.genelist(T.pAdjusted<0.05);
 run.Enrichr(glist);
 
 Tf=e_fgsearun(T);
-OUT=e_fgseanet(Tf,"JaccardCutoff",0.5,"PlotNetwork",false);
+OUT=e_fgseanet(Tf,"JaccardCutoff",0.5,"PlotNetwork",false,"ShowNotepad",false);
 
 g=OUT{1,1};
 [A0sml,A1sml,glst]=gui.i_extractnetworks2compare(A0,A1,genelist,g);
