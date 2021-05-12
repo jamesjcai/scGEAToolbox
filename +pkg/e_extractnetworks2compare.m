a8760471cda9dc0ca6ef3f76f22fbaaea7d2b70a
James Cai <jamescai@genomezoo.net>
2021-05-12 15:57:28 -0500

    Update e_extractnetworks2compare.m

--- +pkg/e_extractnetworks2compare.m
+++ +pkg/e_extractnetworks2compare.m
@@ -2,9 +2,17 @@ function [A0,A1,glist]=e_extractnetworks2compare(A0,A1,genelist,glist)
 if nargin<4
     [glist]=gui.i_selectngenes(genelist);
 end
+
+glist=glist(:);
+genelist=genelist(:);
+
 [y,idx]=ismember(glist,genelist);
-if any(~y), error('xx'); end
+if any(~y), warning('Some genes in GLIST not found in GENELIST'); end
+assert(isequal(genelist(idx(y)),glist(y)))
+
+idx=idx(y);
 A0=A0(idx,idx);
 A1=A1(idx,idx);
 glist=glist(y);
+
 end
