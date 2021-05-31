320d84ad86402ce11f7d5cbb164f19334a61ad8c
James Cai <jamescai@genomezoo.net>
2021-05-30 22:38:58 -0500

    Update sc_qcfilter.m

--- sc_qcfilter.m
+++ sc_qcfilter.m
@@ -1,8 +1,8 @@
 function [X,genelist,keptidxv]=sc_qcfilter(X,genelist,libsize,mtratio,dropout,min_cells_nonzero,removemtgenes)
 
 if nargin<7, removemtgenes=false; end
-if nargin<6 || isempty(min_cells_nonzero), min_cells_nonzero=0.01; end
-if nargin<5 || isempty(dropout), dropout=0.01; end
+if nargin<6 || isempty(min_cells_nonzero), min_cells_nonzero=0.005; end
+if nargin<5 || isempty(dropout), dropout=1; end
 if nargin<4 || isempty(mtratio), mtratio=0.1; end
 if nargin<3 || isempty(libsize), libsize=1000; end
 
@@ -16,7 +16,7 @@ newsz=1;
 c=1;
 while ~isequal(oldsz,newsz)
     oldsz=size(X);
-    [X,genelist]=sc_filterg(X,genelist,dropout);
+    [X,genelist]=sc_filterg(X,genelist,dropout);   % remove empty genes
     [X,keptidx]=sc_filterc(X);
     keptidxv{end+1}=keptidx;
     [X,genelist]=sc_selectg(X,genelist,1,min_cells_nonzero);
