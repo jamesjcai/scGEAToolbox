b1a76ba65a5f1a459c368373fd02d9078121e70a
James Cai <jamescai@genomezoo.net>
2021-06-20 14:38:04 -0500

    .

--- +gui/i_viewtable.m
+++ +gui/i_viewtable.m
@@ -1,12 +1,14 @@
-function i_viewtable
+function i_viewtable(T)
+T.Properties.RowNames=T.Gene;
+T=T(:,2:end);
 
 % https://www.mathworks.com/matlabcentral/answers/254690-how-can-i-display-a-matlab-table-in-a-figure
 
-LastName = {'Smith';'Johnson';'Williams';'Jones';'Brown'};
-Age = [38;43;38;40;49];
-Height = [71;69;64;67;64];
-Weight = [176;163;131;133;119];
-T = table(Age,Height,Weight,'RowNames',LastName);
+% LastName = {'Smith';'Johnson';'Williams';'Jones';'Brown'};
+% Age = [38;43;38;40;49];
+% Height = [71;69;64;67;64];
+% Weight = [176;163;131;133;119];
+% T = table(Age,Height,Weight,'RowNames',LastName);
 
 hFigure = figure('Visible',false);
 set(hFigure, 'MenuBar', 'none');
--- sc_genestats.m
+++ sc_genestats.m
@@ -1,11 +1,15 @@
 function [T]=sc_genestats(X,genelist)
 
-if nargin<2, genelist=string(1:size(X,1))'; end
+if isa(X, 'SingleCellExperiment')
+    genelist=X.g;
+    X=X.X;
+else
+    if nargin<2, genelist=string(1:size(X,1))'; end
+end
 
 dropr=1-sum(X>0,2)./size(X,2);
 u=nanmean(X,2);
 cv=nanstd(X,[],2)./u;
 T=table(genelist,u,cv,dropr);
 T.Properties.VariableNames={'Gene','Mean','CV','Dropout_rate'};
-% fig = uifigure;
-% uit = uitable(fig,'Data',T);
+% gui.i_viewtable(T);
