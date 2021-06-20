8680c733df9b760ab5a5b8faacc1393d92c093ad
James Cai <jamescai@genomezoo.net>
2021-06-20 10:25:04 -0500

    Update i_viewtable.m

--- +gui/i_viewtable.m
+++ +gui/i_viewtable.m
@@ -1,4 +1,4 @@
-function i_viewtable(T)
+function i_viewtable
 
 % https://www.mathworks.com/matlabcentral/answers/254690-how-can-i-display-a-matlab-table-in-a-figure
 
@@ -8,5 +8,13 @@ Height = [71;69;64;67;64];
 Weight = [176;163;131;133;119];
 T = table(Age,Height,Weight,'RowNames',LastName);
 
-uitable('Data',T{:,:},'ColumnName',T.Properties.VariableNames,...
-    'RowName',T.Properties.RowNames,'Units', 'Normalized', 'Position',[0, 0, 1, 1]);
+hFigure = figure('Visible',false);
+set(hFigure, 'MenuBar', 'none');
+set(hFigure, 'ToolBar', 'none');
+uitable(hFigure,'Data',T{:,:},'ColumnName',T.Properties.VariableNames,...
+    'RowName',T.Properties.RowNames,'Units',...
+    'Normalized', 'Position',[0, 0, 1, 1]);
+movegui(hFigure, 'center');
+set(hFigure, 'visible', 'on');
+
+
