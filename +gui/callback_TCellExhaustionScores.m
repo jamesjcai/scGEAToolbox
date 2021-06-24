4b5bda5fdb962f2ef3e9e01b9c276e5052a31c61
James Cai <jamescai@genomezoo.net>
2021-06-24 14:05:41 -0500

    Update callback_TCellExhaustionScores.m

--- +gui/callback_TCellExhaustionScores.m
+++ +gui/callback_TCellExhaustionScores.m
@@ -1,7 +1,10 @@
 function callback_TCellExhaustionScores(src,~)
     FigureHandle=src.Parent.Parent;
     sce=guidata(FigureHandle);
-    cs=sc_cellscore(sce.X,sce.g);
+    % Pd1=pdcd1 tim3=HAVCR2, tcf1=HNF1A  https://www.nature.com/articles/s41577-019-0221-9    
+    posg=["PDCD1","HNF1A","HAVCR2","KLRG1","CD44","LY6C","CTLA","ICOS","LAG3"];
+    negg=["HAVCR2"];
+    cs=sc_cellscore(sce.X,sce.g,posg);
     figure;
     gui.i_stemscatter(sce.s,cs);
     zlabel('Exhaustion Score')
