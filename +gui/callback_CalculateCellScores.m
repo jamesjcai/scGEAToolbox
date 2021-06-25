dc05afbd8dc4d3cdc64ff70af458b6db2d090457
James Cai <jamescai@genomezoo.net>
2021-06-25 12:57:35 -0500

    .

---
+++ +gui/callback_CalculateCellScores.m
@@ -0,0 +1,20 @@
+function callback_CalculateCellScores(src,~)
+    FigureHandle=src.Parent.Parent;
+    sce=guidata(FigureHandle);
+     
+    % Pd1=pdcd1 tim3=HAVCR2, tcf1=HNF1A  https://www.nature.com/articles/s41577-019-0221-9    
+    posgcandidates=["PDCD1","HNF1A","HAVCR2","KLRG1","CD44","LY6C","CTLA","ICOS","LAG3"];
+    [posg]=gui.i_selectngenes(sce.g,posgcandidates);
+    if isempty(posg)
+        return;
+    end
+    cs=sc_cellscore(sce.X,sce.g,posg);
+    figure;
+    gui.i_stemscatter(sce.s,cs);
+    zlabel('Score Value')
+    title('Cell Score')
+            labels = {'Save score values to variable named:'}; 
+        vars = {'CellScores'};
+        values = {cs};
+        export2wsdlg(labels,vars,values);
+end
--- +gui/callback_TCellExhaustionScores.m
+++ +gui/callback_TCellExhaustionScores.m
@@ -1,21 +1,13 @@
 function callback_TCellExhaustionScores(src,~)
     FigureHandle=src.Parent.Parent;
     sce=guidata(FigureHandle);
-    
-       
-    % Pd1=pdcd1 tim3=HAVCR2, tcf1=HNF1A  https://www.nature.com/articles/s41577-019-0221-9    
-    posgcandidates=["PDCD1","HNF1A","HAVCR2","KLRG1","CD44","LY6C","CTLA","ICOS","LAG3"];
-    [posg]=gui.i_selectngenes(sce.g,posgcandidates);
-    if isempty(posg)
-        return;
-    end
-    %negg=["HAVCR2"];
-    cs=sc_cellscore(sce.X,sce.g,posg);
+
+    cs=pkg.e_cellscores(sce.X,sce.g,"T_Cell_Exhaustion");
     figure;
     gui.i_stemscatter(sce.s,cs);
-    zlabel('Exhaustion Score')
+    zlabel('Score Value')
     title('T Cell Exhaustion Score')
-            labels = {'Save values to variable named:'}; 
+            labels = {'Save score values to variable named:'}; 
         vars = {'TCellExhaustionScores'};
         values = {cs};
         export2wsdlg(labels,vars,values);
