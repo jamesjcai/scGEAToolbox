fac344729de52d8eb0b32e0d90b1719b3c7213fa
James Cai <jamescai@genomezoo.net>
2021-06-25 13:31:31 -0500

    Update e_cellscores.m

--- +pkg/e_cellscores.m
+++ +pkg/e_cellscores.m
@@ -2,8 +2,11 @@ function [score]=e_cellscores(X,genelist,type)
 if nargin<3, type="T_Cell_Exhaustion"; end
 switch type
     case "T_Cell_Exhaustion"
-        tgsPos=["CD44","LY6C","KLRG1","CTLA","ICOS","LAG3"];
-        tgsNeg=["IL2","TNF"];
+        tgsPos=["CD69","PDCD1","TGFB1","CTLA4","SPN","LAG3"];
+        %tgsPos=["CD44","LY6C","KLRG1","CTLA","ICOS","LAG3"];
+        tgsNeg="";
+    case "T_Cell_Cytotoxicity"
+        
     otherwise
         error('Undefined')
 end
