f13b121785801e75b35d9407fd2ce5d45e60359a
James Cai <jamescai@genomezoo.net>
2021-07-01 14:46:51 -0500

    .

---
+++ +run/UCell.m
@@ -0,0 +1,30 @@
+function [score]=UCell(X,genelist)
+%UCell: Robust and scalable single-cell gene signature scoring
+%UCell is an R package for calculating gene signatures
+%in single-cell datasets. UCell scores, based on the 
+%Mann-Whitney U statistic, are robust to dataset size 
+%and heterogeneity, and their calculation demands 
+%relatively less computing time and memory than other 
+%available methods, enabling the processing of large 
+%datasets (>10^5 cells). UCell can be applied to any
+%cell vs. gene data matrix, and includes functions to 
+%directly interact with Seurat objects.
+% https://github.com/carmonalab/UCell
+
+oldpth=pwd();
+[isok,msg]=commoncheck_R('R_UCell');
+if ~isok, error(msg); end
+if exist('input.txt','file'), delete('input.txt'); end
+if exist('output.csv','file'), delete('output.csv'); end
+sc_writefile('input.txt',X,upper(genelist));
+RunRcode('script.R');
+if exist('output.csv','file')
+    T=readtable('output.csv','ReadVariableNames',true);
+    score=T.Tcell_exhaustion_UCell;
+else
+    score=[];
+end
+if exist('input.txt','file'), delete('input.txt'); end
+if exist('output.csv','file'), delete('output.csv'); end
+cd(oldpth);
+end
