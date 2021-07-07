bd0fe8cef767d13cee0989db1ede3962b9292988
James Cai <jamescai@genomezoo.net>
2021-07-07 14:40:29 -0500

    Update decontX.m

--- +run/decontX.m
+++ +run/decontX.m
@@ -10,14 +10,14 @@ end
 oldpth=pwd();
 [isok,msg]=commoncheck_R('R_decontX');
 if ~isok, error(msg); end
-%if exist('./input.mat','file'), delete('./input.mat'); end
-%if exist('./output.mat','file'), delete('./output.mat'); end
+if exist('./input.mat','file'), delete('./input.mat'); end
+if exist('./output.mat','file'), delete('./output.mat'); end
 save('input.mat','X','-v6');
 RunRcode('script.R');
 if exist('./output.mat','file')
     load('output.mat','X','contamination')
 end
-%if exist('./input.mat','file'), delete('./input.mat'); end
-%if exist('./output.mat','file'), delete('./output.mat'); end
+if exist('./input.mat','file'), delete('./input.mat'); end
+if exist('./output.mat','file'), delete('./output.mat'); end
 cd(oldpth);
 end
