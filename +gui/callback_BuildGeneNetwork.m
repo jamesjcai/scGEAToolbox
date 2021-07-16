89303e7e1cf119eff67d3e24f892e00cac0f8f49
James Cai <jamescai@genomezoo.net>
2021-07-16 08:42:37 -0500

    .

--- +gui/callback_BuildGeneNetwork.m
+++ +gui/callback_BuildGeneNetwork.m
@@ -8,8 +8,12 @@ function callback_BuildGeneNetwork(src,~)
     [y,i]=ismember(glist,sce.g);
     if ~all(y), error('xxx'); end    
     fprintf("%s\n",glist)
-    fw=gui.gui_waitbar;    
-    x=sce.X(i,:);
+    fw=gui.gui_waitbar;
+    x=sce.X(i,:);    
+    %     y=sce.X(~ismember(sce.g,glist),:);
+    %     [~,y]=pca(y.');
+    %     y=y(:,1:10).';    
+    %     export2wsdlg({'y','x'},{'y','x'},{y,x})    
     x=sc_transform(x);
     A=sc_pcnet(x);
     gui.gui_waitbar(fw);            
