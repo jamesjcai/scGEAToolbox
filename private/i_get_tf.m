037911a3b9402b9193a73c17f2133341a8b98cd9
James Cai <jamescai@genomezoo.net>
2021-07-28 14:01:45 -0500

    .

--- private/i_get_tf.m
+++
@@ -1,7 +0,0 @@
-websave('Homo_sapiens_TF.txt','http://bioinfo.life.hust.edu.cn/static/AnimalTFDB3/download/Homo_sapiens_TF');
-T=readtable('Homo_sapiens_TF.txt');
-hs_tfgenes=string(T.Symbol);
-
-websave('Mus_musculus_TF.txt','http://bioinfo.life.hust.edu.cn/static/AnimalTFDB3/download/Mus_musculus_TF');
-T=readtable('Mus_musculus_TF.txt');
-mm_tfgenes=string(T.Symbol);
