442050861a16eaa8126d46a8f911cf543a65a4c7
James Cai <jamescai@genomezoo.net>
2021-07-29 11:56:09 -0500

    .

--- +run/thirdparty/R_RJcluster/script.R
+++ +run/thirdparty/R_RJcluster/script.R
@@ -2,5 +2,5 @@ library(RJcluster)
 
 X <- as.matrix(read.csv("input.csv", header = FALSE))
 #X <- as.matrix(read.table("input.csv", sep=","))
-res = RJclust(data = X)
+res = RJclust(data = X, scale = TRUE)
 write.csv(res$class,'output.csv')
