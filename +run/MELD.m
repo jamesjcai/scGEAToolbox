4d7085f41896175a452c7f153d4fec463e5f47ce
James Cai <jamescai@genomezoo.net>
2021-08-02 00:31:29 -0500

    .

--- +run/MELD.m
+++ +run/MELD.m
@@ -1,9 +1,9 @@
-function [sample_likelihoods,T]=MELD(X,batchid)
+function [sample_likelihoods,T]=MELD(X,batchid,usematinput)
 % MELD - a graph signal processing tool used to smooth a binary variable on 
 % the cell-cell graph to determine which regions of its underlying 
 % data manifold are enriched or depleted in cells with a specific 
 % feature.
-
+if nargin<3, usematinput=true; end
 if nargin<2
     %batchid=string([true(ceil(size(X,2)/2),1); false(floor(size(X,2)/2),1)]);
     error('USAGE: score=MELD(X,batchid)');
@@ -16,17 +16,26 @@ cd(wrkpth);
 if exist('./batchid.txt','file'), delete('./batchid.txt'); end
 if exist('./input.txt','file'), delete('./input.txt'); end
 if exist('./output.txt','file'), delete('./output.txt'); end
+if exist('./input.mat','file'), delete('./input.mat'); end
+
 
 X=sc_norm(X);
 X=sqrt(X);
 
-writematrix(X,'input.txt');
-
-writematrix(batchid,'batchid.txt');
+if usematinput
+    save('input.mat','X','batchid','-v7');
+else
+    writematrix(X,'input.txt');
+    writematrix(batchid,'batchid.txt');
+end
 
 x=pyenv;
 pkg.i_add_conda_python_path;
-cmdlinestr=sprintf('"%s" "%s%sscript.py"',x.Executable,wrkpth,filesep);
+if usematinput
+    cmdlinestr=sprintf('"%s" "%s%sscript.py"',x.Executable,wrkpth,filesep);
+else
+    cmdlinestr=sprintf('"%s" "%s%sscript_csv.py"',x.Executable,wrkpth,filesep);
+end
 disp(cmdlinestr)
 [status]=system(cmdlinestr);
 
@@ -41,5 +50,6 @@ end
 if exist('./batchid.txt','file'), delete('./batchid.txt'); end
 if exist('./input.txt','file'), delete('./input.txt'); end
 if exist('./output.txt','file'), delete('./output.txt'); end
+if exist('./input.mat','file'), delete('./input.mat'); end
 cd(oldpth);
 end
--- +run/thirdparty/MELD/script_csv.py
+++ +run/thirdparty/MELD/script_csv.py
@@ -4,19 +4,17 @@ dname = os.path.dirname(abspath)
 os.chdir(dname)
 # os.chdir("U:\\GitHub\\scGEAToolbox\\+run\\thirdparty\\MELD")
 # os.chdir("C:\\Users\\jcai.AUTH\\Documents\\GitHub\\scGEAToolbox\\+run\\thirdparty\\MELD")
-# https://www.blogforbrains.com/blog/2014/9/6/loading-matlab-mat-data-in-python
-import scipy.io as spio
+
+
 import pandas as pd
 import meld
 
+data=pd.read_csv("input.txt",header=None).values
+sample_labels=pd.read_csv("batchid.txt",header=None).values
 
-mat = spio.loadmat('input.mat', squeeze_me=True)
-data = mat['X'] # array
-sample_labels=mat['batchid']
 
 meld_op = meld.MELD()
 sample_densities = meld_op.fit_transform(data.T, sample_labels)
 sample_likelihoods = meld.utils.normalize_densities(sample_densities)
 pd.DataFrame(sample_likelihoods).to_csv('output.txt',index=False,header=True)
-# spio.savemat('output.mat',{"score": sample_likelihoods})
 
