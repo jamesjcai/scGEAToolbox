0f1af3785938e1a2dada978f1089a019a98f763e
James Cai <jamescai@genomezoo.net>
2021-08-25 18:47:27 -0500

    Update sc_qcfilter.m

--- sc_qcfilter.m
+++ sc_qcfilter.m
@@ -1,7 +1,7 @@
 function [X,genelist,keptidxv]=sc_qcfilter(X,genelist,libsize,mtratio,dropout,min_cells_nonzero,removemtgenes)
 %Basic QC filter
 if nargin<7, removemtgenes=false; end
-if nargin<6 || isempty(min_cells_nonzero), min_cells_nonzero=0.05; end
+if nargin<6 || isempty(min_cells_nonzero), min_cells_nonzero=0.01; end
 if nargin<5 || isempty(dropout), dropout=1; end
 if nargin<4 || isempty(mtratio), mtratio=0.10; end
 if nargin<3 || isempty(libsize), libsize=1000; end
