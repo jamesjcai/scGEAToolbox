966ef013bb0c661efa6370c38894fbcfece6511a
James Cai <jamescai@genomezoo.net>
2021-11-06 15:40:39 -0500

    Update callback_scTenifoldKnkN.m

--- +gui/callback_scTenifoldKnkN.m
+++ +gui/callback_scTenifoldKnkN.m
@@ -24,15 +24,24 @@ function callback_scTenifoldKnkN(src,~)
         case 'Use existing'
             a=evalin('base','whos');
             b=struct2cell(a);
-            %valididx=ismember(b(4,:),'double');
-            %a=a(valididx);
-            [indx,tf]=listdlg('PromptString',{'Select network variable:'},...
-                'liststring',b(1,:),'SelectionMode','single');
-            if tf==1
-                A0 = evalin('base',a(indx).name);
-            else
+            valididx=ismember(b(4,:),'double');
+            b1=b(1,:);
+            b2=b(2,:); targetsiz=[length(sce.g) length(sce.g)];
+            for k=1:length(valididx)
+                if valididx(k)
+                   valididx(k)=isequal(b2{k},targetsiz);
+                end
+            end            
+            if ~any(valididx)
+                warndlg('No valid network.');                
                 return;
             end
+            [indx,tf]=listdlg('PromptString',...
+                {'Select network:'},...
+                'liststring',b1(valididx),...
+                'SelectionMode','single');
+            if tf~=1, return; end
+            [A0]=evalin('base',a(indx).name);
             [m,n]=size(A0);
             if m~=n || n~=length(sce.g)
                 errordlg('Not a valid network.'); 
@@ -49,60 +58,40 @@ function callback_scTenifoldKnkN(src,~)
         otherwise
             return;
     end
-    
-%     gsorted=sort(sce.g);
-%     [indx2,tf] = listdlg('PromptString',{'Select the KO gene:'},...
-%         'SelectionMode','single','ListString',gsorted);
-%     if tf==1
-%         [~,idx]=ismember(gsorted(indx2),sce.g);
-%     else
-%         return;
-%     end
-    
-%     if isempty(A0)
-%         answer=questdlg(sprintf('Ready to construct network and then knock out gene #%d (%s). Continue?',...
-%                idx,sce.g(idx)));
-%     else
-%         answer=questdlg(sprintf('Ready to knock out gene #%d (%s) from network (%s). Continue?',...
-%                idx,sce.g(idx),a(indx).name));
-%     end
-%     
-%     if ~strcmpi(answer,'Yes'), return; end
 
-        if isempty(A0)
-            try
-                fw = gui.gui_waitbar;
-                [A0]=sc_pcnetdenoised(sce.X);
-                [F]=ten.knk3_buildPerturbationLandscape(A0,sce.g);
-                gui.gui_waitbar(fw);
-             catch ME
-                gui.gui_waitbar(fw);
-                errordlg(ME.message);
-                return;
-            end
-            isreconstructed=true;
-        else
-%                try
-                    fw = gui.gui_waitbar;                    
-                    [F]=ten.knk3_buildPerturbationLandscape(A0,sce.g);
-                    gui.gui_waitbar(fw);
-%                 catch ME
-%                     gui.gui_waitbar(fw);
-%                     errordlg(ME.message);
-%                     return;
-%                 end
-            %A1=A0;
-            %A1(idx,:)=0;
-            %[aln0,aln1]=i_ma(A0,A1);
-            %T=i_dr(aln0,aln1,sce.g,true);
-            isreconstructed=false;
+   answer=questdlg('This analysis may take several hours. Continue?');
+    if ~strcmpi(answer,'Yes'), return; end
+    
+    
+    if isempty(A0)
+        try
+            fw = gui.gui_waitbar;
+            [A0]=sc_pcnetdenoised(sce.X);
+            [F]=ten.knk3_buildPerturbationLandscape(A0,sce.g);
+            gui.gui_waitbar(fw);
+         catch ME
+            gui.gui_waitbar(fw);
+            errordlg(ME.message);
+            return;
         end
+        isreconstructed=true;
+    else
+        try
+            fw = gui.gui_waitbar;                    
+            [F]=ten.knk3_buildPerturbationLandscape(A0,sce.g);
+            gui.gui_waitbar(fw);
+        catch ME
+            gui.gui_waitbar(fw);
+            errordlg(ME.message);
+            return;
+        end
+        isreconstructed=false;
+    end
         
     if isreconstructed
         labels = {'Save network to variable named:',...
             'Save perturbation score to variable named:',...
             'Save gene list to variable named:'}; 
-
         vars = {'A0','F','g'};
         values = {A0,F,sce.g};
     else
