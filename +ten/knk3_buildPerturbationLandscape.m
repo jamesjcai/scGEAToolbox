2f665567ede1256e86b95574f09f4545910b5c96
James Cai <jamescai@genomezoo.net>
2021-11-09 21:35:36 -0600

    Update knk3_buildPerturbationLandscape.m

--- +ten/knk3_buildPerturbationLandscape.m
+++ +ten/knk3_buildPerturbationLandscape.m
@@ -1,4 +1,5 @@
-function [F]=knk3_buildPerturbationLandscape(A,genelist,oldF,a,b)
+function [F]=knk3_buildPerturbationLandscape(A,genelist,oldF,a,b,savetmp)
+if nargin<6, savetmp=false; end
 if nargin<5, b=size(A,1); end
 if nargin<4, a=1; end
 if nargin<3, oldF=[]; end
@@ -11,7 +12,7 @@ else
     F=zeros(n,n);
 end
 tmpmat=tempname;
-    fprintf('Temporary result (tmpF) will be saved in %s.mat\n',tmpmat);
+    % fprintf('Temporary result (tmpF) will be saved in %s.mat\n',tmpmat);
     a=min([a n]); b=min([b n]);
     for k=a:b
         fprintf('%s ... gene %d of %d\n',genelist(k),k,n);
@@ -19,6 +20,7 @@ tmpmat=tempname;
             [t]=ten.knk2_knockoutTargetGene(A,genelist(k),genelist,false);
             F(:,k)=t.drdist;
         end
+        if savetmp
         if mod(k,100)==0
             try
                 tmpF=F;
@@ -27,11 +29,14 @@ tmpmat=tempname;
             catch
             end
         end
+        end
     end
+    if savetmp
     try
         tmpF=F;
         fprintf('Saving temporary result (tmpF) to %s.mat\n',tmpmat);  
         save(tmpmat,'tmpF','genelist');
     catch
+    end
     end
 end
