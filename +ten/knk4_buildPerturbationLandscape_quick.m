fd71d562c571f1522de237aafefd94f5dc0f43de
James Cai <jamescai@genomezoo.net>
2021-11-15 21:39:56 -0600

    Update knk4_buildPerturbationLandscape_quick.m

--- +ten/knk4_buildPerturbationLandscape_quick.m
+++ +ten/knk4_buildPerturbationLandscape_quick.m
@@ -12,42 +12,37 @@ else
     F=zeros(n,n);
 end
 
-ndim=30;
-mu=0.9;
-W1=A+1;
-W2=W1;
-assert(n==size(W1,2));
-W12=eye(size(W1,2),size(W2,2));
-mu = mu*(sum(W1(:))+sum(W2(:)))/(2*sum(W12(:)));
-W = [W1 mu*W12; mu*W12' W2];
+    ndim=30;
+    mu=0.9;
+    W1=A+1;
+    W2=W1;
+    assert(n==size(W1,2));
+    W12=eye(size(W1,2),size(W2,2));
+    mu = mu*(sum(W1(:))+sum(W2(:)))/(2*sum(W12(:)));
+    W = [W1 mu*W12; mu*W12' W2];
 
-    % fprintf('Temporary result (tmpF) will be saved in %s.mat\n',tmpmat);
     a=min([a n]); b=min([b n]);
     for k=a:b
-        tic;
         fprintf('%s ... gene %d of %d\n',genelist(k),k,n);
         if all(F(:,k)==0)
-           if k>1
-               W(n+k-1,n+1:end)=W(k-1,1:n);
-           end
-               W(n+k,n+1:end)=1;
-               D=sum(abs(W));
-                L=diag(D)-W;
-                [V,D] = eigs(L,ndim*2,'smallestreal');
-                d=diag(D);
-                % [V,d] = eig(L,'vector');
-                [d,ind] = sort(d);
-                V=V(:,ind);
-                V=V(:,d>=1e-8);
-                V=V(:,1:ndim);
-
-                assert(n==size(W1,1))
-                aln0=V(1:n,:);
-                aln1=V(n+1:end,:);
-                drdist=vecnorm(aln0-aln1,2,2).^2;
-                F(:,k)=drdist./norm(drdist);
+            tic;
+            if k>1
+                W(n+k-1,n+1:end)=W(k-1,1:n);
+            end
+            W(n+k,n+1:end)=1;
+            D=sum(abs(W));
+            L=diag(D)-W;
+            [V,D] = eigs(L,ndim*2,'smallestreal');
+            d=diag(D);
+            [d,ind] = sort(d);
+            V=V(:,ind);
+            V=V(:,d>=1e-8);
+            V=V(:,1:ndim);
+            aln0=V(1:n,:);
+            aln1=V(n+1:end,:);
+            drdist=vecnorm(aln0-aln1,2,2).^2;
+            F(:,k)=drdist./norm(drdist);
+            toc;
         end
-toc;
     end
-
 end
