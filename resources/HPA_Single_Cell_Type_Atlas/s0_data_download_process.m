fb8fc46d9653124f9c7af1c2d993a67b8f954d06
James Cai <jamescai@genomezoo.net>
2021-12-27 12:02:33 -0600

    Update s0_data_download_process.m

--- resources/HPA_Single_Cell_Type_Atlas/s0_data_download_process.m
+++ resources/HPA_Single_Cell_Type_Atlas/s0_data_download_process.m
@@ -1,3 +1,4 @@
+% ref: https://www.proteinatlas.org/news/2021-07-28/a-single-cell-type-map-of-human-tissues
 urltxt='https://www.proteinatlas.org/download/rna_single_cell_type.tsv.zip';
 fn=unzip(urltxt);
 T=readtable(fn{1},'FileType','text');
