83e4bc95cc754633f0cef5bb3c1eb23af2680bae
James Cai <jamescai@genomezoo.net>
2022-01-13 16:50:58 -0600

    Update sc_scatter.m

--- sc_scatter.m
+++ sc_scatter.m
@@ -271,7 +271,16 @@ promotesave=true;
                 pw1=fileparts(mfilename('fullpath'));
                 fprintf('Loading SCE Data File example_data/testSce.mat...');
                 tic;
+                if ~exist(fullfile(pw1,'example_data/'),'dir')
+                    olddir=pwd();
+                    cdgea;
+                    cd(olddir);
+                end
                 file1=fullfile(pw1,'example_data','testSce.mat');
+                if ~exist(file1,"file"), 
+                    errordlg("Example data file does not exist.");
+                    return;
+                end
                 load(file1,'sce');
                 if strcmp(answerstruced,'Raw')
                     sce=SingleCellExperiment(sce.X,sce.g);
