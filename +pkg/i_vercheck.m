bfef739f986a0feeeeec08bd5c6b4c7aa98f492c
James Cai <jamescai@genomezoo.net>
2022-01-19 15:38:01 -0600

    Update i_vercheck.m

--- +pkg/i_vercheck.m
+++ +pkg/i_vercheck.m
@@ -1,4 +1,4 @@
-function [needupdate,v1,v2]=i_vercheck
+function [needupdate,v1local,v2web]=i_vercheck
 needupdate=false;
 olddir=pwd();
 cdgea;
@@ -13,15 +13,15 @@ a = C{1};
     x=a(contains(a,'<name>'));
     a1=strfind(x,'<name>');
     a2=strfind(x,'</name>');
-    v1=extractBetween(x,a1{1}+length('<name>'),a2{1}-1);
+    v1local=extractBetween(x,a1{1}+length('<name>'),a2{1}-1);
 
-    url='https://raw.githubusercontent.com/jamesjcai/scGEAToolbox/master/info.xml';
+    url='https://raw.githubusercontent.com/jamesjcai/scGEAToolbox/main/info.xml';
     a=webread(url);
     a=strsplit(a,'\n')';
     x=a(contains(a,'<name>'));
     a1=strfind(x,'<name>');
     a2=strfind(x,'</name>');
-    v2=extractBetween(x,a1{1}+length('<name>'),a2{1}-1);
+    v2web=extractBetween(x,a1{1}+length('<name>'),a2{1}-1);
 
     %{
     a=textread('scGEAToolbox.prj','%s');
@@ -39,11 +39,11 @@ a = C{1};
     a2=strfind(x,'</param.version>');
     v2=extractBetween(x,a1{1}+15,a2{1}-1);
     %}
-    needupdate=~isequal(v1,v2);
+    needupdate=~isequal(v1local,v2web);
 catch
    
 end
-if nargout>1, v1=strrep(v1{1},'scGEAToolbox ',''); end
-if nargout>2, v2=strrep(v2{1},'scGEAToolbox ',''); end
+if nargout>1, v1local=strrep(v1local{1},'scGEAToolbox ',''); end
+if nargout>2, v2web=strrep(v2web{1},'scGEAToolbox ',''); end
 cd(olddir);
 end
