78e36652d1c9c6799ab8fcc7d592a487a868402c
James Cai <jamescai@genomezoo.net>
2022-01-23 10:21:27 -0600

    Update callback_SelectCellsByClass.m

--- +gui/callback_SelectCellsByClass.m
+++ +gui/callback_SelectCellsByClass.m
@@ -5,12 +5,23 @@ function callback_SelectCellsByClass(src,~)
     if isempty(thisc), return; end
     [ci,cLi]=grp2idx(thisc);
 
-    [indxx,tfx] = listdlg('PromptString',{'Select groups',...
-    '',''},'SelectionMode','multiple','ListString',string(cLi));
+    [indxx,tfx] = listdlg('PromptString',{'Select groups'},...
+        'SelectionMode','multiple','ListString',string(cLi));
     if tfx==1
         [ax,bx]=view();
-        fw=gui.gui_waitbar;
         idx=ismember(ci,indxx);
+
+        answer = questdlg('Select or unselect?','','Select', 'Unselect',...
+                          'Cancel', 'Select');
+        if strcmp(answer, 'Select')
+              % do nothing
+        elseif strcmp(answer, 'Unselect')            
+            idx=~idx;
+        else
+            return;
+        end        
+        fw=gui.gui_waitbar;
+
         scex=selectcells(sce,idx);
         % scex.c=cLi(ci(idx));
         scex.c=ci(idx);
