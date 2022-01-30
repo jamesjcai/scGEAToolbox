beeefcb708402445b6a44b4fa52652e64d9d248a
James Cai <jamescai@genomezoo.net>
2022-01-30 16:04:44 -0600

    .

--- +gui/callback_MultiGroupingViewer.m
+++ +gui/callback_MultiGroupingViewer.m
@@ -2,7 +2,7 @@ function callback_MultiGroupingViewer(src,~)
     FigureHandle=src.Parent.Parent;
     sce=guidata(FigureHandle);
         
-    [thisc1,clable1,thisc2,clable2]=gui.i_select2class(sce);
+    [thisc1,clable1,thisc2,clable2]=gui.i_select2state(sce);
      if isempty(thisc1) || isempty(thisc2)
          return;
      end
