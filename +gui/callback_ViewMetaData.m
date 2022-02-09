67ad722ab17c9fddef34113165562925b3242914
James Cai <jamescai@genomezoo.net>
2022-02-08 18:11:45 -0600

    Update callback_ViewMetaData.m

--- +gui/callback_ViewMetaData.m
+++ +gui/callback_ViewMetaData.m
@@ -1,5 +1,15 @@
 function callback_ViewMetaData(src,~)
     FigureHandle=src.Parent.Parent;
     sce=guidata(FigureHandle);
-    inputdlg('Data Info:','Metadata Viewer',[10 50],{char(sce.metadata)});
+    a=inputdlg('Data Info:','Metadata Viewer',[10 50],{char(sce.metadata)});
+    if ~isempty(a)
+        newmetadata=strtrim(string(a{1}));
+        if ~isequal(newmetadata,sce.metadata)
+            answer=questdlg('Save metadata?','');
+            if strcmp(answer,'Yes')
+                sce.metadata=newmetadata;
+                guidata(FigureHandle,sce);
+            end
+        end
+    end
 end
