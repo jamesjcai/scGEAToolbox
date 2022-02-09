f4ab969b166f9b1ae5599b26bb9c4874b02ee71d
James Cai <jamescai@genomezoo.net>
2022-02-08 22:09:20 -0600

    Update e_processDETable.m

--- +pkg/e_processDETable.m
+++ +pkg/e_processDETable.m
@@ -1,8 +1,8 @@
 function [Tup,Tdn]=e_processDETable(T)
 
     isok=(abs(T.pct_2-T.pct_1)>0.05 | abs(T.avg_log2FC)>1.0)&T.p_val_adj<0.01;
-    Tup=T(T.avg_log2FC>0 & isok,:);
-    Tdn=T(T.avg_log2FC<0 & isok,:);
+    Tup=T(T.avg_log2FC < 0 & isok,:);
+    Tdn=T(T.avg_log2FC > 0 & isok,:);
     
     Tup = sortrows(Tup,'abs_log2FC','descend');
     Tup = sortrows(Tup,'p_val_adj','ascend');
