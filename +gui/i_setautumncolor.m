b0cc38cd4ad1bf4b134c268b21a2336bfe56b0bc
James Cai <jamescai@genomezoo.net>
2022-02-20 17:51:48 -0600

    .

--- +gui/i_setautumncolor.m
+++ +gui/i_setautumncolor.m
@@ -1,5 +1,6 @@
-function i_setautumncolor(c)
-    a = colormap('autumn');
+function i_setautumncolor(c,cmapname)
+if nargin<2, cmapname='autumn'; end
+    a = colormap(cmapname);
     a(1, :) = [.8 .8 .8];
     if numel(unique(c)) == 1
         for kk = 1:size(a, 1)
--- +gui/i_sortgenenames.m
+++ +gui/i_sortgenenames.m
@@ -7,11 +7,11 @@ function [gsorted]=i_sortgenenames(sce)
                 gsorted=sort(sce.g);
             case 'Average Expression'
                 [T]=sc_genestats(sce.X,sce.g);
-                [~,idx]=sort(T.Dropout_rate);
+                [~,idx]=sort(T.Mean,'descend');
                 gsorted=sce.g(idx);                
             case '% of Nonzero Cells'
                 [T]=sc_genestats(sce.X,sce.g);
-                [~,idx]=sort(T.Dropout_rate);
+                [~,idx]=sort(T.Dropout_rate,'ascend');
                 gsorted=sce.g(idx);
             otherwise
                 return;
