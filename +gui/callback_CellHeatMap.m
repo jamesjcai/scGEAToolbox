20505a1716272ffddaab74f8a9dec464d2bd0990
James Cai <jamescai@genomezoo.net>
2022-03-09 07:20:42 -0600

    .

--- +gui/callback_CellHeatMap.m
+++ +gui/callback_CellHeatMap.m
@@ -13,12 +13,36 @@ function callback_CellHeatMap(src,~)
 %         return;
 % end
 
-f=figure;
+
+
+hFigure=figure;
+        UitoolbarHandle = uitoolbar('Parent', hFigure);
+
+pkg.i_addbutton2fig(UitoolbarHandle)
 [~,idx]=sort(sce.c);
 X=sce.X(:,idx);
 
 imagesc(log10(1+log10(1+X(1:1000,:))));
 xlabel("Genes")
 ylabel("Cells")
+
+
+    function i_changec(~,~)
+            [thisc,clable,~,newpickclable]=gui.i_select1state(sce);
+    
+    if strcmp(clable,'Cell Cycle Phase')
+        if length(unique(thisc))>1
+            sce.c_cell_cycle_tx=thisc;
+        end
+    end
+    if isempty(thisc), return; end
+        if strcmp(clable,'Customized C...')
+            clable=gui.i_renamec(clable,sce,newpickclable);
+            sce.list_cell_attributes=[sce.list_cell_attributes,clable];
+            sce.list_cell_attributes=[sce.list_cell_attributes,thisc];
+        end
+        [c,cL]=grp2idx(thisc);        
+        sce.c=c;
+    end
 
 end
---
+++ +pkg/i_addbutton2fig.m
@@ -0,0 +1,18 @@
+function i_addbutton2fig(toolbarHdl,sepTag,callbackFnc,imgFil,tooltipTxt)
+mfolder = fileparts(mfilename('fullpath'));
+
+if nargin<2 || isempty(sepTag), sepTag='off'; end
+if nargin<3 || isempty(callbackFnc), callbackFnc="version"; end
+if nargin<4 || isempty(imgFil), imgFil='list.gif'; end
+if nargin<5 || isempty(tooltipTxt), tooltipTxt="Test"; end
+
+    if ischar(callbackFnc) || isstring(callbackFnc)
+        callbackFnc=str2func(callbackFnc);
+    end
+
+pt = uipushtool(toolbarHdl, 'Separator', sepTag);
+        [img, map] = imread(fullfile(mfolder,'..', 'resources', imgFil));
+        ptImage = ind2rgb(img, map);
+        pt.CData = ptImage;
+        pt.Tooltip = tooltipTxt;
+        pt.ClickedCallback = callbackFnc;
