187a4a34593d7907338b33ff7331d28cce1e5761
James Cai <jamescai@genomezoo.net>
2022-03-25 16:42:04 -0500

    Create i_confirmscript.m

---
+++ +gui/i_confirmscript.m
@@ -0,0 +1,42 @@
+function [t]=i_confirmscript(qtxt,stxt,langtag)
+
+t=false;
+if nargin<1, qtxt='Run pseudotime analysis (Monocle)?'; end
+if nargin<2, stxt='R_monocle'; end
+if nargin<3, langtag='R'; end
+
+switch langtag
+    case 'R'
+        scriptfile='script.R';
+    case 'python'
+        scriptfile='script.py';
+    otherwise
+        error('Unknown language tag.');
+end
+
+        answer = questdlg(qtxt,'', ...
+            'Yes','Review R Script','Cancel','Yes');
+        switch answer
+            case 'Cancel'
+                return;
+            case 'Yes'
+                t=true;
+             case 'Review R Script'
+                folder=fileparts(mfilename('fullpath'));
+                scriptfile=fullfile(folder,'..','+run','external', ...
+                    stxt,scriptfile);
+                ts=fileread(scriptfile);
+                %LF=char(10);
+                CR=char(13);  %  carriage return character equivalent to char(13) or sprintf('\r').
+                ts=strrep(ts,[CR newline],newline);
+                a=inputdlg('Review Script:','R Code',[10 90],{ts});
+                if isempty(a)
+                    return; 
+                else
+                    t=true;
+                end
+            otherwise
+                return;
+        end
+
+end
