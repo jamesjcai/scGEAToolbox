cec3afcd7275a4659fe44e8eb55b2dd457f44df7
James Cai <jamescai@genomezoo.net>
2022-04-07 13:12:17 -0500

    .

---
+++ +gui/callback_RenameCells.m
@@ -0,0 +1,29 @@
+function [requirerefresh]=callback_RenameCells(src)
+    requirerefresh=false;
+    FigureHandle=src.Parent.Parent;
+    sce=guidata(FigureHandle);
+
+    if isempty(sce.c_cell_type_tx)
+        errordlg('sce.c_cell_type_tx undefined');
+        return;
+    end
+    answer = questdlg('Rename a cell type?');
+    if ~strcmp(answer, 'Yes'), return; end
+    [ci, cLi] = grp2idx(sce.c_cell_type_tx);
+    [indxx, tfx] = listdlg('PromptString',...
+        {'Select cell type'},...
+        'SelectionMode', 'single',...
+        'ListString', string(cLi));
+    if tfx == 1
+        i = ismember(ci, indxx);
+        newctype = inputdlg('New cell type', 'Rename', [1 50], cLi(ci(i)));
+        if ~isempty(newctype)
+            cLi(ci(i)) = newctype;
+            sce.c_cell_type_tx = string(cLi(ci));
+            requirerefresh=true;
+            %[c, cL] = grp2idx(sce.c_cell_type_tx);
+            %i_labelclusters(false);
+        end
+    end
+    guidata(FigureHandle, sce);
+end
--- sc_scatter_sce.m
+++ sc_scatter_sce.m
@@ -548,30 +548,40 @@ end
         colormap(para.oldColorMap);
     end
 
-    function RenameCellType(~, ~)
-        if isempty(sce.c_cell_type_tx)
-            errordlg('sce.c_cell_type_tx undefined');
-            return;
+    function RenameCellType(src,~)
+        [requirerefresh]=gui.callback_RenameCells(src);
+        if requirerefresh
+            sce = guidata(FigureHandle);
+            [c, cL] = grp2idx(sce.c_cell_type_tx);
+            i_labelclusters(false);
         end
-        answer = questdlg('Rename a cell type?');
-        if ~strcmp(answer, 'Yes'), return; end
-        [ci, cLi] = grp2idx(sce.c_cell_type_tx);
-        [indxx, tfx] = listdlg('PromptString',...
-            {'Select cell type'},...
-            'SelectionMode', 'single',...
-            'ListString', string(cLi));
-        if tfx == 1
-            i = ismember(ci, indxx);
-            newctype = inputdlg('New cell type', 'Rename', [1 50], cLi(ci(i)));
-            if ~isempty(newctype)
-                cLi(ci(i)) = newctype;
-                sce.c_cell_type_tx = string(cLi(ci));
-                [c, cL] = grp2idx(sce.c_cell_type_tx);
-                i_labelclusters(false);
-            end
-        end
-        guidata(FigureHandle, sce);
     end
+
+
+%     function RenameCellTypex(~, ~)
+%         if isempty(sce.c_cell_type_tx)
+%             errordlg('sce.c_cell_type_tx undefined');
+%             return;
+%         end
+%         answer = questdlg('Rename a cell type?');
+%         if ~strcmp(answer, 'Yes'), return; end
+%         [ci, cLi] = grp2idx(sce.c_cell_type_tx);
+%         [indxx, tfx] = listdlg('PromptString',...
+%             {'Select cell type'},...
+%             'SelectionMode', 'single',...
+%             'ListString', string(cLi));
+%         if tfx == 1
+%             i = ismember(ci, indxx);
+%             newctype = inputdlg('New cell type', 'Rename', [1 50], cLi(ci(i)));
+%             if ~isempty(newctype)
+%                 cLi(ci(i)) = newctype;
+%                 sce.c_cell_type_tx = string(cLi(ci));
+%                 [c, cL] = grp2idx(sce.c_cell_type_tx);
+%                 i_labelclusters(false);
+%             end
+%         end
+%         guidata(FigureHandle, sce);
+%     end
 
     function EmbeddingAgain(src, ~)
         answer = questdlg('Which embedding method?', 'Select method',...
