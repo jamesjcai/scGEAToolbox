dd2188550d8d8e2a6f7ce4103408f7cf3741eb87
James Cai <jamescai@genomezoo.net>
2022-04-22 22:05:48 -0500

    Create i_select1embedding.m

---
+++ +gui/i_select1embedding.m
@@ -0,0 +1,36 @@
+function [thiss,clable,sce]=i_select1embedding(sce)
+thiss=[];
+clable='';
+
+listitems={''};
+methodtagv=fieldnames(sce.struct_cell_embeddings);
+for k=1:length(methodtagv)
+    methodtag=methodtagv{k};
+    if ~isempty(sce.struct_cell_embeddings.(methodtag))
+        listitems=[listitems,methodtag];
+    end
+end
+listitems(1)=[];
+if isempty(listitems)
+    listitems=[listitems,'Compute tSNE embedding...'];
+    %helpdlg('No embedding is available.','');
+    %return;
+end
+
+[indx2,tf2] = listdlg('PromptString',...
+    {'Select embedding:'},...
+     'SelectionMode','single','ListString',listitems);
+if tf2==1
+    clable=listitems{indx2};
+    switch clable
+        case 'Compute tSNE embedding...'
+            sce=sce.embedcells('tsne',true);
+            thiss=sce.struct_cell_embeddings.tsne;
+            clable='tsne';
+        otherwise
+            thiss=sce.struct_cell_embeddings.(clable);
+    end
+end
+
+
+end
