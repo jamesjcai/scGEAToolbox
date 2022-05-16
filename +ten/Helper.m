a9fe972cf5088223787fcc2509e993f257739396
James Cai <jamescai@genomezoo.net>
2022-05-15 19:01:38 -0500

    Create Helper.m

---
+++ +ten/Helper.m
@@ -0,0 +1,13 @@
+classdef Helper
+% Helper defines miscelanious functions for loading, converting, preparing, and analyzing data
+%
+%#ok<*AGROW>
+
+    methods (Static)
+        function Test
+            disp('xxx');
+% % %             import tree.*;
+% % %             import Constants.*;
+        end
+    end
+end
