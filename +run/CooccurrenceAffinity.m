47c08b4ac4c6ef9f97d39a2b85308405f82050f4
James Cai <jamescai@genomezoo.net>
2022-07-13 16:46:10 -0500

    .

--- +run/CooccurrenceAffinity.m
+++ +run/CooccurrenceAffinity.m
@@ -10,11 +10,15 @@ tmpfilelist={'input.h5','output.h5'};
 if ~isdebug, pkg.i_deletefiles(tmpfilelist); end
 X=uint8(X>0);
 
+% if exist("input.h5",'file'), delete("input.h5"); end
+% if exist("output.h5",'file'), delete("output.h5"); end
 h5create('input.h5', '/X', size(X));
 h5write('input.h5', '/X', X);
 
 pkg.RunRcode('script.R');
 p=h5read('output.h5','/p');
+if isstring(p), p=str2double(p); end
+
 if ~isdebug, pkg.i_deletefiles(tmpfilelist); end
 cd(oldpth);
 
--- +run/external/R_CooccurrenceAffinity/script.R
+++ +run/external/R_CooccurrenceAffinity/script.R
@@ -5,3 +5,5 @@ X <- h5read(file = "input.h5", name = "/X")
 myout <- affinity(data = X, row.or.col = "row", squarematrix = c("p_value"))
 h5createFile("output.h5")
 h5write(as.matrix(myout$p_value), "output.h5","/p")
+
+# report error to: https://github.com/kpmainali/CooccurrenceAffinity/issues
