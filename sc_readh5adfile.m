9d2be5b9328744f8812fb506a065dfae140a0464
James Cai <jamescai@genomezoo.net>
2022-07-31 23:09:32 -0500

    Update sc_readh5adfile.m

--- sc_readh5adfile.m
+++ sc_readh5adfile.m
@@ -1,4 +1,4 @@
-function [X,genelist,filenm]=sc_readh5adfile(filenm)
+function [X,genelist,barcodes,filenm]=sc_readh5adfile(filenm)
 %Read HDF5 file
 % https://www.mathworks.com/help/matlab/hdf5-files.html
 % http://scipy-lectures.org/advanced/scipy_sparse/csc_matrix.html
@@ -7,7 +7,7 @@ function [X,genelist,filenm]=sc_readh5adfile(filenm)
 % https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSM3489183
 % h5file='GSM3489183_IPF_01_filtered_gene_bc_matrices_h5.h5';
 
-% barcodes=[];
+barcodes=[];
 if nargin<1
 [filenm, pathname] = uigetfile( ...
        {'*.h5ad', 'H5AD Files (*.h5)';
@@ -45,6 +45,7 @@ shape=double(hinfo.Groups(idx).Attributes(idx2).Value);
 %     barcodes=h5read(filenm,[hinfo.Groups.Groups(1).Name,'/barcodes']);
 % catch
 %         try
+barcodes=h5read(filenm,[hinfo.Groups(2).Name,'/index']);
 %             barcodes=h5read(filenm,[hinfo.Groups(1).Name,'/barcodes']);
 %         catch
 %             warning('BARCODES not found.');
