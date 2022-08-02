744ecd99bc0a482c6354fd431710c2fb916ae0b8
James Cai <jamescai@genomezoo.net>
2022-08-02 12:11:42 -0500

    .

--- +run/readh5ad.m
+++ +run/readh5ad.m
@@ -10,7 +10,7 @@ oldpth=pwd();
 pw1=fileparts(mfilename('fullpath'));
 wrkpth=fullfile(pw1,'external','py_readh5ad');
 cd(wrkpth);
-tmpfilelist={'output.mat'};
+tmpfilelist={'X.h5','obs.csv','var.csv'};
 if ~isdebug, pkg.i_deletefiles(tmpfilelist); end
 
 x=pyenv;
@@ -20,9 +20,31 @@ cmdlinestr=sprintf('"%s" "%s%sscript.py" "%s"',x.Executable, ...
 disp(cmdlinestr)
 [status]=system(cmdlinestr);
 
-if status==0 && exist('output.txt','file')
+if status==0 && exist('obs.csv','file')
+t1=readtable('obs.csv');
+t2=readtable('var.csv');
+g=string(t2.gene_name);
+b=string(t1.index);
 
+data=h5read('X.h5','/X/data');
+indptr=h5read('X.h5','/X/indptr');
+indices=h5read('X.h5','/X/indices');
+
+%hinfo=h5info('X.h5');
+%shape=double(hinfo.Groups(1).Attributes(1).Value);
+%X=zeros(shape(1),shape(2));
+
+shape = h5readatt('X.h5','/X','shape');
+X=zeros(shape(1),shape(2));
+
+% X=zeros(size(t1,1),size(t2,1));
+for k=1:length(indptr)-1
+    i=indptr(k)+1:indptr(k+1);
+    y=indices(i)+1;
+    X(y,k)=data(i);
 end
+end
+X=X.';
 
 if ~isdebug, pkg.i_deletefiles(tmpfilelist); end
 cd(oldpth);
