9203f9941d8ea0819005fec3878078ce052b229e
James Cai <jamescai@genomezoo.net>
2022-10-02 15:30:18 -0500

    .

--- +gui/i_selectwhitelist.m
+++ +gui/i_selectwhitelist.m
@@ -4,7 +4,7 @@ function [whitelist]=i_selectwhitelist(sce)
                 'Whitelist Genes','Yes','No','Cancel','No');
     switch answer
         case 'Yes'
-            whitelist=0;
+%            whitelist=0;
 %             [gsorted]=gui.i_sortgenenames(sce);
 %             if isempty(gsorted), return; end
 %             [idx]=gui.i_selmultidlg(gsorted);
@@ -12,6 +12,20 @@ function [whitelist]=i_selectwhitelist(sce)
 %             if isscalar(idx) && idx==0, return; end
 % whitelist=gsorted(idx);
             [whitelist]=gui.i_selectngenes(sce);
+            if isempty(whitelist)
+                waitfor(helpdlg('No whitelist gene selected. Click OK to continue.',''));
+                return;
+            end
+            [y]=ismember(whitelist,sce.g);
+            if ~all(y)
+                whitelist=whitelist(y);
+            end
+            if ~isempty(whitelist)
+                waitfor(helpdlg(sprintf('%d whitelist gene(s) selected. Click OK to continue.', ...
+                    length(whitelist)),''));
+            else
+                waitfor(helpdlg('No whitelist gene selected. Click OK to continue.',''));
+            end
         case 'No'
             whitelist=[];     % when isempty, continue..
             return;
