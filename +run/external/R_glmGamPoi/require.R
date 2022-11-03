cc7d2f0aa0eb1b177b7e2e00e5432559744e4b62
James Cai <jamescai@genomezoo.net>
2022-11-03 17:27:10 -0500

    .

---
+++ +run/external/R_glmGamPoi/require.R
@@ -0,0 +1,17 @@
+#setwd(getSrcDirectory()[1])
+
+if (!requireNamespace("glmGamPoi", quietly = TRUE)){
+if (!requireNamespace("BiocManager", quietly = TRUE)){
+    install.packages("BiocManager", repo="http://cran.rstudio.com/")
+}
+    BiocManager::install("glmGamPoi")
+}
+
+if (!requireNamespace("rhdf5", quietly = TRUE)){
+if (!requireNamespace("BiocManager", quietly = TRUE)){
+    install.packages("BiocManager", repo="http://cran.rstudio.com/")
+}
+    BiocManager::install("rhdf5")
+}
+
+
