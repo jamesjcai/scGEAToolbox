a626e4e5ebcc97fd77d69f7afa7dd8359b98064d
James Cai <jamescai@genomezoo.net>
2023-01-31 19:59:56 -0600

    .

--- +gui/callback_CompareGeneNetwork.m
+++ +gui/callback_CompareGeneNetwork.m
@@ -13,12 +13,12 @@ function callback_CompareGeneNetwork(src,~)
     [y,i]=ismember(glist,sce.g);
     if ~all(y), error('xxx'); end
     fprintf("%s\n",glist)
-    fw=gui.gui_waitbar;    
-    x1=sce.X(i,i1);
-    x2=sce.X(i,i2);
-    A1=sc_pcnet(x1);
-    A2=sc_pcnet(x2);
-    gui.gui_waitbar(fw);
+    %fw=gui.gui_waitbar;
+        x1=sce.X(i,i1);
+        x2=sce.X(i,i2);
+        A1=sc_pcnet(x1,3,false,true,true);
+        A2=sc_pcnet(x2,3,false,true,true);
+    %gui.gui_waitbar(fw);
     pause(1)
     sc_grnview2(A1,A2,glist);
 end
