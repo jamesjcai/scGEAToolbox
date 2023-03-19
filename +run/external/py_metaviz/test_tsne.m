9af1678d95d94522ab1100de74e23e32afc6c6cf
James Cai <jamescai@genomezoo.net>
2023-03-19 16:02:51 -0500

    .

--- +run/external/py_metaviz/test_tsne.m
+++ +run/external/py_metaviz/test_tsne.m
@@ -1,9 +1,12 @@
 load fisheriris
 rng default % for reproducibility
-Y = tsne(meas,'Distance','euclidean');
-%gscatter(Y(:,1),Y(:,2),species)
+Y = tsne(meas,'Distance','mahalanobis');
+figure;
+gscatter(Y(:,1),Y(:,2),species)
 
-D=pdist2(meas,meas);
+D=pdist2(meas,meas,"mahalanobis");
+rng default
 Y2 = tsne(D,'Distance',@ExampleDistFunc);
+figure;
 gscatter(Y2(:,1),Y2(:,2),species)
 
