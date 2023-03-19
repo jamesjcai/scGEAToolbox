d4faf2a5001a6068426b454f2d9d7b9e0a5701a8
James Cai <jamescai@genomezoo.net>
2023-03-18 21:03:00 -0500

    .

---
+++ +run/external/py_metaviz/metaviz.m
@@ -0,0 +1,38 @@
+load 1.txt
+load 2.txt
+load 3.txt
+load 4.txt
+load 5.txt
+
+
+Sinput={X1,X2,X3,X4,X5};
+
+%%
+K=length(Sinput);      % K = number of embeddings
+n=size(Sinput{1},1);   % n = number of cells
+w=zeros(K,n);
+%%
+D=zeros(n,n,K);
+for k=1:K
+    d=pdist2(Sinput{k},Sinput{k});
+    D(:,:,k)=d./vecnorm(d);
+    %D(:,:,k)=d;
+end
+%%
+for x=1:n              % n of cells
+    S=1-squareform(pdist(squeeze(D(x,:,:))','cosine'));
+    [v,~]=eigs(double(S),1);
+    w(:,x)=abs(v);
+end
+
+M=zeros(n,n);
+for i=1:n             % cell    
+    d=zeros(n,1);
+    for k=1:K         % type of embedding
+        d=d+w(k,i)'.*squeeze(D(:,i,k));
+    end
+    M(:,i)=d;
+end
+s=tsne(M,"NumDimensions",2);
+load c.txt
+figure; scatter(s(:,1),s(:,2),[],c)
---
+++ +run/external/py_metaviz/testyy.py
@@ -0,0 +1,23 @@
+import os
+#abspath = os.path.abspath(__file__)
+#dname = os.path.dirname(abspath)
+#os.chdir(dname)
+os.chdir("D:\\GitHub\\scGEAToolbox\\+run\\external\\py_metaviz")
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import umap
+from sklearn import manifold, datasets
+
+S_color = pd.read_csv("c.txt", header=None)
+X1 = pd.read_csv("1.txt", header=None)
+X2 = pd.read_csv("2.txt", header=None)
+X3 = pd.read_csv("3.txt", header=None)
+X4 = pd.read_csv("4.txt", header=None)
+X5 = pd.read_csv("5.txt", header=None)
+
+from meta_visualization import meta_viz
+
+meta_distances = meta_viz([X1, X2, X3, X4, X5])
+
