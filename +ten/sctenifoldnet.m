5288dd102174dc526110cb60292161f4e523b523
James Cai <jamescai@genomezoo.net>
2023-03-31 13:24:51 -0500

    .

--- +ten/sctenifoldnet.m
+++ +ten/sctenifoldnet.m
@@ -8,8 +8,7 @@ import ten.*
 if ~(ismcc || isdeployed)
 if exist('tensor.m','file')~=2
     pw1=fileparts(mfilename('fullpath'));
-    %pth=fullfile(pw1,'..','tensor_toolbox');
-    pth=fullfile(pw1,'..','tensor_lite');
+    pth=fullfile(pw1,'..','tensor_toolbox');    
     addpath(pth);
 end
 end
