274b2d7d1ebae3db75d1c0038cf175a944205739
James Cai <jamescai@genomezoo.net>
2023-04-04 18:12:29 -0500

    .

---
+++ +pkg/e_diameterpath.m
@@ -0,0 +1,11 @@
+function [P,s,t]=e_diameterpath(W)
+
+G=graph(W);
+d=distances(G);
+d(isinf(d))=0;
+[~,idx]=max(d(:));
+[s,t]=ind2sub(size(d),idx);
+P=shortestpath(G,s,t);
+
+% allpaths(G,s,t,'MinPathLength',size(W,1), ...
+%     'MaxPathLength',size(W,1),'MaxNumPaths',1);
