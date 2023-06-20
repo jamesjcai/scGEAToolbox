49524aa650ca85fe30ce9f4ccef0c755ed20de8c
James Cai <jamescai@genomezoo.net>
2023-06-20 12:53:14 -0500

    Update require.py

--- +run/external/py_MELD/require.py
+++ +run/external/py_MELD/require.py
@@ -9,3 +9,29 @@ try:
 except ImportError as exc:
     print(exc)
     exit(10)
+
+
+
+#import subprocess
+#import sys
+#
+#try:
+#    import pandas as pd
+#except ImportError:
+#    subprocess.check_call([sys.executable, "-m", "pip", "install", 'pandas'])
+#finally:
+#    import pandas as pd
+
+
+#import sys
+#import subprocess
+#import pkg_resources
+#
+#required  = {'numpy', 'pandas', '<etc>'} 
+#installed = {pkg.key for pkg in pkg_resources.working_set}
+#missing   = required - installed
+#
+#if missing:
+    # implement pip as a subprocess:
+#    subprocess.check_call([sys.executable, '-m', 'pip', 'install', *missing])
+# https://stackoverflow.com/questions/12332975/how-can-i-install-a-python-module-within-code
