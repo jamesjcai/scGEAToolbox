098922692201076f38617f468fa46e7eff339a10
James Cai <jamescai@genomezoo.net>
2023-06-29 14:32:01 -0500

    Update sc_readloomfile.m

--- sc_readloomfile.m
+++ sc_readloomfile.m
@@ -32,23 +32,26 @@ catch
 end
 
 
-try
-    
-    %X=sparse(double(X));
-catch
-    tic
-    S=spalloc(size(X,1),size(X,2),nnz(X));
-    idx=find(X>0);
-    S(idx)=X(idx);
-    toc
-    % X=S;
+if ~issparse(X) && ~isa(X,'double')
+    try    
+        X=sparse(double(X));
+    catch ME
+        if (strcmp(ME.identifier,'MATLAB:array:SizeLimitExceeded'))
+            disp('Converting X to sparse.');
+            % tic
+            % S=spalloc(size(X,1),size(X,2),nnz(X));
+            % idx=find(X>0);
+            % S(idx)=X(idx);
+            % toc
+            % X=S;
+            a=floor(size(X)./2);
+            x1=sparse(double(X(1:a(1),1:a(2))));
+            x2=sparse(double(X(a(1)+1:end,1:a(2))));
+            x3=sparse(double(X(1:a(1),a(2)+1:end)));
+            x4=sparse(double(X(a(1)+1:end,a(2)+1:end)));
+            X=[x1 x3; x2 x4];
+        end
+    end
 
-% a=floor(size(X)./2)
-% x1=sparse(X(1:a(1),1:a(2)));
-% x1=sparse(double(X(1:a(1),1:a(2))));
-% x2=sparse(double(X(a(1)+1:end,1:a(2))));
-% x3=sparse(double(X(1:a(1),a(2)+1:end)));
-% x4=sparse(double(X(a(1)+1:end,a(2)+1:end)));
-% Y=[x1 x3; x2 x4];
 end
 end
