fb0d4f52d8a47727d3b79e5142ce6fc3d0741c53
James Cai <jamescai@genomezoo.net>
2023-06-30 23:40:02 -0500

    Update test_h5ad.m

--- test_h5ad.m
+++ test_h5ad.m
@@ -3,6 +3,6 @@ h=h5info(filnm);
 
 % a=h5read(filnm,'/obs/seurat_clusters')
 % a=h5read(filnm,'/var/_index')
-a=h5read(filnm,'/obs/annotation')
+a=pkg.e_guessh5field(filnm,'/obs/','annotation',true)
 a=h5read(filnm,'/obsm/X_umap')
 a=h5read(filnm,'/obs/cell_type')
