859c7d11a26067f407e2743cbcc9d2ee57251199
James Cai <jamescai@genomezoo.net>
2023-07-04 15:59:36 -0500

    .

--- test_h5ad.m
+++
@@ -1,8 +0,0 @@
-filnm='hca_heart_neuronal_raw.h5ad';
-h=h5info(filnm);
-
-% a=h5read(filnm,'/obs/seurat_clusters')
-% a=h5read(filnm,'/var/_index')
-a=pkg.e_guessh5field(filnm,'/obs/','annotation',true);
-a=h5read(filnm,'/obsm/X_umap');
-a=h5read(filnm,'/obs/cell_type');
