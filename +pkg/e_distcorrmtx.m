e8c8b121c6d0d1da14e20ccc21cac2ca3faa6c19
James Cai <jamescai@genomezoo.net>
2023-07-21 21:52:03 -0500

    Create e_distcorrmtx.m

---
+++ +pkg/e_distcorrmtx.m
@@ -0,0 +1,15 @@
+function [R]=e_distcorrmtx(X)
+
+n=size(X,1);  % number of genes
+R=zeros(n);
+c=0;
+for k=1:n
+    for l=k+1:n
+        c=c+1;
+        R(k,l)=pkg.distcorr(X(k,:)',X(l,:)');
+        c
+        if c>20, break; end
+    end
+            if c>20, break; end
+
+end
