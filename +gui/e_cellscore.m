9b2eb32260a1339b69cea89c4ae11f077eb9a182
James Cai <jamescai@genomezoo.net>
2023-08-10 11:53:55 -0500

    Update e_cellscore.m

--- +gui/e_cellscore.m
+++ +gui/e_cellscore.m
@@ -1,34 +1,36 @@
-function [cs]=e_cellscore(sce,posg)
+function [cs]=e_cellscore(sce,posg,methodid)
 
+cs=[];
+if nargin<3 || isempty(methodid)
     answer = questdlg('Select algorithm:',...
     'Select Method', ...
     'UCell [PMID:34285779]','AddModuleScore/Seurat', ...
     'UCell [PMID:34285779]');
     switch answer
         case 'AddModuleScore/Seurat'
-            fw=gui.gui_waitbar;
-            try
-                [cs]=sc_cellscore_admdl(sce.X,sce.g,posg);
-            catch ME
-                gui.gui_waitbar(fw,true);
-                errordlg(ME.message);                
-            return;
-            end
-            gui.gui_waitbar(fw);
+            methodid=2;
         case 'UCell [PMID:34285779]'
-            %[cs]=run.UCell(sce.X,sce.g,posg);
-            fw=gui.gui_waitbar;
-            try
-                [cs]=sc_cellscore_ucell(sce.X,sce.g,posg);
-            catch ME
-                gui.gui_waitbar(fw,true);
-                errordlg(ME.message);
-            return;
-            end
-            gui.gui_waitbar(fw);
+            methodid=1;            
         otherwise
             return;
     end
+end
+
+    fw=gui.gui_waitbar;
+    try
+        if methodid==1
+            [cs]=sc_cellscore_ucell(sce.X,sce.g,posg);
+        elseif methodid==2
+            [cs]=sc_cellscore_admdl(sce.X,sce.g,posg);
+        end                
+    catch ME
+        gui.gui_waitbar(fw,true);
+        errordlg(ME.message);
+        return;
+    end
+    gui.gui_waitbar(fw);
+
+
 
     posg=sort(posg);
     isexpressed=ismember(upper(posg),upper(sce.g));
