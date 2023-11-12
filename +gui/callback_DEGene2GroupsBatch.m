3e9e7b916e9d5581ad20f9ef0d43f96ed635b470
James Cai <jamescai@genomezoo.net>
2023-11-12 13:51:37 -0600

    Update callback_DEGene2GroupsBatch.m

--- +gui/callback_DEGene2GroupsBatch.m
+++ +gui/callback_DEGene2GroupsBatch.m
@@ -4,11 +4,15 @@ function callback_DEGene2GroupsBatch(src, ~)
 FigureHandle = src.Parent.Parent;
 sce = guidata(FigureHandle);
 
-warndlg('Function is under development.','');
-return;
+%warndlg('Function is under development.','');
+%return;
 
+if length(unique(sce.c_cell_type_tx))==1
+    warndlg('Only one cell type or cell type is undetermined.','');
+    return;
+end
 
-answer = questdlg('Compare 2 groups across all cell types (DE analysis in batch)?', '');
+answer = questdlg('This function iterates all cell types and, for each cell type, perform DE analysis between two groups (e.g., WT and KO). Continue?', '');
 if ~strcmp(answer,'Yes'), return; end
 
 [thisc, clable]=gui.i_select1class(sce);
@@ -25,167 +29,105 @@ if isempty(i1) || isempty(i2) || isempty(cL1) || isempty(cL2)
 end
 if length(i1) == 1 || length(i2) == 1, return; end
 
-answer = questdlg('Which method?', ...
-    'Select Method', 'Wilcoxon rank-sum test 🐇', ...
-    'DESeq 2 (R required) 🐢', ...
-    'MAST (R required) 🐢', ...
-    'Wilcoxon rank-sum test 🐇');
+%methodtag = "ranksum";
 
-if strcmpi(answer, 'Wilcoxon rank-sum test 🐇')
-    methodtag = "ranksum";
-elseif strcmpi(answer, 'DESeq 2 (R required) 🐢')
-    methodtag = "deseq2";
-elseif strcmpi(answer, 'MAST (R required) 🐢')
-    methodtag = "mast";
-    if isempty(pkg.FindRpath)
-        warndlg('This function requires R environment.')
-            return;
-    end
-else
-    return;
-end
+% answer = questdlg('Which method?', ...
+%     'Select Method', 'Wilcoxon rank-sum test 🐇', ...
+%     'DESeq 2 (R required) 🐢', ...
+%     'MAST (R required) 🐢', ...
+%     'Wilcoxon rank-sum test 🐇');
+% 
+% if strcmpi(answer, 'Wilcoxon rank-sum test 🐇')
+%     methodtag = "ranksum";
+% elseif strcmpi(answer, 'DESeq 2 (R required) 🐢')
+%     methodtag = "deseq2";
+% elseif strcmpi(answer, 'MAST (R required) 🐢')
+%     methodtag = "mast";
+%     if isempty(pkg.FindRpath)
+%         warndlg('This function requires R environment.')
+%             return;
+%     end
+% else
+%     return;
+% end
+answer=questdlg('Select a folder to save the outupt Excel files. Continue?','');
+if ~strcmp(answer,'Yes'), return; end
 
-% ------------
-[CellTypeSorted]=in_sortcelltypes(sce.c_cell_type_tx)
+outdir = uigetdir;
+if ~isfolder(outdir), return; end
+
+answer=questdlg(sprintf('Result files will be save in %s. Continue?', outdir), '');
+if ~strcmp(answer,'Yes'), return; end
+
+%waitfor(helpdlg(sprintf('Result files will be save in %s', outdir), ''));
+
+
+[CellTypeSorted]=in_sortcelltypes(sce.c_cell_type_tx);
+
+    fw = gui.gui_waitbar_adv;
 
 for k=1:length(CellTypeSorted)
-    idx1 = sce.c_cell_type_tx == CellTypeSorted{k};
-    % sum(idx1)
+    
+    gui.gui_waitbar_adv(fw, ...
+        k/length(CellTypeSorted), ...
+        sprintf('Processing %s ...', CellTypeSorted{k}));
+
+    idx = sce.c_cell_type_tx == CellTypeSorted{k};
+    T = sc_deg(sce.X(:, i1&idx), sce.X(:, i2), sce.g, 1, false);
+    T = in_DETableProcess(T,cL1,cL2);
+
+    [Tup, Tdn] = pkg.e_processDETable(T, true);
+    outfile = sprintf('%s_vs_%s_%s', ...
+        matlab.lang.makeValidName(string(cL1)), ...
+        matlab.lang.makeValidName(string(cL2)), ...
+        matlab.lang.makeValidName(string(CellTypeSorted{k})));
+    filesaved = fullfile(outdir, outfile);
+    writetable(T, filesaved, 'FileType', 'spreadsheet', 'Sheet', 'All genes');
+    writetable(Tup, filesaved, "FileType", "spreadsheet", 'Sheet', 'Up-regulated');
+    writetable(Tdn, filesaved, "FileType", "spreadsheet", 'Sheet', 'Down-regulated');
+end
+gui.gui_waitbar_adv(fw);
+
+answer=questdlg(sprintf('Result files saved. Open the folder %s?', outdir), '');
+if strcmp(answer,'Yes'), winopen(outdir); end
+
 end
 
 
-
-try
-    switch methodtag
-        case 'ranksum'
-            %fw=gui.gui_waitbar;
-            T = sc_deg(sce.X(:, i1), sce.X(:, i2), ...
-                sce.g, 1, true);
-        case 'deseq2'
-            [ok] = gui.i_confirmscript('DE analysis (DESeq2)', ...
-                'R_DESeq2', 'r');
-            if ~ok, return; end
-            fw = gui.gui_waitbar;
-            T = run.r_DESeq2(sce.X(:, i1), sce.X(:, i2), sce.g);
-            gui.gui_waitbar(fw);
-        case 'mast'
-            [ok] = gui.i_confirmscript('DE analysis (MAST)', ...
-                'R_MAST', 'r');
-            if ~ok, return; end
-            fw = gui.gui_waitbar;
-            T = run.r_MAST(sce.X(:, i1), sce.X(:, i2), sce.g);
-            gui.gui_waitbar(fw);
-    end
-
-catch ME
-    gui.gui_waitbar(fw, true);
-    errordlg(ME.message);
-    return;
-end
+% try
+%     switch methodtag
+%         case 'ranksum'
+%             %fw=gui.gui_waitbar;
+%             T = sc_deg(sce.X(:, i1), sce.X(:, i2), ...
+%                 sce.g, 1, true);
+%         case 'deseq2'
+%             [ok] = gui.i_confirmscript('DE analysis (DESeq2)', ...
+%                 'R_DESeq2', 'r');
+%             if ~ok, return; end
+%             fw = gui.gui_waitbar;
+%             T = run.r_DESeq2(sce.X(:, i1), sce.X(:, i2), sce.g);
+%             gui.gui_waitbar(fw);
+%         case 'mast'
+%             [ok] = gui.i_confirmscript('DE analysis (MAST)', ...
+%                 'R_MAST', 'r');
+%             if ~ok, return; end
+%             fw = gui.gui_waitbar;
+%             T = run.r_MAST(sce.X(:, i1), sce.X(:, i2), sce.g);
+%             gui.gui_waitbar(fw);
+%     end
+% 
+% catch ME
+%     gui.gui_waitbar(fw, true);
+%     errordlg(ME.message);
+%     return;
+% end
 
 % mavolcanoplot(sce.X(:,i1),sce.X(:,i2),T.p_val_adj,'Labels',T.gene)
 
-try
-    T = sortrows(T, 'p_val_adj', 'ascend');
-    T = sortrows(T, 'pct_1', 'ascend');
-    T = sortrows(T, 'pct_2', 'descend');
-    T = sortrows(T, 'avg_log2FC', 'ascend');
-catch ME
-    warning(ME.message);
-end
-
-try
-    if contains(T.Properties.VariableNames{5}, 'avg_1')
-        T.Properties.VariableNames{5} = sprintf('%s_%s', ...
-            T.Properties.VariableNames{5}, ...
-            matlab.lang.makeValidName(string(cL1)));
-    end
-
-    if contains(T.Properties.VariableNames{6}, 'avg_2')
-        T.Properties.VariableNames{6} = sprintf('%s_%s', ...
-            T.Properties.VariableNames{6}, ...
-            matlab.lang.makeValidName(string(cL2)));
-    end
-
-    if contains(T.Properties.VariableNames{7}, 'pct_1')
-        T.Properties.VariableNames{7} = sprintf('%s_%s', ...
-            T.Properties.VariableNames{7}, ...
-            matlab.lang.makeValidName(string(cL1)));
-    end
-
-    if contains(T.Properties.VariableNames{8}, 'pct_2')
-        T.Properties.VariableNames{8} = sprintf('%s_%s', ...
-            T.Properties.VariableNames{8}, ...
-            matlab.lang.makeValidName(string(cL2)));
-    end
-catch ME
-    warning(ME.message);
-end
-
-outfile = sprintf('%s_vs_%s', ...
-    matlab.lang.makeValidName(string(cL1)), matlab.lang.makeValidName(string(cL2)));
-if isatac, T.gene = "chr" + T.gene; end
-
-[filetype, filesaved] = gui.i_exporttable(T, true, 'T', outfile);
-tf = 0;
-if ~(ismcc || isdeployed) && strcmp(filetype, 'Workspace')
-    [Tup, Tdn] = pkg.e_processDETable(T, true);
-    labels = {'Save DE results (selected up-regulated) to variable named:', ...
-        'Save DE results (selected down-regulated) to variable named:'};
-    vars = {'Tup', 'Tdn'};
-    values = {Tup, Tdn};
-    [~, tf] = export2wsdlg(labels, vars, values);
-    if tf ~= 1
-        return;
-    end
-end
-
-if ~isempty(filesaved)
-    if strcmp(filetype, 'Excel file')
-        answer = questdlg('Save up- and down-regulated genes to seperate sheets?');
-        if strcmp(answer, 'Yes')
-            [Tup, Tdn] = pkg.e_processDETable(T, true);
-            % strcmp(extractAfter(filesaved,strlength(filesaved)-4),'xlsx')
-            writetable(Tup, filesaved, "FileType", "spreadsheet", 'Sheet', 'Up-regulated');
-            writetable(Tdn, filesaved, "FileType", "spreadsheet", 'Sheet', 'Down-regulated');
-            waitfor(helpdlg(sprintf('Result has been saved in %s', filesaved), ''));
-            %writetable(Tup,fullfile(tempdir,sprintf('%s_up.xlsx',outfile)),'FileType','spreadsheet',);
-            %writetable(Tdn,fullfile(tempdir,sprintf('%s_up.xlsx',outfile)),'FileType','spreadsheet');
-        end
-    elseif strcmp(filetype, 'Text file')
-        % strcmp(extractAfter(filesaved,strlength(filesaved)-3),'txt')
-        answer = questdlg('Save up- and down-regulated genes to seperate files?');
-        if strcmp(answer, 'Yes')
-            [Tup, Tdn] = pkg.e_processDETable(T, true);
-            [~, ~] = gui.i_exporttable(Tup, true, 'Tup', 'Upregulated', 'Text file');
-            [~, ~] = gui.i_exporttable(Tdn, true, 'Tdn', 'Downregulated', 'Text file');
-        end
-    end
-end
 
 
-    if tf == 1
-        disp('To run pathway analysis, type:');
-        disp('t=run.r_SPIA(T);');
-        disp('To run enrichment analysis, type:');
-        disp('run.web_Enrichr(Tup.gene(1:200))');
-        disp('run.web_Enrichr(Tdn.gene(1:200))');
 
-        answer = questdlg('Run enrichment analysis with top K (=200 by default) up-regulated DE genes?');
-        if strcmp(answer, 'Yes')
-            gui.i_enrichtest(Tup.gene(1:min(numel(Tup.gene), 200)), sce.g);
-        else
-            return;
-        end
 
-        answer = questdlg('Run enrichment analysis with top K (=200 by default) down-regulated DE genes?');
-        if strcmp(answer, 'Yes')
-            gui.i_enrichtest(Tdn.gene(1:min(numel(Tdn.gene), 200)), sce.g);
-        else
-            return;
-        end
-    end
-end
 
 function [i1, i2, cL1, cL2]=in_twogrpsencoding(thisc)
     [ci, cLi] = grp2idx(thisc);
@@ -223,4 +165,54 @@ function [cLsorted]=in_sortcelltypes(thisc)
     [cmx] = countmember(cmv, c);
     [~, idxx] = sort(cmx, 'descend');
     cLsorted=cL(idxx);
+end
+
+function [T]=in_DETableProcess(T,cL1,cL2)
+    try
+        T = sortrows(T, 'p_val_adj', 'ascend');
+        T = sortrows(T, 'pct_1', 'ascend');
+        T = sortrows(T, 'pct_2', 'descend');
+        T = sortrows(T, 'avg_log2FC', 'ascend');
+    catch ME
+        warning(ME.message);
+    end
+    
+    try
+        if contains(T.Properties.VariableNames{5}, 'avg_1')
+            T.Properties.VariableNames{5} = sprintf('%s_%s', ...
+                T.Properties.VariableNames{5}, ...
+                matlab.lang.makeValidName(string(cL1)));
+        end
+    
+        if contains(T.Properties.VariableNames{6}, 'avg_2')
+            T.Properties.VariableNames{6} = sprintf('%s_%s', ...
+                T.Properties.VariableNames{6}, ...
+                matlab.lang.makeValidName(string(cL2)));
+        end
+    
+        if contains(T.Properties.VariableNames{7}, 'pct_1')
+            T.Properties.VariableNames{7} = sprintf('%s_%s', ...
+                T.Properties.VariableNames{7}, ...
+                matlab.lang.makeValidName(string(cL1)));
+        end
+    
+        if contains(T.Properties.VariableNames{8}, 'pct_2')
+            T.Properties.VariableNames{8} = sprintf('%s_%s', ...
+                T.Properties.VariableNames{8}, ...
+                matlab.lang.makeValidName(string(cL2)));
+        end
+    catch ME
+        warning(ME.message);
+    end
+
+        variables = T.Properties.VariableNames;
+        for k = 1:length(variables)
+            xx = T.(variables{k});
+            if isnumeric(xx) && any(isinf(xx))
+                xx(isinf(xx) & xx > 0) = 1e99;
+                xx(isinf(xx) & xx < 0) = -1e99;
+                T.(variables{k}) = xx;
+            end
+        end
+    
 end
