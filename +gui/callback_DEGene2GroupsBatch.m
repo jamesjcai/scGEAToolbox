d54140764f1fcf1ebf097c4e2b5b1953a8c9c452
James Cai <jamescai@genomezoo.net>
2023-11-12 22:04:24 -0600

    Update callback_DEGene2GroupsBatch.m

--- +gui/callback_DEGene2GroupsBatch.m
+++ +gui/callback_DEGene2GroupsBatch.m
@@ -14,7 +14,12 @@ end
 
 gui.gui_showrefinfo('DE in Batch Mode');
 
-[thisc, clable]=gui.i_select1class(sce);
+[CellTypeSorted]=pkg.e_sortcatbysize(sce.c_cell_type_tx);
+[CellTypeList]=in_selectcelltypes(CellTypeSorted);
+if isempty(CellTypeList), return; end
+
+[thisc, clable]=in_select1class(sce);
+if isempty(thisc), return; end
 if strcmp(clable,'Cell Type')
     helpdlg('Cannot select ''Cell Type'' as grouping varialbe.');
     return;
@@ -61,18 +66,16 @@ if ~strcmp(answer,'Yes'), return; end
 %waitfor(helpdlg(sprintf('Result files will be save in %s', outdir), ''));
 
 
-% [CellTypeSorted]=in_sortcelltypes(sce.c_cell_type_tx);
-[CellTypeSorted]=pkg.e_sortcatbysize(sce.c_cell_type_tx);
 
     fw = gui.gui_waitbar_adv;
 
-for k=1:length(CellTypeSorted)
+for k=1:length(CellTypeList)
     
     gui.gui_waitbar_adv(fw, ...
-        k/length(CellTypeSorted), ...
-        sprintf('Processing %s ...', CellTypeSorted{k}));
+        k/length(CellTypeList), ...
+        sprintf('Processing %s ...', CellTypeList{k}));
 
-    idx = sce.c_cell_type_tx == CellTypeSorted{k};
+    idx = sce.c_cell_type_tx == CellTypeList{k};
     T = sc_deg(sce.X(:, i1&idx), sce.X(:, i2), sce.g, 1, false);
     T = in_DETableProcess(T,cL1,cL2);
 
@@ -80,7 +83,7 @@ for k=1:length(CellTypeSorted)
     outfile = sprintf('%s_vs_%s_%s', ...
         matlab.lang.makeValidName(string(cL1)), ...
         matlab.lang.makeValidName(string(cL2)), ...
-        matlab.lang.makeValidName(string(CellTypeSorted{k})));
+        matlab.lang.makeValidName(string(CellTypeList{k})));
     filesaved = fullfile(outdir, outfile);
     writetable(T, filesaved, 'FileType', 'spreadsheet', 'Sheet', 'All genes');
     writetable(Tup, filesaved, "FileType", "spreadsheet", 'Sheet', 'Up-regulated');
@@ -155,7 +158,23 @@ function [i1, i2, cL1, cL2]=in_twogrpsencoding(thisc)
         cL2 = cLi(idx2);
     else
         i1=[]; i2=[]; cL1=[]; cL2=[];
-    end        
+    end
+end
+
+function [CellTypeList]=in_selectcelltypes(CellTypeSorted)
+    CellTypeList=[];
+    %pause(1);
+    %[idx] = gui.i_selmultidlg(CellTypeSorted, CellTypeSorted);
+    %if isempty(idx), return; end
+    %if idx == 0, return; end
+    %CellTypeList = CellTypeSorted(idx);
+    [indx2, tf2] = listdlg('PromptString', ...
+    {'Select Cell Types:'}, ...
+    'SelectionMode', 'multiple', 'ListString', CellTypeSorted, ...
+    'InitialValue',1:length(CellTypeSorted));
+    if tf2 == 1
+        CellTypeList = CellTypeSorted(indx2);
+    end
 end
 
 % function [cLsorted]=in_sortcelltypes(thisc)
@@ -215,4 +234,87 @@ function [T]=in_DETableProcess(T,cL1,cL2)
             end
         end
     
+end
+
+
+
+function [thisc, clable] = in_select1class(sce, allowunique)
+    if nargin < 2, allowunique = false; end
+    thisc = [];
+    clable = '';
+    
+    listitems = {'Current Class (C)'};
+    if ~isempty(sce.c_cluster_id)
+        if allowunique
+            listitems = [listitems, 'Cluster ID'];
+        else
+            if numel(unique(sce.c_cluster_id)) > 1
+                listitems = [listitems, 'Cluster ID'];
+            end
+        end
+    end
+    
+    if ~isempty(sce.c_cell_cycle_tx)
+        if allowunique
+            listitems = [listitems, 'Cell Cycle Phase'];
+        else
+            if numel(unique(sce.c_cell_cycle_tx)) > 1
+                listitems = [listitems, 'Cell Cycle Phase'];
+            end
+        end
+    end
+    if ~isempty(sce.c_batch_id)
+        if allowunique
+            listitems = [listitems, 'Batch ID'];
+        else
+            if numel(unique(sce.c_batch_id)) > 1
+                listitems = [listitems, 'Batch ID'];
+            end
+        end
+    end
+    
+    a = evalin('base', 'whos');
+    b = struct2cell(a);
+    v = false(length(a), 1);
+    for k = 1:length(a)
+        if max(a(k).size) == sce.NumCells && min(a(k).size) == 1
+            v(k) = true;
+        end
+    end
+    if any(v)
+        a = a(v);
+        b = b(:, v);
+        listitems = [listitems, 'Workspace Variable...'];
+    end
+    
+    [indx2, tf2] = listdlg('PromptString', ...
+        {'Select grouping variable:'}, ...
+        'SelectionMode', 'single', 'ListString', listitems);
+    if tf2 == 1
+        clable = listitems{indx2};
+        switch clable
+            case 'Current Class (C)'
+                thisc = sce.c;
+            case 'Cluster ID' % cluster id
+                thisc = sce.c_cluster_id;
+            case 'Batch ID' % batch id
+                thisc = sce.c_batch_id;
+            %case 'Cell Type' % cell type
+            %    thisc = sce.c_cell_type_tx;
+            case 'Cell Cycle Phase' % cell cycle
+                thisc = sce.c_cell_cycle_tx;
+            case 'Workspace Variable...'
+                thisc = i_pickvariable;
+        end
+    end
+    
+    function [c] = i_pickvariable
+        c = [];
+        [indx, tf] = listdlg('PromptString', {'Select variable:'}, ...
+            'liststring', b(1, :), 'SelectionMode', 'single');
+        if tf == 1
+            c = evalin('base', a(indx).name);
+        end
+        %    end
+    end
 end
