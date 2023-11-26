37fa6d85ee6beff8fb40a98cb654af660714cc1d
James Cai <jamescai@genomezoo.net>
2023-11-26 10:35:27 -0600

    .

--- scgeatool.m
+++ scgeatool.m
@@ -12,6 +12,36 @@ function scgeatool(X, g, s, c)
 
 olddir = pwd();
 cdgea;
+try
+    Col = webread('https://api.github.com/repos/jamesjcai/scGEAToolbox');
+    if ~exist('TIMESTAMP','file')
+        fid=fopen('TIMESTAMP','w');
+        fprintf(fid,'%s\n',Col.pushed_at);
+        fclose(fid);
+    else
+        fid=fopen('TIMESTAMP','r');
+        d=textscan(fid,'%s');
+        d=d{1}{1};
+        fclose(fid);
+        if ~strcmp(d,Col.pushed_at)
+            answer=questdlg('There is a new version of scGEAToolbox. Learn how to install?','', ...
+                'Yes','No','Ignore','Yes');
+            switch answer
+                case 'Yes'
+                    web('https://scgeatoolbox.readthedocs.io/en/latest/quick_installation.html');
+                    return;
+                case 'No'
+                case 'Ignore'
+                    fid=fopen('TIMESTAMP','w');
+                    fprintf(fid,'%s\n',Col.pushed_at);
+                    fclose(fid);
+                otherwise
+            end        
+        end
+    end
+catch
+end
+
 cd(olddir);
 
 if ~(ismcc || isdeployed)
