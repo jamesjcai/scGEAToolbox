b1be78a225f03e070a788c3251a773fa895d4773
James Cai <jamescai@genomezoo.net>
2023-12-31 16:57:23 -0600

    .

--- sc_cellcyclescore.m
+++ sc_cellcyclescore.m
@@ -1,74 +1,74 @@
 function [ScoreV, T] = sc_cellcyclescore(X, g)
-% Score cell cycle phases
-
-% [~,sgenes,g2mgenes]=pkg.i_get_cellcyclegenes;
-pw1 = fileparts(mfilename('fullpath'));
-wrkpth = fullfile(pw1, 'resources', 'cellcyclegenes.xlsx');
-
-T = readtable(wrkpth, ...
-    'ReadVariableNames', true, 'FileType', 'spreadsheet', ...
-    'Sheet', 'Regev_cell_cycle_genes');
-sgenes = string(T.S);
-sgenes = sgenes(strlength(sgenes) > 0);
-g2mgenes = string(T.G2M);
-g2mgenes = g2mgenes(strlength(g2mgenes) > 0);
-
-score_S = sc_cellscore_admdl(X, g, sgenes);
-score_G2M = sc_cellscore_admdl(X, g, g2mgenes);
-
-ScoreV = string(repmat('G1', size(X, 2), 1));
-C = [score_S, score_G2M];
-[~, I] = max(C, [], 2);
-Cx = C > 0;
-i = sum(Cx, 2) > 0;
-ScoreV(i & I == 1) = "S";
-ScoreV(i & I == 2) = "G2M";
-if nargout > 1
-    T = table(score_S, score_G2M, ScoreV);
+    % Score cell cycle phases
+    
+    % [~,sgenes,g2mgenes]=pkg.i_get_cellcyclegenes;
+    pw1 = fileparts(mfilename('fullpath'));
+    wrkpth = fullfile(pw1, 'resources', 'cellcyclegenes.xlsx');
+    
+    T = readtable(wrkpth, ...
+        'ReadVariableNames', true, 'FileType', 'spreadsheet', ...
+        'Sheet', 'Regev_cell_cycle_genes');
+    sgenes = string(T.S);
+    sgenes = sgenes(strlength(sgenes) > 0);
+    g2mgenes = string(T.G2M);
+    g2mgenes = g2mgenes(strlength(g2mgenes) > 0);
+    
+    score_S = sc_cellscore_admdl(X, g, sgenes);
+    score_G2M = sc_cellscore_admdl(X, g, g2mgenes);
+    
+    ScoreV = string(repmat('G1', size(X, 2), 1));
+    C = [score_S, score_G2M];
+    [~, I] = max(C, [], 2);
+    Cx = C > 0;
+    i = sum(Cx, 2) > 0;
+    ScoreV(i & I == 1) = "S";
+    ScoreV(i & I == 2) = "G2M";
+    if nargout > 1
+        T = table(score_S, score_G2M, ScoreV);
+    end
 end
-end
-
-
-% function [g,sgenes,g2mgenes]=i_get_cellcyclegenes
-% %Get cell-cycle genes
-%
-% % https://satijalab.org/seurat/v2.4/cell_cycle_vignette.html
-% % Also read in a list of cell cycle markers, from Tirosh et al, 2015
-% %cc.genes <- readLines(con = "~/Downloads/seurat_resources/regev_lab_cell_cycle_genes.txt")
-%
-% folder=fileparts(mfilename('fullpath'));
-% a=strfind(folder,filesep);
-% folder=extractBefore(folder,a(end)+1);
-% wrkpth=fullfile(folder,'resources',filesep);
-% fname=[wrkpth 'regev_lab_cell_cycle_genes.txt'];
-% g=string(readcell(fname));
-%
-% sgenes=g(1:43);
-% g2mgenes=g(44:97);
-%
-% return;
-% %https://science.sciencemag.org/content/352/6282/189
-%
-% % https://www.frontiersin.org/articles/10.3389/fgene.2017.00001/full
-%
-% %{
-% s1="CDK2, MKI67, RB1, E2F1, HIST1H2AE, CCNB1, CCNB1, CBX3, ND1, MKI67, GAPDH, CCNB1, CCNB2";
-% g1=unique(strsplit(s1,", "));
-% s2="ATAD2, BORA, BTG3, CASP8AP2, CCDC134, CCNA2, CCNB1, CCNB2, CCNE1, CCNE2, CDC25A, CDC25B, CDC25C, CDC5L, CDC7, CDCA3, CDCA4, CDCA7, CDK1, CDK19, CDK4, CDKN2C, CDKN2D, CDKN3, CKS1B, CKS2, CRLF3, DBF4B, DLGAP5, E2F1, E2F7, E2F8, FAM83D, FBXO5, FOXM1, FZR1, GMNN, LIN54, LIN9, MASTL, MELK, MYBL2, NPAT, ODF2, PA2G4, PBK, PIMREG, PKMYT1, PRR11, RBL1, STIL, TCF19, TFDP1, TICRR, TOE1, TRIM28, TTF2, UBE2C, UBE2S, UHRF2, USP37, WEE1, CDK2, AURKA, KIF14, DTL, GTSE1, PLK1";
-% g2=unique(strsplit(s2,", "));
-% g=unique([g1 g2]);
-%
-% return;
-%
-% options = weboptions('Timeout',21);
-% fname=tempname;
-% websave(fname,'https://raw.githubusercontent.com/hbc/tinyatlas/master/cell_cycle/Homo_sapiens.csv',options);
-% % t=readtable('a.txt','PreserveVariableNames',true);
-% warning off
-% t=readtable(fname,'Range','A:B');
-% warning on
-% g=string(t.ApprovedSymbol);
-% delete(fname);
-% %}
-%
-% % https://academic.oup.com/jmcb/article/11/8/703/5188008
+    
+    
+    % function [g,sgenes,g2mgenes]=i_get_cellcyclegenes
+    % %Get cell-cycle genes
+    %
+    % % https://satijalab.org/seurat/v2.4/cell_cycle_vignette.html
+    % % Also read in a list of cell cycle markers, from Tirosh et al, 2015
+    % %cc.genes <- readLines(con = "~/Downloads/seurat_resources/regev_lab_cell_cycle_genes.txt")
+    %
+    % folder=fileparts(mfilename('fullpath'));
+    % a=strfind(folder,filesep);
+    % folder=extractBefore(folder,a(end)+1);
+    % wrkpth=fullfile(folder,'resources',filesep);
+    % fname=[wrkpth 'regev_lab_cell_cycle_genes.txt'];
+    % g=string(readcell(fname));
+    %
+    % sgenes=g(1:43);
+    % g2mgenes=g(44:97);
+    %
+    % return;
+    % %https://science.sciencemag.org/content/352/6282/189
+    %
+    % % https://www.frontiersin.org/articles/10.3389/fgene.2017.00001/full
+    %
+    % %{
+    % s1="CDK2, MKI67, RB1, E2F1, HIST1H2AE, CCNB1, CCNB1, CBX3, ND1, MKI67, GAPDH, CCNB1, CCNB2";
+    % g1=unique(strsplit(s1,", "));
+    % s2="ATAD2, BORA, BTG3, CASP8AP2, CCDC134, CCNA2, CCNB1, CCNB2, CCNE1, CCNE2, CDC25A, CDC25B, CDC25C, CDC5L, CDC7, CDCA3, CDCA4, CDCA7, CDK1, CDK19, CDK4, CDKN2C, CDKN2D, CDKN3, CKS1B, CKS2, CRLF3, DBF4B, DLGAP5, E2F1, E2F7, E2F8, FAM83D, FBXO5, FOXM1, FZR1, GMNN, LIN54, LIN9, MASTL, MELK, MYBL2, NPAT, ODF2, PA2G4, PBK, PIMREG, PKMYT1, PRR11, RBL1, STIL, TCF19, TFDP1, TICRR, TOE1, TRIM28, TTF2, UBE2C, UBE2S, UHRF2, USP37, WEE1, CDK2, AURKA, KIF14, DTL, GTSE1, PLK1";
+    % g2=unique(strsplit(s2,", "));
+    % g=unique([g1 g2]);
+    %
+    % return;
+    %
+    % options = weboptions('Timeout',21);
+    % fname=tempname;
+    % websave(fname,'https://raw.githubusercontent.com/hbc/tinyatlas/master/cell_cycle/Homo_sapiens.csv',options);
+    % % t=readtable('a.txt','PreserveVariableNames',true);
+    % warning off
+    % t=readtable(fname,'Range','A:B');
+    % warning on
+    % g=string(t.ApprovedSymbol);
+    % delete(fname);
+    % %}
+    %
+    % % https://academic.oup.com/jmcb/article/11/8/703/5188008
--- sc_cellscore_admdl.m
+++ sc_cellscore_admdl.m
@@ -1,106 +1,103 @@
 function [score] = sc_cellscore_admdl(X, genelist, tgsPos, tgsNeg, nbin, ctrl)
-% Compute cell scores from a list of feature genes
-%
-% tgsPos - positive features (negative target marker genes)
-% tgsNeg - negative features (negative target marker genes)
-%
-% see also: PKG.E_CELLSCORES, SC_CELLSCORE_UCELL
-%
-% ref: AddModuleScore - https://github.com/satijalab/seurat/blob/master/R/utilities.R
-% ref: https://www.ncbi.nlm.nih.gov/pmc/articles/PMC8271111/
-% disp("Seurat provides a computationally efficient gene signature scoring function,
-% named AddModuleScore, originally proposed by Tirosh et al. [5]. ")
-
-% The score is the average expression of a set of genes subtracted with
-% the average expression of a reference set of genes. The reference set is randomly sampled from the gene_pool for each binned expression value.
-% This reproduces the approach in Seurat [Satija15] and has been implemented for Scanpy by Davide Cittaro.
-
-if nargin < 6, ctrl = 5; end
-if nargin < 5, nbin = 25; end
-if nargin < 4
-    tgsNeg = [];
+    % Compute cell scores from a list of feature genes
+    %
+    % tgsPos - positive features (negative target marker genes)
+    % tgsNeg - negative features (negative target marker genes)
+    %
+    % see also: PKG.E_CELLSCORES, SC_CELLSCORE_UCELL
+    %
+    % ref: AddModuleScore - https://github.com/satijalab/seurat/blob/master/R/utilities.R
+    % ref: https://www.ncbi.nlm.nih.gov/pmc/articles/PMC8271111/
+    % disp("Seurat provides a computationally efficient gene signature scoring function,
+    % named AddModuleScore, originally proposed by Tirosh et al. [5]. ")
+    
+    % The score is the average expression of a set of genes subtracted with
+    % the average expression of a reference set of genes. The reference set is randomly sampled from the gene_pool for each binned expression value.
+    % This reproduces the approach in Seurat [Satija15] and has been implemented for Scanpy by Davide Cittaro.
+    
+    if nargin < 6, ctrl = 5; end
+    if nargin < 5, nbin = 25; end
+    if nargin < 4
+        tgsNeg = [];
+    end
+    if nargin < 3 || isempty(tgsPos)
+        error('USAGE: >>[score]=sc_cellscore_admdl(X,genelist,tgsPos);')
+        % tgsPos=["CD44","LY6C","KLRG1","CTLA","ICOS","LAG3"];
+    end
+    
+    if ~any(matches(genelist, tgsPos, 'IgnoreCase', true))
+        score = NaN(size(X, 2), 1);
+        warning('No feature genes found in GENELIST. NaN scores returned');
+        return;
+    end
+    
+    %genelist=upper(genelist);
+    %tgsPos=upper(tgsPos);
+    %tgsNeg=upper(tgsNeg);    
+    
+    X = sc_norm(X);
+    %disp('Library-size normalization...done.')
+    X = log(X+1);
+    %disp('Log(x+1) transformation...done.')
+    
+    %idx=matches(genelist, tgsPos, 'IgnoreCase',true);
+    % if ~any(idx)
+    %     score=NaN(size(X,2),1);
+    %     warning('No feature genes found in GENELIST.');
+    %     return;
+    % end
+    
+    [score] = i_calculate_score(X, genelist, tgsPos, 1, nbin, ctrl);
+    if ~isempty(tgsNeg) && any(strlength(tgsNeg) > 0)
+        [s] = i_calculate_score(X, genelist, tgsNeg, -1, nbin, ctrl);
+        score = score + s; % add scores from negative markers (greater values for non-target cells)
+    end
 end
-if nargin < 3 || isempty(tgsPos)
-    error('USAGE: >>[score]=sc_cellscore_admdl(X,genelist,tgsPos);')
-    % tgsPos=["CD44","LY6C","KLRG1","CTLA","ICOS","LAG3"];
-end
-
-if ~any(matches(genelist, tgsPos, 'IgnoreCase', true))
-    score = NaN(size(X, 2), 1);
-    warning('No feature genes found in GENELIST. NaN scores returned');
-    return;
-end
-
-%genelist=upper(genelist);
-%tgsPos=upper(tgsPos);
-%tgsNeg=upper(tgsNeg);
-
-
-X = sc_norm(X);
-%disp('Library-size normalization...done.')
-X = log(X+1);
-%disp('Log(x+1) transformation...done.')
-
-
-%idx=matches(genelist, tgsPos, 'IgnoreCase',true);
-% if ~any(idx)
-%     score=NaN(size(X,2),1);
-%     warning('No feature genes found in GENELIST.');
-%     return;
-% end
-
-[score] = i_calculate_score(X, genelist, tgsPos, 1, nbin, ctrl);
-if ~isempty(tgsNeg) && any(strlength(tgsNeg) > 0)
-    [s] = i_calculate_score(X, genelist, tgsNeg, -1, nbin, ctrl);
-    score = score + s; % add scores from negative markers (greater values for non-target cells)
-end
-end
-
-
+        
 function [score] = i_calculate_score(X, genelist, tgs, directtag, nbin, ctrl)
-if nargin < 6, ctrl = 5; end
-if nargin < 5, nbin = 25; end
-if nargin < 4, directtag = 1; end
-
-rng default
-% Initial stats
-cluster_lenght = size(X, 1);
-data_avg = mean(X, 2);
-[~, I] = sort(data_avg);
-
-% Sorting data
-data_avg = data_avg(I);
-gsorted = genelist(I);
-Xsorted = X(I, :);
-
-% Assigning bins by expression
-assigned_bin = diag(zeros(cluster_lenght));
-bin_size = cluster_lenght / nbin;
-for i = 1:nbin
-    bin_match = data_avg <= data_avg(round(bin_size*i));
-    pos_avail = (assigned_bin == 0);
-    assigned_bin(pos_avail & bin_match) = i;
-end
-
-% Selecting bins of same expression
-idx = matches(gsorted, tgs, 'IgnoreCase', true);
-selected_bins = unique(assigned_bin(idx));
-samebin_genes = gsorted(ismember(assigned_bin, selected_bins));
-ctrl_use = [];
-for i = 1:length(tgs)
-    ctrl_use = [ctrl_use; ...
-        randsample(samebin_genes, ctrl)];
-end
-ctrl_use = unique(ctrl_use);
-
-% Averaging expression
-ctrl_score = mean(Xsorted(matches(gsorted, ctrl_use, 'IgnoreCase', true), :), 1);
-features_score = mean(Xsorted(idx, :), 1);
-
-% Scoring
-if directtag > 0
-    score = transpose(features_score-ctrl_score);
-else
-    score = transpose(ctrl_score-features_score);
-end
+    if nargin < 6, ctrl = 5; end
+    if nargin < 5, nbin = 25; end
+    if nargin < 4, directtag = 1; end
+    
+    rng default
+    % Initial stats
+    cluster_lenght = size(X, 1);
+    data_avg = mean(X, 2);
+    [~, I] = sort(data_avg);
+    
+    % Sorting data
+    data_avg = data_avg(I);
+    gsorted = genelist(I);
+    Xsorted = X(I, :);
+    
+    % Assigning bins by expression
+    assigned_bin = diag(zeros(cluster_lenght));
+    bin_size = cluster_lenght / nbin;
+    for i = 1:nbin
+        bin_match = data_avg <= data_avg(round(bin_size*i));
+        pos_avail = (assigned_bin == 0);
+        assigned_bin(pos_avail & bin_match) = i;
+    end
+    
+    % Selecting bins of same expression
+    idx = matches(gsorted, tgs, 'IgnoreCase', true);
+    selected_bins = unique(assigned_bin(idx));
+    samebin_genes = gsorted(ismember(assigned_bin, selected_bins));
+    ctrl_use = [];
+    for i = 1:length(tgs)
+        ctrl_use = [ctrl_use; ...
+            randsample(samebin_genes, ctrl)];
+    end
+    ctrl_use = unique(ctrl_use);
+    
+    % Averaging expression
+    ctrl_score = mean(Xsorted(matches(gsorted, ctrl_use, 'IgnoreCase', true), :), 1);
+    features_score = mean(Xsorted(idx, :), 1);
+    
+    % Scoring
+    if directtag > 0
+        score = transpose(features_score-ctrl_score);
+    else
+        score = transpose(ctrl_score-features_score);
+    end
 end
