19ba25f90b99042e62e8413e1ff29d723aa69270
James Cai <jamescai@genomezoo.net>
2024-01-10 11:27:08 -0600

    Update require.py

--- +run/external/py_datamapplot/require.py
+++ +run/external/py_datamapplot/require.py
@@ -21,7 +21,7 @@ required  = {'h5py', 'numpy', 'pandas', 'scipy', 'numba', 'matplotlib', 'datamap
 #installed = {pkg.key for pkg in pkg_resources.working_set}
 installed = {distribution.metadata["Name"] for distribution in importlib.metadata.distributions()}
 
-missing   = required - installed2
+missing   = required - installed
 
 if missing:
     subprocess.check_call([sys.executable, '-m', 'pip', 'install', *missing])
