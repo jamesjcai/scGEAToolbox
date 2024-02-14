6d06679299b636fb1980a925c3020ce4b77c896a
sromero1992 <selimiles52@gmail.com>
2024-02-13 21:11:58 -0600

    Docomented and cleaned sc_splinefit.m, and initial version of sc_variability.m

--- sc_splinefit.m
+++ sc_splinefit.m
@@ -1,23 +1,33 @@
 function [T, Xsorted_completed, gsorted_completed, ...
     xyz1] = sc_splinefit(X, genelist, sortit, plotit, removenan)
-%SC_SPLINEFIT identify genes with a profile deviated from normal
-%
-% USAGE:
-% >> [X,genelist]=sc_readfile('example_data/GSM3044891_GeneExp.UMIs.10X1.txt');
-% >> [X]=sc_norm(X,'type','libsize');
-% >> [T]=sc_splinefit(X,genelist,true,true);
-
-% if nargin<2 || isempty(genelist)
-%     genelist=string(1:size(X,1))';
-%     genelist=strcat("gene_",genelist);
-% end
+%{
+    SC_SPLINEFIT identify genes with a profile deviated from normal
+    INPUTS
+        X ----------> Count matrix
+        genelist ---> gene list for count matrix X 
+        sortit -----> boolean to sort or not the gene statistics (it's required)
+        plotit -----> boolean to plot cubic spline fit
+        removenan --> removing noisy and undesired NaN data
+    OUTPUT
+        T ----------> table containing log mean (lgu), log coef_variance (lgcv),
+                      drop rate (dropr), distance spline-gene (d), pvalue
+                      (pval) and false discovery rate (fdr)
+        Xsorted ----> Sorted Xcount matrix according to T order (completed)
+        gsorted ----> Sorted genelist according to T order (completed)
+        xyz1 -------> Spline fit (cubic spline) of noisy data evaluated
+                      into s-grid-size (lgu size)
+    USAGE
+        [X,genelist]=sc_readfile('example_data/GSM3044891_GeneExp.UMIs.10X1.txt');
+        [X]=sc_norm(X,'type','libsize');
+        [T]=sc_splinefit(X,genelist,true,true);
+%}
 
 if nargin < 5, removenan = true; end
 if nargin < 4, plotit = false; end
 if nargin < 3, sortit = true; end
 if nargin < 2, genelist = string(1:size(X, 1)); end
 
-
+% Computing log-mean, log-coef_variance and drop out rate 
 [lgu, dropr, lgcv, gsorted, Xsorted, ...
     removedgidx, removedT] = sc_genestat(X, genelist, sortit, removenan);
 
@@ -31,43 +41,44 @@ else
     Xsorted_completed = Xsorted;
 end
 
-% lgu=zscore(lgu);
-% dropr=zscore(dropr);
-% lgcv=zscore(lgcv);
-
-%[~,i]=max(lgcv);
+% xyz contains log-mean, log-coef_variance and drop out rate 
 xyz = [lgu, lgcv, dropr];
 
-% [~,j]=sort(pdist2(xyz,xyz(i,:)));
-% xyz=xyz(j,:)';
-% lgu=lgu(j);
-% dropr=dropr(j);
-% lgcv=lgcv(j);
+% s is what? statistics curve?
+% Cumulative sum( sqrt( D1^2 + D2^2 + D3^2) )
+%  Di = [Xi(2)-Xi(1) Xi(3)-Xi(2) ... Xi(m)-Xi(m-1)]
+%  Xi can be lgu_i, lgcv_i, dropr_i
+s = cumsum( [ 0; sqrt( diff( lgu(:)  ).^2 + ...
+                       diff( lgcv(:) ).^2 + ...
+                       diff( dropr(:)).^2) ] );
 
-%xyz=[lgu dropr lgcv]';
-
-%s = cumsum([0;sqrt(diff(lgu(:)).^2 + diff(dropr(:)).^2 ...
-%    + diff(lgcv(:)).^2)]);
-s = cumsum([0; sqrt(diff(lgu(:)).^2+diff(lgcv(:)).^2 ...
-    +diff(dropr(:)).^2)]);
-
+% Construct polinomial points of cubic spline of noisy data
 pp1 = splinefit(s, xyz.', 15, 0.75);
+% Polynomial points evaluated from splinefit into s-grid-size
 xyz1 = ppval(pp1, s)';
 
-
+% search for nearest points
 [~, d] = dsearchn(xyz1, xyz);
-fitmeanv=xyz1(:,1);
-x=xyz(:,1); y=xyz(:,2);
-d(x>max(fitmeanv))=d(x>max(fitmeanv))./100;
-d(x<min(fitmeanv))=d(x<min(fitmeanv))./10;
-d((y-xyz1(:, 2))<0)=d((y-xyz1(:, 2))<0)./100;
 
+% passing the polynomial fit from gene statistics
+fitmeanv = xyz1(:,1);
 
-%D = pdist2(xyz, xyz1);
-%d = min(D, [], 2);
+% x and y values from lgu and lgcv 
+x = xyz(:,1); y = xyz(:,2);
+
+% if x is far from mean curve, then scale?
+d(x>max(fitmeanv)) = d(x>max(fitmeanv))./100;
+d(x<min(fitmeanv)) = d(x<min(fitmeanv))./10;
+d((y-xyz1(:, 2))<0) = d((y-xyz1(:, 2))<0)./100;
+
+% Obtain distances that are up to a cumulative probability up to 90%
 dx = d(d <= quantile(d, 0.9));
 
+% Normal distribution fit for distances in dx
 distFit = fitdist([-dx; dx], 'Normal');
+
+% Cumulative distribution function (cdf) according to
+% a standard normal distribution in d from dx sigma
 pval = normcdf(d, 0, distFit.sigma, 'upper');
 [~, ~, ~, fdr] = pkg.fdr_bh(pval);
 
@@ -77,9 +88,11 @@ if ~isempty(gsorted)
 else
     T = table(lgu, lgcv, dropr, d, pval, fdr);
 end
+
 % 'variablenames',{'Genes','Log10_Mean','Dropout_Rate','Log10_CV','Deviation_3DFeature'});
 
-T.d(T.dropr > (1 - 0.05)) = 0; % ignore genes with dropout rate > 0.95
+% ignore genes with dropout rate > 0.95
+T.d(T.dropr > (1 - 0.05)) = 0; 
 % disp('NOTE: Genes with dropout rate > 0.95 are excluded.');
 
 if ~isempty(removedT) && istable(removedT)
