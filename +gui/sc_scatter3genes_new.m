5e545755caa3a310f7164751b1f821c69d8ba89d
James Cai <jamescai@genomezoo.net>
2024-02-25 09:57:50 -0600

    Update sc_scatter3genes_new.m

--- +gui/sc_scatter3genes_new.m
+++ +gui/sc_scatter3genes_new.m
@@ -14,6 +14,8 @@ z = dropr;
 
 fw = gui.gui_waitbar;
 hFig = figure('Visible','off');
+hFig.Position(3)=hFig.Position(3)*1.8;
+
 if ~isempty(parentfig)
     [px_new] = gui.i_getchildpos(parentfig, hFig);
     if ~isempty(px_new)
@@ -24,25 +26,24 @@ if ~isempty(parentfig)
 end
 
 hAx = axes('Parent', hFig);
+
 UitoolbarHandle = uitoolbar('Parent', hFig);
 set(UitoolbarHandle, 'Tag', 'FigureToolBar', ...
     'HandleVisibility', 'off', 'Visible', 'on');
-
 pkg.i_addbutton2fig(UitoolbarHandle, 'off', @in_ShowProfile, 'plotpicker-qqplotx.gif', 'Show Profile of Genes');
-
 pkg.i_addbutton2fig(UitoolbarHandle, 'off', @in_HighlightGenes, 'plotpicker-qqplot.gif', 'Highlight top HVGs');
 pkg.i_addbutton2fig(UitoolbarHandle, 'off', @in_HighlightSelectedGenes, 'xplotpicker-qqplot.gif', 'Highlight selected genes');
 pkg.i_addbutton2fig(UitoolbarHandle, 'off', @ExportGeneNames, 'export.gif', 'Export selected HVG gene names...');
 pkg.i_addbutton2fig(UitoolbarHandle, 'off', @ExportTable, 'xexport.gif', 'Export HVG Table...');
 pkg.i_addbutton2fig(UitoolbarHandle, 'off', @EnrichrHVGs, 'plotpicker-andrewsplot.gif', 'Enrichment analysis...');
 pkg.i_addbutton2fig(UitoolbarHandle, 'off', @ChangeAlphaValue, 'xplotpicker-andrewsplot.gif', 'Change MarkerFaceAlpha value');
-
 gui.add_3dcamera(UitoolbarHandle, 'HVGs');
 pkg.i_addbutton2fig(UitoolbarHandle, 'off', {@gui.i_savemainfig, 3}, "powerpoint.gif", 'Save Figure to PowerPoint File...');
 
 if showdata
     %h=scatter3(hAx,x,y,z);  % 'filled','MarkerFaceAlpha',.5);
-    h = scatter3(hAx, x, y, z, 'filled', 'MarkerFaceAlpha', .1);
+    hAx1 = subplot(1,2,1);
+    h = scatter3(hAx1, x, y, z, 'filled', 'MarkerFaceAlpha', .1);
 
     if ~isempty(g)
         dt = datacursormode(hFig);
@@ -57,7 +58,6 @@ end
     xlabel('Mean, log');
     ylabel('CV, log');
     zlabel('Dropout rate (% of zeros)');
-
 
 % [xData, yData, zData] = prepareSurfaceData(x,y,z);
 % xyz=[xData yData zData]';
@@ -76,11 +76,10 @@ if dofit
     %     pp1 = splinefit(s,xyz,pieces,0.75);
     %     xyz1 = ppval(pp1,s);
     hold on
-    plot3(xyz1(:, 1), xyz1(:, 2), xyz1(:, 3), '-', 'linewidth', 4);
+    plot3(hAx1, xyz1(:, 1), xyz1(:, 2), xyz1(:, 3), '-', 'linewidth', 4);
     % scatter3(xyz1(:,1),xyz1(:,2),xyz1(:,3)); %,'MarkerEdgeAlpha',.8);
 
     [~, d] = dsearchn(xyz1, [x, y, z]);
-
 
     fitmeanv=xyz1(:,1);
     d(x>max(fitmeanv))=d(x>max(fitmeanv))./100;
@@ -107,15 +106,27 @@ if dofit
     disp('to the fitted 3D spline surface. Genes with larger distances are ')
     disp('ranked higher for feature selection.')        
 end
+
+
+hAx2 = subplot(1,2,2);
+xlim([1 size(X,2)]);
+xlabel(hAx2,'Cell Index')
+ylabel(hAx2,'Expression Level')
+dt.UpdateFcn = {@in_myupdatefcn3, g};
+
 gui.gui_waitbar(fw);
 hFig.Visible = true;
 drawnow;
 
 
     function in_ShowProfile(~, ~)
-        if ~isempty(dt)
-           dt.UpdateFcn = {@i_myupdatefcn3, g, X};
-        end
+        idx = 1;
+        x1 = X(idx, :);
+        stem(hAx2, 1:length(x1), x1, 'marker', 'none');
+        xlim(hAx2,[1 size(X,2)]);
+        title(hAx2, g(idx));
+        xlabel(hAx2,'Cell Index')
+        ylabel(hAx2,'Expression Level')
     end
 
     function ChangeAlphaValue(~, ~)
@@ -200,5 +211,20 @@ drawnow;
         if ~all(yes), error('Running time error.'); end
         tgenes=Tx.hvg;
         gui.i_enrichtest(tgenes, g, numel(tgenes));
-    end 
+    end
+
+    function txt = in_myupdatefcn3(~, event_obj, g)
+        idx = event_obj.DataIndex;
+        txt = {g(idx)};
+        x1 = X(idx, :);
+        stem(hAx2, 1:length(x1), x1, 'marker', 'none');
+        xlim(hAx2,[1 size(X,2)]);
+        title(hAx2, g(idx));
+
+        [titxt] = gui.i_getsubtitle(x1);
+        subtitle(hAx2, titxt);
+        xlabel(hAx2,'Cell Index')
+        ylabel(hAx2,'Expression Level')        
+    end
+
 end
