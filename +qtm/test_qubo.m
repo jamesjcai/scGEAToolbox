05ff0b628014a813ea11cac51785293fe66be3f1
James Cai <jamescai@genomezoo.net>
2024-02-26 07:33:18 -0600

    Update test_qubo.m

--- +qtm/test_qubo.m
+++ +qtm/test_qubo.m
@@ -5,4 +5,10 @@ c = [-5 6 -4];
 d = 12;
 qprob = qubo(Q,c,d);
 ts = tabuSearch(Display="iter",MaxStallTime=0.01);
-result = solve(qprob,Algorithm=ts)
+result = solve(qprob,Algorithm=ts);
+
+% https://arxiv.org/ftp/arxiv/papers/1811/1811.11538.pdf
+Q=[-5 2 4 0; 2 -3 1 0; 4 1 -8 5; 0 0 5 -6];
+qprob=qubo(Q);
+r=solve(qprob);
+r.BestX
