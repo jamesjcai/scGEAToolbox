7bef9aad019022c7c6ec8092d8b64e74fafb2bb1
James Cai <jamescai@genomezoo.net>
2024-03-06 13:12:22 -0600

    Update gui_uishowrefinfo.m

--- +gui/gui_uishowrefinfo.m
+++ +gui/gui_uishowrefinfo.m
@@ -1,5 +1,8 @@
-function [txt,T] = gui_uishowrefinfo(reftarget)
+function [y,txt,T] = gui_uishowrefinfo(reftarget,parentfig)
 
+if nargin<2, parentfig = []; end
+
+y = false;
 txt = [];
 pw1 = fileparts(mfilename('fullpath'));
 fname = fullfile(pw1, '..','resources','refinfo.txt');
@@ -19,34 +22,44 @@ end
 % end
 
 fig = uifigure;
-g = uigridlayout(fig,[3 1]);
-g.RowHeight = {'fit','1x','fit'};
-g.ColumnWidth = {'1x'};
+g = uigridlayout(fig,[3 2]);
+g.RowHeight = {'fit','2x','fit'};
+g.ColumnWidth = {'1x','1x'};
 
 lbl = uilabel(g,"Text",reftarget);
 lbl.Layout.Row = 1;
 lbl.Layout.Column = 1;
 txa = uitextarea(g);
 txa.Layout.Row = 2;
-txa.Layout.Column = 1;
+txa.Layout.Column = [1 2];
 txa.Value = txt;
 % txa.Position(4)=txa.Position(4)*2;
-btn = uibutton(g,"Text","Submit","Enable","off");
+btn = uibutton(g,"Text","Continue","Enable","on");
 btn.Layout.Row = 3;
 btn.Layout.Column = 1;
+btn.ButtonPushedFcn = @(src,event) textEntered(src,event,btn);
 
-txa.ValueChangedFcn = @(src,event) textEntered(src,event,btn);
-end
+btn2 = uibutton(g,"Text","Cancel","Enable","on");
+btn2.Layout.Row = 3;
+btn2.Layout.Column = 2;
+btn2.ButtonPushedFcn = @(src,event) textEntered(src,event,btn2);
 
-function textEntered(src,event,btn)
-val = src.Value;
-btn.Enable = "off";
-% Check each element of text area cell array for text
-for k = 1:length(val)
-    if ~isempty(val{k})
-        btn.Enable = "on";
-        break
+
+if ~isempty(parentfig) && isa(parentfig,'matlab.ui.Figure') 
+    [px_new] = gui.i_getchildpos(parentfig, hFig);
+    if ~isempty(px_new)
+        movegui(hFig, px_new);
     end
 end
+
+waitfor(fig);
+
+function textEntered(src,event,btn)
+    if strcmp(btn.Text,"Continue")
+        y = true;
+    end
+    delete(fig);
+end
+
 end
 
