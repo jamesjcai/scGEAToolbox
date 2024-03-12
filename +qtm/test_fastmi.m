9fc2ada2f691098d275c32209ab69a8f18feadaa
James Cai <jamescai@genomezoo.net>
2024-03-11 21:32:46 -0500

    Update test_fastmi.m

--- +qtm/test_fastmi.m
+++ +qtm/test_fastmi.m
@@ -28,14 +28,20 @@ R0  = qtm.FastPairMI(data, h);
 R = R0(1:end-1,1:end-1)/(K-1);
 J = R0(end,1:end-1);
 
+tic;
 fun = @(alpha)howmany(alpha,R,J) - K;
 alphasol = fzero(fun,[0 1]);
 % alphasol = 0.3;
 [~,xsol] = howmany(alphasol,R,J);
-
-
 g(find(xsol.BestX))
+toc;
 
+tic;
+B = lasso(X',y');
+g(B(:,80)~=0)
+toc;
+
+intersect(g(find(xsol.BestX)), g(B(:,80)~=0))
 
 
 function [n,result] = howmany(alpha,R,J)
