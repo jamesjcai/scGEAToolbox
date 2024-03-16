5b4aaa9854aad453a6c1726df955886956b2ba5d
James Cai <jamescai@genomezoo.net>
2024-03-16 14:16:22 -0500

    Update callback_MultiGroupingViewer.m

--- +gui/callback_MultiGroupingViewer.m
+++ +gui/callback_MultiGroupingViewer.m
@@ -3,27 +3,27 @@ function callback_MultiGroupingViewer(src, ~)
     sce = guidata(FigureHandle);
     
     answer = questdlg('Select the type of multi-view:','', ...
-        'Two-group','Multigrouping','Multiembedding','Two-group');
+        'Multigrouping','Multiembedding','Cancel','Multigrouping');
 
     switch answer
-        case 'Two-group'
-
-            if matlab.ui.internal.isUIFigure(FigureHandle), focus(FigureHandle); end
-            [thisc1, clable1, thisc2, clable2] = gui.i_select2state_new(sce);
-            if isempty(thisc1) || isempty(thisc2), return; end
-            
-            if matlab.ui.internal.isUIFigure(FigureHandle), focus(FigureHandle); end    
-            fw=gui.gui_waitbar;    
-            [c, cL] = grp2idx(thisc1);
-            cx1.c = c;
-            cx1.cL = strrep(cL, '_', '\_');
-            [c, cL] = grp2idx(thisc2);
-            cx2.c = c;
-            cx2.cL = strrep(cL, '_', '\_');
-            gui.sc_multigroupings(sce, cx1, cx2, clable1, clable2, FigureHandle);
-            gui.gui_waitbar(fw);
-            
-            if matlab.ui.internal.isUIFigure(FigureHandle), focus(FigureHandle); end
+        % case 'Two-group'
+        % 
+        %     if matlab.ui.internal.isUIFigure(FigureHandle), focus(FigureHandle); end
+        %     [thisc1, clable1, thisc2, clable2] = gui.i_select2state_new(sce);
+        %     if isempty(thisc1) || isempty(thisc2), return; end
+        % 
+        %     if matlab.ui.internal.isUIFigure(FigureHandle), focus(FigureHandle); end    
+        %     fw=gui.gui_waitbar;    
+        %     [c, cL] = grp2idx(thisc1);
+        %     cx1.c = c;
+        %     cx1.cL = strrep(cL, '_', '\_');
+        %     [c, cL] = grp2idx(thisc2);
+        %     cx2.c = c;
+        %     cx2.cL = strrep(cL, '_', '\_');
+        %     gui.sc_multigroupings(sce, cx1, cx2, clable1, clable2, FigureHandle);
+        %     gui.gui_waitbar(fw);
+        % 
+        %     if matlab.ui.internal.isUIFigure(FigureHandle), focus(FigureHandle); end
 
         case 'Multigrouping'
             if matlab.ui.internal.isUIFigure(FigureHandle), focus(FigureHandle); end
@@ -49,17 +49,14 @@ function callback_MultiGroupingViewer(src, ~)
             dt.UpdateFcn = {@in_myupdatefcnx12};
             %evalin('base', 'h = findobj(gcf,''type'',''axes'');');
             %evalin('base', 'hlink = linkprop(h, {''CameraPosition'',''CameraUpVector''});');
-            
             evalin('base', 'linkprop(findobj(gcf,''type'',''axes''), {''CameraPosition'',''CameraUpVector''});');
             %h = findobj(hFig,'type','axes');
             %linkprop(h, {'CameraPosition','CameraUpVector'});
-
             rotate3d(hFig,'on');
             hBr = brush(hFig);
             hBr.ActionPostCallback = {@onBrushAction, axesv};
             
-        case 'Multiembedding'            
-
+        case 'Multiembedding'
             listitems = fieldnames(sce.struct_cell_embeddings);
             n = length(listitems);
             valididx = false(n,1);
@@ -75,48 +72,16 @@ function callback_MultiGroupingViewer(src, ~)
                 return;
             end
             n = length(listitems);
-
             [indx2, tf2] = listdlg('PromptString', ...
                 {'Select embeddings:'}, ...
                 'SelectionMode', 'multiple', ...
                 'ListString', listitems, ...
                 'InitialValue', 1:n);
             if tf2 == 1
-
                 gui.sc_multiembeddingview(sce, listitems(indx2), FigureHandle);
-
-                %{
-                hFig = figure('Visible', 'off');
-                hFig.Position(3) = hFig.Position(3) * 1.8;
-                axesv = cell(length(indx2),1);
-                for k=1:length(indx2)
-                    s = sce.struct_cell_embeddings.(listitems{k});
-                    if size(s,2)>1 && size(s,1)==sce.NumCells
-                        axesv{k} = nexttile;
-                        gui.i_gscatter3(s, sce.c, 1, 1);
-                        title(listitems{k});
-                    end
-                end
-                [px_new] = gui.i_getchildpos(FigureHandle, hFig);
-                if ~isempty(px_new)
-                    movegui(hFig, px_new);
-                else
-                    movegui(hFig, 'center');
-                end
-                drawnow;
-                hFig.Visible=true;
-                % evalin('base', 'h = findobj(gcf,''type'',''axes'');');
-                % evalin('base', 'a = false(length(h),1); for k=1:length(h), [~,b]=view(h(k)); if b==90, a(k) = true; end; end');
-                % evalin('base', 'h(a) = [];');
-                % evalin('base', 'hlink = linkprop(h,{''CameraPosition'',''CameraUpVector''});');
-
-                evalin('base', 'linkprop(findobj(gcf,''type'',''axes''), {''CameraPosition'',''CameraUpVector''});');
-
-                hBr = brush(hFig);
-                hBr.ActionPostCallback = {@onBrushAction, axesv};
-                %}
             end
         otherwise
+
     end
 
         function onBrushAction(~, event, axv)
