b38c109b636d8c30d0b8f7f2b3cbf8048f3b5aa9
James Cai <jamescai@genomezoo.net>
2024-03-18 13:09:22 -0500

    .

--- +run/external/R_decontX/require.R
+++ +run/external/R_decontX/require.R
@@ -12,3 +12,19 @@ if (!requireNamespace("BiocManager", quietly = TRUE)){
 }
 BiocManager::install("rhdf5")
 }
+
+#if (!requireNamespace("scater", quietly = TRUE)){
+#if (!requireNamespace("BiocManager", quietly = TRUE)){
+#    install.packages("BiocManager", repo="http://cran.rstudio.com/")
+#}
+#BiocManager::install("scater")
+#}
+
+#if (!requireNamespace("decontX", quietly = TRUE)){
+#if (!requireNamespace("BiocManager", quietly = TRUE)){
+#    install.packages("BiocManager", repo="http://cran.rstudio.com/")
+#}
+#BiocManager::install("decontX")
+#}
+
+
--- +run/external/R_decontX/script.R
+++ +run/external/R_decontX/script.R
@@ -1,19 +1,25 @@
-#setwd("U:\\GitHub\\scGEAToolbox\\+run\\external\\R_decontX")
 library(celda)
 library(rhdf5)
 
-#library(R.matlab)
-#sc <- read.table('input.csv', sep = ',', stringsAsFactors = FALSE)
-#inputd <-readMat('input6.mat')
-#sc2 <- inputd$X
+# library(R.matlab)
+# sc <- read.table('input.csv', sep = ',', stringsAsFactors = FALSE)
+# inputd <-readMat('input6.mat')
+# sc2 <- inputd$X
 
 sc <- h5read(file = "input.mat", name = "/X")
 res <- decontX(x = sc)
-X=res$decontXcounts
-contamination=res$contamination
+X = res$decontXcounts
+contamination = res$contamination
 h5createFile("output.h5")
-h5write(as.matrix(X), "output.h5","/X")
-h5write(contamination, "output.h5","/contamination")
+h5write(as.matrix(X), "output.h5", "/X")
+h5write(contamination, "output.h5", "/contamination")
 
-#writeMat("output.mat", X = X, contamination=contamination)
-#write.table(as.matrix(X),file="output.csv", sep=",",col.names=FALSE,row.names = FALSE)
+# writeMat("output.mat", X = X, contamination=contamination)
+# write.table(as.matrix(X),file="output.csv", sep=",",col.names=FALSE, row.names = FALSE)
+
+# Celda is a suite of Bayesian hierarchical models for clustering 
+# single-cell RNA-sequencing (scRNA-seq) data. It is able to perform 
+# "bi-clustering" and simultaneously cluster genes into gene modules 
+# and cells into cell subpopulations. It also contains DecontX, a novel 
+# Bayesian method to computationally estimate and remove RNA 
+# contamination in individual cells without empty droplet information. 
