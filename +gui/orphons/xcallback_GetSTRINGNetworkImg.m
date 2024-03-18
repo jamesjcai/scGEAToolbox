cbfef401f7a3181bd2732f8913ca43ecaf3d7e4b
James Cai <jamescai@genomezoo.net>
2024-03-18 15:57:34 -0500

    .

--- +gui/orphons/xcallback_GetSTRINGNetworkImg.m
+++
@@ -1,6 +0,0 @@
-function xcallback_GetSTRINGNetworkImg(src, ~, ~)
-
-FigureHandle = src.Parent.Parent;
-sce = guidata(FigureHandle);
-
-[speciestag] = i_selectspecies(3);
--- @SingleCellNetwork/detect_modules.m
+++
@@ -1,5 +0,0 @@
-% methodid={'ap','wgcna','gcna'}
-%
-% ap   - affinity propagation
-% wgcna -
-% gcna - https://bmcbioinformatics.biomedcentral.com/articles/10.1186/s12859-019-2733-5
