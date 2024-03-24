57fc68571c33f60509e4eed0993169aa7a0131b1
James Cai <jamescai@genomezoo.net>
2024-03-24 14:30:30 -0500

    Update callback_DVGene2Groups.m

--- +gui/callback_DVGene2Groups.m
+++ +gui/callback_DVGene2Groups.m
@@ -1,12 +1,11 @@
 function callback_DVGene2Groups(src, ~)
 
-warndlg('The function is under development.');
-return;
+%warndlg('The function is under development.');
+%return;
 
     FigureHandle = src.Parent.Parent;
     sce = guidata(FigureHandle);
     gui.gui_showrefinfo('DV Analysis');
-    
     
     [i1, i2, cL1, cL2] = gui.i_select2grps(sce);
     if length(i1) == 1 || length(i2) == 1, return; end
@@ -26,19 +25,31 @@ return;
     sce1 = sce1.qcfilter;  
     sce2 = sce2.qcfilter;  
 
-
-    % fw = gui.gui_waitbar;
+    if sce1.NumCells < 50 || sce2.NumCells < 50
+        uiwait(warndlg('One of groups contains too few cells (n < 50). The result may not be reliable.',''));
+    end
+    if sce1.NumGenes < 50 || sce2.NumGenes < 50
+        uiwait(warndlg('One of groups contains too few genes (n < 50). The result may not be reliable.',''));
+    end
 
     [T1, ~, ~, xyz1] = sc_splinefit(sce1.X, sce1.g, true, false);
     [T2, ~, ~, xyz2] = sc_splinefit(sce2.X, sce2.g, true, false);
     
+    [~,ia,ib] = intersect(T1.genes, T2.genes);
+    T1 = T1(ia, :);
+    T2 = T2(ib, :);
+
     %x1 = T1.lgu; y1 = T1.lgcv; z1 = T1.dropr;
     %x2 = T2.lgu; y2 = T2.lgcv; z2 = T2.dropr;
     
     valididx = T1.nearidx>0 & T2.nearidx>0;
+    T1 = T1(valididx,:);
+    T2 = T2(valididx,:);
+
+    assert(isequal(T1.genes, T2.genes))
     
-    bd = abs(xyz1(T1.nearidx(valididx)) - xyz2(T2.nearidx(valididx))); % baseline difference
-    dd = abs(T1.d(valididx) - T2.d(valididx));
+    bd = vecnorm(xyz1(T1.nearidx,:) - xyz2(T2.nearidx,:),2,2); % baseline difference
+    dd = abs(T1.d - T2.d);
     ddn = dd./bd;
     
     BaselineDiffDist = zeros(size(T1,1), 1);
@@ -73,6 +84,7 @@ return;
 
 % ---------
 
+%{
 hFig = figure('Visible','off');
 hFig.Position(3)=hFig.Position(3)*1.8;
 [px_new] = gui.i_getchildpos(FigureHandle, hFig);
@@ -122,3 +134,4 @@ title(hAx2, '');
 subtitle(hAx2, titxt);
 xlabel(hAx2,'Cell Index');
 ylabel(hAx2,'Expression Level');
+%}
