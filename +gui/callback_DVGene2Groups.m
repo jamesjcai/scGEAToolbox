452e5718466572352dd2fd00d09a4cab373c4cf5
James Cai <jamescai@genomezoo.net>
2024-03-26 16:13:12 -0500

    Update callback_DVGene2Groups.m

--- +gui/callback_DVGene2Groups.m
+++ +gui/callback_DVGene2Groups.m
@@ -3,16 +3,22 @@ function callback_DVGene2Groups(src, ~)
 %warndlg('The function is under development.');
 %return;
 
+cx=lines(2);
+cx1=cx(1,:);
+cx2=cx(2,:);
+
     FigureHandle = src.Parent.Parent;
     sce = guidata(FigureHandle);
     if ~gui.gui_showrefinfo('DV Analysis'), return; end
     
-    [i1, i2, cL1, cL2] = gui.i_select2grps(sce);
+    [i1, i2, cL1, cL2] = gui.i_select2grps(sce, false);
     if length(i1) == 1 || length(i2) == 1, return; end
+
+    fw = gui.gui_waitbar;
     
     c=zeros(size(i1));
     c(i1)=1; c(i2)=2;
-    cL=[cL1;cL2];
+    % cL=[cL1;cL2];
     cL1 = matlab.lang.makeValidName(cL1);
     cL2 = matlab.lang.makeValidName(cL2);
     if ~all(c>0)
@@ -34,17 +40,25 @@ function callback_DVGene2Groups(src, ~)
         uiwait(warndlg('One of groups contains too few genes (n < 50). The result may not be reliable.',''));
     end
 
-    [T1, ~, ~, xyz1] = sc_splinefit(sce1.X, sce1.g, true, false);
-    [T2, ~, ~, xyz2] = sc_splinefit(sce2.X, sce2.g, true, false);
+    [T1, X1, g1, xyz1] = sc_splinefit(sce1.X, sce1.g, true, false);
+    [T2, X2, g2, xyz2] = sc_splinefit(sce2.X, sce2.g, true, false);
     
     [g, ia, ib] = intersect(T1.genes, T2.genes);
     T1 = T1(ia, :);
     T2 = T2(ib, :);
 
-    sce1.X = sce1.X(ia, :);
-    sce1.g = sce1.g(ia);
-    sce2.X = sce2.X(ia, :);
-    sce2.g = sce2.g(ia);
+    X1 = X1(ia, :);
+    X2 = X2(ib, :);
+    g1 = g1(ia);
+    g2 = g2(ib);
+
+
+    %sce1.X = sce1.X(ia, :);
+    %sce1.g = sce1.g(ia);
+    %sce2.X = sce2.X(ib, :);
+    %sce2.g = sce2.g(ib);
+
+    %isequal(sce1.g, sce2.g)
 
     %x1 = T1.lgu; y1 = T1.lgcv; z1 = T1.dropr;
     %x2 = T2.lgu; y2 = T2.lgcv; z2 = T2.dropr;
@@ -53,12 +67,21 @@ function callback_DVGene2Groups(src, ~)
     T1 = T1(valididx,:);
     T2 = T2(valididx,:);
 
-    sce1.X = sce1.X(valididx, :);
-    sce1.g = sce1.g(valididx);
-    sce2.X = sce2.X(valididx, :);
-    sce2.g = sce2.g(valididx);
-    
+    % size(T1)
+    % size(xyz1)
+    % size(T2)
+    % size(xyz2)
+
+    X1 = X1(valididx, :);
+    g1 = g1(valididx);
+    X2 = X2(valididx, :);
+    g2 = g2(valididx);
+    g = g(valididx);
+
+    assert(isequal(g1, g2))
+    assert(isequal(g, g2))
     assert(isequal(T1.genes, T2.genes))
+    assert(isequal(T1.genes, g))
     
     bd = vecnorm(xyz1(T1.nearidx,:) - xyz2(T2.nearidx,:),2,2); % baseline difference
     dd = abs(T1.d - T2.d);
@@ -72,8 +95,8 @@ function callback_DVGene2Groups(src, ~)
     DiffDistRaw(valididx) = dd;
     DiffDistNormlized(valididx) = ddn;
 
-    x1 = T1.lgu; y1 = T1.lgcv; z1 = T1.dropr;
-    x2 = T2.lgu; y2 = T2.lgcv; z2 = T2.dropr;
+    px1 = T1.lgu; py1 = T1.lgcv; pz1 = T1.dropr;
+    px2 = T2.lgu; py2 = T2.lgcv; pz2 = T2.dropr;
     
     T1.Properties.VariableNames = append(T1.Properties.VariableNames, sprintf('_%s',cL1{1}));
     T2.Properties.VariableNames = append(T2.Properties.VariableNames, sprintf('_%s',cL2{1}));
@@ -88,7 +111,7 @@ function callback_DVGene2Groups(src, ~)
     
     T = sortrows(T,"DiffDistNormlized","descend");
     
-    %    gui.gui_waitbar(fw);
+    gui.gui_waitbar(fw);
 
     outfile = sprintf('%s_vs_%s', ...
         matlab.lang.makeValidName(string(cL1)), matlab.lang.makeValidName(string(cL2)));
@@ -114,29 +137,27 @@ tb = findall(hFig, 'Tag', 'FigureToolBar'); % get the figure's toolbar handle
 uipushtool(tb, 'Separator', 'off');
 hFig.Visible=true;
 hAx0 = subplot(2,2,[1 3]);
-h1 = scatter3(hAx0, x1, y1, z1, 'filled', 'MarkerFaceAlpha', .1);
+h1 = scatter3(hAx0, px1, py1, pz1, 'filled', 'MarkerFaceAlpha', .1);
 hold on
-h2 = scatter3(hAx0, x2, y2, z2, 'filled', 'MarkerFaceAlpha', .1);
-plot3(hAx0, xyz1(:, 1), xyz1(:, 2), xyz1(:, 3), '-', 'linewidth', 4);
-plot3(hAx0, xyz2(:, 1), xyz2(:, 2), xyz2(:, 3), '-', 'linewidth', 4);
+h2 = scatter3(hAx0, px2, py2, pz2, 'filled', 'MarkerFaceAlpha', .1);
+plot3(hAx0, xyz1(:, 1), xyz1(:, 2), xyz1(:, 3), '-b', 'linewidth', 4);
+plot3(hAx0, xyz2(:, 1), xyz2(:, 2), xyz2(:, 3), '-r', 'linewidth', 4);
 
 xlabel(hAx0,'Mean+1, log');
 ylabel(hAx0,'CV+1, log');
 zlabel(hAx0,'Dropout rate (% of zeros)');
 
 
-    if ~isempty(g)
-        dt = datacursormode(hFig);
-        dt.UpdateFcn = {@in_myupdatefcn3, g};
-    end
-X1 = sce1.X;
-X2 = sce2.X;
+if ~isempty(g)
+    dt = datacursormode(hFig);
+    dt.UpdateFcn = {@in_myupdatefcn3, g};
+end
 
 hAx1 = subplot(2,2,2);
 x1 = X1(1,:);
-sh1 = plot(hAx1, 1:length(x1), x1);
+sh1 = plot(hAx1, 1:length(x1), x1, 'Color',cx1);
 xlim(hAx1,[1 size(X1,2)]);
-title(hAx1, '');
+title(hAx1, sce1.g(1));
 [titxt] = gui.i_getsubtitle(x1);
 subtitle(hAx1, titxt);
 xlabel(hAx1,'Cell Index');
@@ -144,13 +165,14 @@ ylabel(hAx1,'Expression Level');
 
 hAx2 = subplot(2,2,4);
 x2 = X2(1,:);
-sh2 = plot(hAx2, 1:length(x2), x2);
+sh2 = plot(hAx2, 1:length(x2), x2, 'Color',cx2);
 xlim(hAx2,[1 size(X2,2)]);
-title(hAx2, '');
+title(hAx2, sce2.g(1));
 [titxt] = gui.i_getsubtitle(x1);
 subtitle(hAx2, titxt);
 xlabel(hAx2,'Cell Index');
 ylabel(hAx2,'Expression Level');
+h3 = [];
 
 
 
@@ -165,11 +187,18 @@ ylabel(hAx2,'Expression Level');
                 idx = idx - length(g);
             end
 
+            if ~isempty(h3)
+                delete(h3)                
+            end
+            h3 = plot3(hAx0, [px1(idx) px2(idx)], ...
+                [py1(idx), py2(idx)], ...
+                [pz1(idx), pz2(idx)],'k-','LineWidth',1);
+
             txt = {g(idx)};
 
             x1 = X1(idx, :);
             if ~isempty(sh1) && isvalid(sh1), delete(sh1); end
-            sh1 = plot(hAx1, 1:length(x1), x1, 'marker', 'none');
+            sh1 = plot(hAx1, 1:length(x1), x1, 'Color',cx1);
             xlim(hAx1,[1 size(X1,2)]);
             title(hAx1, g(idx));    
             [titxt] = gui.i_getsubtitle(x1);
@@ -179,7 +208,7 @@ ylabel(hAx2,'Expression Level');
 
             x2 = X2(idx, :);
             if ~isempty(sh2) && isvalid(sh2), delete(sh2); end
-            sh1 = plot(hAx2, 1:length(x2), x2, 'marker', 'none');
+            sh1 = plot(hAx2, 1:length(x2), x2, 'Color',cx2);
             xlim(hAx2,[1 size(X2,2)]);
             title(hAx2, g(idx));    
             [titxt] = gui.i_getsubtitle(x2);
