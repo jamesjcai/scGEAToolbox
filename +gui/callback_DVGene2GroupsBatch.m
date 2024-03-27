22dc0990d26720603c19f47c870156b89f0223c6
James Cai <jamescai@genomezoo.net>
2024-03-26 20:39:55 -0500

    Update callback_DVGene2GroupsBatch.m

--- +gui/callback_DVGene2GroupsBatch.m
+++ +gui/callback_DVGene2GroupsBatch.m
@@ -4,7 +4,7 @@ function callback_DVGene2GroupsBatch(src, ~)
 FigureHandle = src.Parent.Parent;
 sce = guidata(FigureHandle);
 
-if ~gui.gui_showrefinfo('DP in Batch Mode'), return; end
+if ~gui.gui_showrefinfo('DV in Batch Mode'), return; end
 
 if length(unique(sce.c_cell_type_tx)) == 1
     warndlg('Only one cell type or cell type is undetermined.','');
