0f30f5d77c0318277c8454fa7e75c6529298b0da
James Cai <jamescai@genomezoo.net>
2024-03-26 20:32:36 -0500

    Update callback_DEGene2Groups.m

--- +gui/callback_DEGene2Groups.m
+++ +gui/callback_DEGene2Groups.m
@@ -7,7 +7,7 @@ sce = guidata(FigureHandle);
 if ~gui.gui_showrefinfo('DE Analysis'), return; end
 
 
-[i1, i2, cL1, cL2] = gui.i_select2grps(sce);
+[i1, i2, cL1, cL2] = gui.i_select2grps(sce, false);
 if length(i1) == 1 || length(i2) == 1, return; end
 
 answer = questdlg('Which method?', ...
