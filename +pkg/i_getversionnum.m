6a90766ec04d290791169034f0a180d60c277aae
James Cai <jamescai@genomezoo.net>
2024-03-28 23:05:34 -0500

    .

--- +pkg/i_getversionnum.m
+++ +pkg/i_getversionnum.m
@@ -3,15 +3,36 @@ function [v1] = i_getversionnum
     mfolder = fileparts(mfilename('fullpath'));
     xfile = 'scGEAToolbox.prj';
     tag = 'param.version';
-
-    xfilelocal = fullfile(mfolder,'..', xfile);
-    fid = fopen(xfilelocal, 'r');
-    C = textscan(fid, '%s', 'delimiter', '\n');
-    fclose(fid);
-    a = C{1};
-    x = a(contains(a, sprintf('<%s>',tag)));
-    a1 = strfind(x, sprintf('<%s>',tag));
-    a2 = strfind(x, sprintf('</%s>',tag));
-    v1 = extractBetween(x, a1{1}+length(sprintf('<%s>',tag)), a2{1}-1);
-    % v1 = strrep(v1{1}, 'scGEAToolbox ', '');
-    v1 = v1{1};
+    
+    v1 = '';
+    try
+        xfilelocal = fullfile(mfolder,'..', xfile);
+        fid = fopen(xfilelocal, 'r');
+        C = textscan(fid, '%s', 'delimiter', '\n');
+        fclose(fid);
+        a = C{1};
+        x = a(contains(a, sprintf('<%s>',tag)));
+        a1 = strfind(x, sprintf('<%s>',tag));
+        a2 = strfind(x, sprintf('</%s>',tag));
+        v1 = extractBetween(x, a1{1}+length(sprintf('<%s>',tag)), a2{1}-1);
+        % v1 = strrep(v1{1}, 'scGEAToolbox ', '');
+        v1 = v1{1};
+    catch
+    end
+    if isempty(v1)
+    try
+        xfilelocal = fullfile(mfolder,'..', 'info.xml');
+        fid = fopen(xfilelocal, 'r');
+        C = textscan(fid, '%s', 'delimiter', '\n');
+        fclose(fid);
+        a = C{1};
+        % https://www.mathworks.com/matlabcentral/answers/359034-how-do-i-replace-textread-with-textscan
+        x = a(contains(a, '<version>'));
+        a1 = strfind(x, '<version>');
+        a2 = strfind(x, '</version>');
+        v1 = extractBetween(x, a1{1}+length('<version>'), a2{1}-1);
+        v1 = v1{1};
+    catch
+    end
+    end
+end
