b0c60199b8860ab3e437780b7954a71f12db167f
James Cai <jamescai@genomezoo.net>
2024-03-31 14:51:16 -0500

    Update i_violintabs.m

--- +gui/i_violintabs.m
+++ +gui/i_violintabs.m
@@ -19,7 +19,7 @@ import mlreportgen.ppt.*;
 pw1 = fileparts(mfilename('fullpath'));
 pth = fullfile(pw1, '..', 'resources', 'myTemplate.pptx');
 
-hFig = figure("Visible","off",'MenuBar','none','ToolBar','none');
+hFig = figure("Visible","off",'MenuBar','none','ToolBar','none', 'DockControls', 'off');
 hFig.Position(3) = hFig.Position(3) * 1.8;
 
 % if ~isempty(cx)
