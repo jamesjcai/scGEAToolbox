38a5e02eafedcac82d49255b2ba0bebe02b6241d
James Cai <jamescai@genomezoo.net>
2024-04-03 14:19:54 -0500

    .

--- +gui/i_enrichtest.m
+++ +gui/i_enrichtest.m
@@ -12,19 +12,3 @@ end
 numgene = min([length(genelist), numgene]);
 
 run.web_Enrichr_bkg(genelist, bkglist, numgene);
-
-
-% answer1 = gui.timeoutdlg(@(x){questdlg('Which functional enrichment analysis do you want to use?', 'Analysis Method', ...
-%     'Enrichr', 'GOrilla', 'Enrichr+GOrilla', 'Enrichr')}, 15);
-% if isempty(answer1), return; end
-% switch answer1
-%     case 'Enrichr'
-%         run.web_Enrichr(genelist, numgene, bkglist);
-%     case 'GOrilla'
-%         run.web_GOrilla(genelist(1:numgene));
-%     case 'Enrichr+GOrilla'
-%         run.web_Enrichr(genelist, numgene, bkglist);
-%         run.web_GOrilla(genelist(1:numgene));
-%     otherwise
-%         return;
-% end
