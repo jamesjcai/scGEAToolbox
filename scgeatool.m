3c97681dea06b19cedb649da7716004cf1314c2b
James Cai <jamescai@genomezoo.net>
2024-04-16 15:01:43 -0500

    .

--- scgeatool.m
+++ scgeatool.m
@@ -64,7 +64,7 @@ if ~isempty(s_in), sce.s = s_in; end
 [c, cL] = grp2idx(sce.c);
 
 if ~isempty(v1)
-    figname = sprintf('SCGEATOOL v%sc', v1);
+    figname = sprintf('SCGEATOOL v%sd', v1);
 else
     figname = 'SCGEATOOL';
 end
