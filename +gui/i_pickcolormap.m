a1b26237d64ec840a9727c841cf84fceb69022ad
jamesjcai <jamescai@genomezoo.net>
2024-11-02 15:59:29 -0500

    Update i_pickcolormap.m

--- +gui/i_pickcolormap.m
+++ +gui/i_pickcolormap.m
@@ -1,5 +1,10 @@
 function i_pickcolormap(~, ~, c, setzerocolor)
 if nargin < 4, setzerocolor = false; end
+if nargin < 3, c = []; end
+a = colormap;
+if all(a(1, :) == [0.8 0.8 0.8])
+    setzerocolor = true;
+end
 if setzerocolor
     list = {'parula', 'turbo', 'hsv', 'hot', 'cool', 'spring', ...
         'summer', 'autumn (default)', ...
@@ -16,8 +21,8 @@ if tf == 1
     if strcmp(a, 'autumn (default)')
         a = 'autumn';
     end
-    if setzerocolor
-        gui.i_setautumncolor(c, a);
+    if setzerocolor        
+        gui.i_setautumncolor(1:5, a);
     else
         colormap(a);
     end
