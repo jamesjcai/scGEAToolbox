a73cf6001a2b077215df0a2622383788e99a68d2
James Cai <jamescai@genomezoo.net>
2025-01-16 19:26:10 -0600

    Update i_addbutton2fig.m

--- +pkg/i_addbutton2fig.m
+++ +pkg/i_addbutton2fig.m
@@ -11,31 +11,48 @@ if ischar(callbackFnc) || isstring(callbackFnc)
     callbackFnc = str2func(callbackFnc);
 end
 
-if isempty(imgFil)
-% Add a "spacer" by using an invisible push tool
-pt = uipushtool(toolbarHdl, ...
-    'Separator', 'off', ...  % Adds visual separation
-    'Visible', 'on');      % Hides this tool to act as a spacer
+    if isempty(imgFil)
+        % % Add a "spacer" by using an invisible push tool
+        iconSize = [16, 16]; 
 
-else
-pt = uipushtool(toolbarHdl, 'Separator', sepTag);
-try
-    [ptImage, map] = imread(fullfile(mfolder, '..', 'resources', 'Images', imgFil));
-    if ~isempty(map)
-        ptImage = ind2rgb(ptImage, map);
+        %{
+        blankIcon = zeros([iconSize, 3]); % Black icon (invisible on dark themes)
+        pt = uipushtool(toolbarHdl, ...
+            'CData', blankIcon, ...
+            'TooltipString', '');
+        % pt = uipushtool(toolbarHdl, ...
+        %     'Separator', 'off', ...  % Adds visual separation
+        %     'Visible', 'on');      % Hides this tool to act as a spacer  
+        %}
+
+        
+        % bgColor = get(hFig, 'Color'); % Figure background color matches toolbar
+
+        bgColor = [0.9400    0.9400    0.9400];
+        % transparentIcon = zeros([iconSize, 3]); % RGB values all set to zero (black)
+        transparentIcon = repmat(reshape(bgColor, [1, 1, 3]), iconSize); % Match background
+        pt = uipushtool(toolbarHdl, ...
+            'CData', transparentIcon, ...
+            'TooltipString', '');
+    else
+        pt = uipushtool(toolbarHdl, 'Separator', sepTag);
+        try
+            [ptImage, map] = imread(fullfile(mfolder, '..', 'resources', 'Images', imgFil));
+            if ~isempty(map)
+                ptImage = ind2rgb(ptImage, map);
+            end
+        catch
+            ptImage = rand(16, 16, 3);
+        end
+        
+        if size(ptImage, 3) == 1
+        %     colormapName = gray(256);
+        %     ptImage = ind2rgb(uint8(ptImage * 255), colormapName);
+        ptImage = cat(3, ptImage, ptImage, ptImage);
+        end
+        
+        pt.CData = ptImage;
+        pt.Tooltip = tooltipTxt;
+        pt.ClickedCallback = callbackFnc;
     end
-catch
-    ptImage = rand(16, 16, 3);
-end
-
-if size(ptImage, 3) == 1
-%     colormapName = gray(256);
-%     ptImage = ind2rgb(uint8(ptImage * 255), colormapName);
-ptImage = cat(3, ptImage, ptImage, ptImage);
-end
-
-pt.CData = ptImage;
-pt.Tooltip = tooltipTxt;
-pt.ClickedCallback = callbackFnc;
-end
 end
