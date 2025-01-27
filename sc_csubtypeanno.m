49ee3ec65984a08640e28d41c1e3ff81ecdbf7c0
James Cai <jamescai@genomezoo.net>
2025-01-27 16:19:58 -0600

    Update sc_csubtypeanno.m

--- sc_csubtypeanno.m
+++ sc_csubtypeanno.m
@@ -1,4 +1,109 @@
 function [sce] = sc_csubtypeanno(sce, cell_type_target, formatid, speciestag)
+    if nargin < 4 || isempty(speciestag)
+        speciestag = 'human';
+    end
+
+    if nargin < 3 || isempty(formatid)
+        formatid = 0;
+    end
+
+    selectedidx = sce.c_cell_type_tx == cell_type_target;
+
+    if ~any(selectedidx)
+        error('SCE.C_CELLTYPE_TXT does not contain the target cell type.');
+    end
+
+    pw1 = fileparts(mfilename('fullpath'));
+    pth2 = fullfile(pw1, 'resources', 'PanglaoDB', 'cellsubtypes.xlsx');
+
+    T = readtable(pth2);
+
+    if ~ismember(upper(cell_type_target), upper(string(unique(T.CellType))))
+        error('Target cell type is not a primary cell type in cellsubtype.xlsx.');
+    end
+
+    idx = upper(string(T.CellType)) == upper(cell_type_target);
+
+    if ~any(idx)
+        error('SC_CSUBTYPEANNO: Runtime Error.');
+    end
+
+    T = T(idx, :);
+
+    [pmarkerstr] = in_getprimarymarkers(pw1, cell_type_target);
+    Tm = T(:, 2:3);
+    [Tm] = in_addprimarymarkers(Tm, pmarkerstr);
+
+    Tw = pkg.e_markerweight(Tm);
+
+    wvalu = Tw.Var2;
+    wgene = string(Tw.Var1);
+    celltypev = string(Tm.SubType);
+    markergenev = string(Tm.PositiveMarkers);
+
+    sce2 = sce.selectcells(selectedidx);
+    sce2 = sce2.embedcells('tsne3d', true, true, 3);
+    sce2 = sce2.clustercells([], [], true);
+
+    [c, cL] = grp2idx(sce2.c_cluster_id);
+
+    for ik = 1:max(c)
+        ptsSelected = c == ik;
+        [Tct] = pkg.e_determinecelltype(sce2, ptsSelected, wvalu, ...
+                wgene, celltypev, markergenev);
+
+        ctxt = Tct.C1_Cell_Type{1};
+        cL{ik} = ctxt;
+    end
+
+    sce2.c_cell_type_tx = string(cL(c));
+
+    sce.c_cell_type_tx(selectedidx) = in_formatsubtype(sce2.c_cell_type_tx, ...
+        cell_type_target, formatid);
+end
+
+function [a] = in_formatsubtype(a, b, formatid)
+    switch formatid
+        case 0
+            return;
+        case 1
+            for k = 1:length(a)
+                a(k) = sprintf('%s_{%s}', b, a(k));
+            end
+        case 2
+            for k = 1:length(a)
+                a(k) = sprintf('%s (%s)', b, a(k));
+            end
+    end
+end
+
+function [primarymarkerstr] = in_getprimarymarkers(pw1, cell_type_target)
+    pth1 = fullfile(pw1, '+run', 'thirdparty', 'alona_panglaodb', 'marker_hs.mat');
+    load(pth1, 'Tm');
+
+    if ~ismember(upper(cell_type_target), upper(string(Tm.Var1)))
+        error('Target cell type is not an available primary cell type.');
+    end
+
+    idx = upper(string(Tm.Var1)) == upper(cell_type_target);
+    primarymarkerstr = Tm.Var2{idx};
+    primarymarkerstr = strtrim(primarymarkerstr);
+    primarymarkerstr = erase(primarymarkerstr, " ");
+    primarymarkerstr = strip(primarymarkerstr, 'right', ',');
+end
+
+function [Tm] = in_addprimarymarkers(Tm, pmarkerstr)
+    for k = 1:size(Tm, 1)
+        a = string(Tm.PositiveMarkers{k});
+        a = strtrim(a);
+        a = erase(a, " ");
+        a = strip(a, 'right', ',');
+        Tm.PositiveMarkers{k} = char(string(a) + "," + pmarkerstr);
+    end
+end
+
+%{
+function [sce] = sc_csubtypeanno(sce, cell_type_target, formatid, speciestag)
 
     if nargin < 4 || isempty(speciestag), speciestag='human'; end
     if nargin < 3 || isempty(formatid), formatid = 0; end
@@ -118,3 +223,6 @@ end
             Tm.PositiveMarkers{k} = char(string(a)+","+pmarkerstr);
         end
     end
+
+%}
+
