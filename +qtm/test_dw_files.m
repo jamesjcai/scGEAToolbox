e6d6dc53f2d6c97574b7c8076851815a1c63d920
James Cai <jamescai@genomezoo.net>
2025-01-29 09:13:52 -0600

    Update test_dw_files.m

--- +qtm/test_dw_files.m
+++ +qtm/test_dw_files.m
@@ -1,5 +1,5 @@
-% chat = ollamaChat("deepseek-r1", TimeOut=1200);
-chat = ollamaChat("qwen2.5-coder", TimeOut=1200);
+% chat = ollamaChat("deepseek-r1", TimeOut = 1200);
+chat = ollamaChat("qwen2.5-coder", TimeOut = 1200);
 
 url = 'https://github.com/jamesjcai/scGEAToolbox/tree/main/%2Bgui';
 html = webread(url);
@@ -17,28 +17,20 @@ for i = 1:2 %length(file_links)
     outfile = filename(1:end-2) + "_coder.txt";
     fid = fopen(outfile, 'w');
     try
-        % Read file content
         code = webread(file_url);
         prompt = "Review this MATLAB code for professional feedback: " + code;
-        feedbk = generate(chat, prompt);
-        
+        feedbk = generate(chat, prompt);        
         fprintf(fid, '%s', feedbk);
-        % disp(content);
     catch
         fprintf('Failed to read %s\n', filename);
     end
     fclose(fid);
 end
 
-
-
-
 %{
 chat = ollamaChat("deepseek-r1", StreamFun=@printToken);
 prompt = "What is Model-Based Design?";
 generate(chat, prompt, MaxNumTokens=500);
-
-
 function printToken(token)
     fprintf("%s",token);
 end
