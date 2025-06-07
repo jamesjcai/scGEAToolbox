e1b68be469621b7f8a33ceba9dafbb4d152c7c27
James Cai <jamescai@genomezoo.net>
2025-06-07 14:01:47 -0500

    Update callback_Quantis.m

--- +gui/callback_Quantis.m
+++ +gui/callback_Quantis.m
@@ -1,10 +1,10 @@
 function callback_Quantis(src, ~)
     [FigureHandle, ~] = gui.gui_getfigsce(src);
 
-listitems = natsort(["Cisplatin","Carboplatin","Paclitaxel","Docetaxel","Doxorubicin","Cyclophosphamide","Methotrexate","5-Fluorouracil (5-FU)","Capecitabine","Gemcitabine","Vincristine","Etoposide","Imatinib","Trastuzumab","Bevacizumab","Rituximab","Pemetrexed","Nivolumab","Pembrolizumab","Tamoxifen"]);
-promptstr = "Select a drug:";
+    listitems = natsort(["Cisplatin","Carboplatin","Paclitaxel","Docetaxel","Doxorubicin","Cyclophosphamide","Methotrexate","5-Fluorouracil (5-FU)","Capecitabine","Gemcitabine","Vincristine","Etoposide","Imatinib","Trastuzumab","Bevacizumab","Rituximab","Pemetrexed","Nivolumab","Pembrolizumab","Tamoxifen"]);
+    promptstr = "Select a drug:";
 
-if gui.i_isuifig(FigureHandle)
+    if gui.i_isuifig(FigureHandle)
         [indx, tf] = gui.myListdlg(FigureHandle, listitems, ...
             promptstr);
     else     
@@ -12,7 +12,8 @@ if gui.i_isuifig(FigureHandle)
             {promptstr}, ...
             'SelectionMode', 'single', 'ListString', listitems, ...
             'ListSize', [220 300], 'Name', ' ');
-    end    
+    end
+    
 
     if tf == 1        
         gui.myHelpdlg(FigureHandle, "You selected "+listitems(indx)+".");
