341b984ec1a6fa17c1e20503e6bd91c491bed3e1
James Cai <jamescai@genomezoo.net>
2025-06-09 15:38:38 -0500

    Update sc_qcmetrics.m

--- sc_qcmetrics.m
+++ sc_qcmetrics.m
@@ -1,22 +1,38 @@
 function [nmad] = sc_qcmetrics(X)
-%QC metrics
+    % SC_QCMETRICS Calculate quality control metrics for single-cell data
+    %
+    %   NMAD = SC_QCMETRICS(X)
+    %
+    %   Input:
+    %       X - Gene expression matrix (genes x cells)
+    %
+    %   Output:
+    %       NMAD - Number of median absolute deviations below the median
+    %              of quality metrics
+    %
+    %   Description:
+    %   This function calculates quality control metrics for single-cell data.
+    %   The NMAD value represents the number of median absolute deviations
+    %   below the median of two quality metrics:
+    %   1. Log of library size
+    %   2. Log of number of expressed genes
+    %   
+    %   A cell is removed if it falls below one of these metrics by more than
+    %   the specified NMAD value. Higher NMAD values remove fewer cells,
+    %   while lower values remove more cells.
+    %
+    %   Reference:
+    %   https://alona.panglaodb.se/add.html
 
-%{
-https://alona.panglaodb.se/add.html
-
-nmad - this field gives the number of median absolute deviations (MADs)
-below the median of two quality metrics for which a cell is removed.
-    The two quality metrics are the log of the library size and
-    the log of the number of expressed genes.
-    It is enough for a cell to be below one of the metrics for it to be removed.
-            The default value is 3.
-            Give a higher value to remove less cells and a lower value to remove more cells
-
-            pct - Use genes expressed in at least X% of the cells
-            %}
-            nc = size(X, 2);
-            pct = sum(X > 0, 2) ./ nc; % 1%
-            reads_per_cell = sum(X, 2)'; % reads per cell
-            nmad = mad(log(sum(X, 2))-log(sum(X > 0, 2)), 1);
-
-        end
+    % Calculate number of cells
+    nc = size(X, 2);
+    
+    % Calculate percentage of cells expressing each gene
+    pct = sum(X > 0, 2) ./ nc; % 1%
+    
+    % Calculate reads per cell
+    reads_per_cell = sum(X, 2)'; % reads per cell
+    
+    % Calculate NMAD
+    nmad = mad(log(sum(X, 2)) - log(sum(X > 0, 2)), 1);
+end
