0b3576672053de89bedfbb32e3fb8b4b813b1d32
James Cai <jamescai@genomezoo.net>
2025-07-08 20:46:28 -0500

    Add Enrichr libraries selection callback
    Introduced callback_SelectEnrichrLibraries.m to handle user selection of Enrichr gene set libraries via a dialog. Updated scgeatoolApp.mlapp to integrate this new callback.

---
+++ +gui/callback_SelectEnrichrLibraries.m
@@ -0,0 +1,31 @@
+function callback_SelectEnrichrLibraries(src, ~)
+
+    [FigureHandle] = gui.gui_getfigsce(src);
+    try
+        [genesets] = in_selDataSources;
+    catch ME
+        gui.myErrordlg(FigureHandle,'Runtime error');
+    end
+    if ~isempty(genesets)
+        gui.myHelpdlg(FigureHandle, 'Enrichr gene set libraries selected.');
+    end
+
+
+    function [genesets] = in_selDataSources
+        genesets = [];
+        dsv = pkg.i_get_enrichr_libraries;
+
+        enrichrlibraries = getpref('scgeatoolbox', 'enrichrlibraries', ...
+                                   ["GO_Biological_Process_2025", ...
+                                    "GO_Molecular_Function_2025", ...
+                                     "KEGG_2021_Human",...
+                                     "Reactome_Pathways_2024"]);
+
+        [idx1] = gui.i_selmultidlg(dsv, enrichrlibraries, FigureHandle);
+        if isempty(idx1), return; end
+        if idx1 == 0, return; end
+        genesets = dsv(idx1);
+        setpref('scgeatoolbox', 'enrichrlibraries', genesets);    
+    end
+
+end
